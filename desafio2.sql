79601842cb117f40ea475bd833ca903c7256bc03
rodolforezende <rodolforrcont@gmail.com>
2021-09-03 17:16:28 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT
+(SELECT COUNT(*) FROM spotifyclone.musicas) AS `cancoes`,
+(SELECT COUNT(*) FROM spotifyclone.artistas) AS `artistas`,
+(SELECT COUNT(*) FROM spotifyclone.albuns) AS `albuns`;
