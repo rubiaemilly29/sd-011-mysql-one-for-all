ad8a504a048db7dcca3eeb00162e553c06a301f3
Filizzola <juliofilizzola@hotmail.com>
2021-09-03 15:45:05 -0300

    fixed desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT 	u.usuario as usuario, m.music_nome AS musica FROM
+SELECT 	u.usuario as usuario, m.music_nome AS nome FROM
 SpotifyClone.Historico as h 
 inner join SpotifyClone.usuarios as u
 inner join SpotifyClone.musicas as m
 ON h.usuario_id = u.usuario_id AND h.musica_id = m.musica_id
-ORDER BY usuario, musica;
+ORDER BY usuario, nome;
 
 SELECT * FROM historico_reproducao_usuarios;
