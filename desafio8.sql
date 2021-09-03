f534c9a89526e7e62520ad72d33050b6275c0315
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 19:26:38 -0300

    FIX: DESAFIO 8 sem alias

--- desafio8.sql
+++ desafio8.sql
@@ -4,10 +4,10 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.seguindo_artistas AS sa
-WHERE sa.usuario_id = old.usuario_id;
-DELETE FROM SpotifyClone.historico_de_reproducoes AS hr
-WHERE hr.usuario_id = old.usuario_id;
+DELETE FROM SpotifyClone.seguindo_artistas
+WHERE usuario_id = old.usuario_id;
+DELETE FROM SpotifyClone.historico_de_reproducoes
+WHERE usuario_id = old.usuario_id;
 END $$
 
 DELIMITER ;
