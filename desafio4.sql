edc2311ea89d5e245f60efc2e10b716643daaeb0
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-09-03 20:28:25 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT
+CONCAT(a.first_name, ' ', a.last_name) AS artista,
+COUNT(*) AS seguidores
+from SpotifyClone.artists AS a
+inner join SpotifyClone.user_following as follow
+ON follow.artist_id = a.artist_id
+GROUP BY `artista`
+ORDER BY `seguidores` DESC, `artista` ASC;
