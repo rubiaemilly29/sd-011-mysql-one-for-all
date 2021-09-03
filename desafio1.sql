9596d6074fea9352f89ee8492fdfe02735908419
guinhoanjos <iago.anjos@hotmail.com>
2021-09-03 18:43:34 -0300

    Update desafio1.sql

--- desafio1.sql
+++ desafio1.sql
@@ -1,43 +1,18 @@
 DROP DATABASE IF EXISTS SpotifyClone;
-CREATE DATABASE  IF NOT EXISTS `spotifyclone` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `spotifyclone`;
--- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
---
--- Host: localhost    Database: spotifyclone
--- ------------------------------------------------------
--- Server version	8.0.26
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `albums`
---
+CREATE DATABASE   'spotifyclone';
+USE 'spotifyclone';
 
 DROP TABLE IF EXISTS `albums`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `albums` (
-  `album_id` int NOT NULL AUTO_INCREMENT,
-  `album_nome` varchar(100) NOT NULL,
-  `artista_id` int NOT NULL,
-  PRIMARY KEY (`album_id`),
-  KEY `albums_ibfk_1` (`artista_id`),
-  CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`artista_id`) REFERENCES `artistas` (`artista_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `albums`
---
+CREATE TABLE 'albums' (
+  'album_id' int NOT NULL AUTO_INCREMENT,
+  'album_nome' varchar(100) NOT NULL,
+  'artista_id' int NOT NULL,
+  PRIMARY KEY ('album_id'),
+  KEY 'albums_ibfk_1' ('artista_id'),
+  CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`artista_id`) REFERENCES `artistas` (`artista_id`)
+) ENGINE=InnoDB;
+
 
 LOCK TABLES `albums` WRITE;
 /*!40000 ALTER TABLE `albums` DISABLE KEYS */;
