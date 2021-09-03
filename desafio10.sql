5fddf55da0e7432174782df07251bb0408a76cc9
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 19:01:48 -0300

    FIX: DESAFIO 10

--- desafio10.sql
+++ desafio10.sql
@@ -4,7 +4,7 @@ CREATE FUNCTION quantidade_musicas_no_historico(id_artista INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE qtd_musicas INT;
-SELECT COUNT(*)
+SET qtd_musicas = (SELECT COUNT(*)
 FROM SpotifyClone.cancoes AS c
 INNER JOIN
 SpotifyClone.albuns AS alb
@@ -12,7 +12,7 @@ ON alb.album_id = c.album_id
 INNER JOIN
 SpotifyClone.artistas AS art
 ON art.artista_id = alb.artista_id
-WHERE id_artista =  art.artista_id INTO qtd_musicas;
+WHERE id_artista =  art.artista_id);
 RETURN qtd_musicas;
 END $$
 
