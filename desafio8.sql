5e19b5b3af8490297e8b1a1f73baa044fc2e194f
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 17:42:35 -0300

    FEAT: Desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,1 +1,13 @@
-SELECT * FROM SpotifyClone;
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+    DELETE FROM SpotifyClone.seguindo_artistas AS sa
+    WHERE sa.usuario_id = old.usuario_id;
+    DELETE FROM SpotifyClone.historico_de_reproducoes AS hr
+    WHERE hr.usuario_id = old.usuario_id;
+END $$
+
+DELIMITER ;
