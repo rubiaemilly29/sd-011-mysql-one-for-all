41631938a1c198ef0c3c7d8663ac49476f1c7f99
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-03 17:59:35 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+CREATE VIEW top_3_artistas AS
+  SELECT (
+    SELECT nome
+    FROM SpotifyClone.artistas AS a
+    WHERE sa.artista_id = a.artista_id
+  ) AS `artista`,
+  COUNT(user_id) AS `seguidores`
+  FROM SpotifyClone.seguindo_artistas AS sa
+  GROUP BY artista_id
+  ORDER BY `seguidores` DESC, `artista`
+  LIMIT 3;
