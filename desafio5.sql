b7cb8d9423d3117c54639e6d3c93e67e0895d55c
benlopes <rubenssouss@hotmail.com>
2021-09-03 14:52:35 -0300

    add challenge 5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT C.cancao cancao, COUNT(RP.historico_de_reproducoes) `reproducoes`
+FROM SpotifyClone.cancoes C
+INNER JOIN SpotifyClone.reproducoes RP
+ON C.cancao_id = RP.cancao_id
+GROUP BY cancao
+ORDER BY `reproducoes` DESC LIMIT 2;
