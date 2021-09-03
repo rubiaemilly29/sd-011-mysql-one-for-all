4f1aab3d3bebdba5364d39c80ed37467f56d60e7
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-03 15:32:35 -0300

    Add Desafio como view

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,4 @@
-
-SELECT
+CREATE VIEW historico_reproducao_usuarios AS SELECT
 (SELECT nome_usuario FROM SpotifyClone.usuarios WHERE id_usuario=t1.id_usuario) AS `usuario`,
 (SELECT nome_cancao FROM SpotifyClone.cancoes WHERE id_cancao=t1.id_cancao) AS `nome`
 FROM SpotifyClone.historico AS t1 ORDER BY `usuario`, `nome`;
