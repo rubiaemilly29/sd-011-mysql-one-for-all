7324b05fe2284e287ab036cf6a465f5f13b9da96
marcoswarmling <marcoswarmling15@gmail.com>
2021-09-03 15:37:19 -0300

    refazendo o 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,14 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(user_name VARCHAR(20))
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE music_quantities INT;
-    SELECT COUNT(*)
+    SELECT COUNT(*) 
     FROM SpotifyClone.Historico_De_Reproducao AS HDR
     INNER JOIN SpotifyClone.Usuario AS US
     ON US.Usuario_ID = HDR.Usuario_ID
-    WHERE US.Nome = user_name
+    WHERE US.Usuario_ID = id
     GROUP BY HDR.Usuario_ID
     INTO music_quantities;
     RETURN music_quantities;
