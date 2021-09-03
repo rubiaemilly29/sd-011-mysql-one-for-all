5dacd9ad42a7c0e966489e89734a95e4d113b630
rodolforezende <rodolforrcont@gmail.com>
2021-09-03 19:02:03 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW perfil_artistas AS
+SELECT a.artista_nome AS `artista`,
+al.album_nome AS `album`,
+COUNT(*) AS `seguidores`
+FROM spotifyclone.artistas AS a
+INNER JOIN SpotifyClone.albuns AS al
+INNER JOIN SpotifyClone.seguidores AS s
+ON a.artista_id = al.artista_id
+AND a.artista_id = s.artista_id
+GROUP BY `artista`, `album`
+ORDER BY `seguidores` DESC, `artista` ASC, `album` ASC;
+
+SELECT * FROM perfil_artistas
