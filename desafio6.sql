297e0e1dc9e248d84318e9e978675ec2dabdb686
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-03 19:15:46 -0300

    Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
   SELECT 
   MIN(p.valor) AS `faturamento_minimo`,
   MAX(p.valor) AS `faturamento_maximo`,
-  ROUND(AVG(p.valor), 2) AS `faturamento_medio`,
+  FORMAT(ROUND(AVG(p.valor), 2), 2) AS `faturamento_medio`,
   SUM(p.valor) AS `faturamento_total`
   FROM SpotifyClone.usuarios AS u
   INNER JOIN SpotifyClone.planos AS p
