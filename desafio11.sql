c89f251e5537280b701b56783fce11d21c0ca0cc
anaclarabck <ackyotoku@gmail.com>
2021-09-03 17:55:29 -0300

    [ADD] requirement 11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT s.name AS 'nome',
+(SELECT COUNT(hr.song_id)
+FROM SpotifyClone.history_reproduction hr
+WHERE s.song_id = hr.song_id) AS 'reproducoes'
+FROM SpotifyClone.history_reproduction hr
+INNER JOIN SpotifyClone.songs s
+ON hr.song_id = s.song_id
+INNER JOIN SpotifyClone.users u
+ON hr.user_id = u.user_id
+WHERE u.plan_id IN (2,3)
+ORDER BY `nome`;
