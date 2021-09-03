b4788b21b2d3dc8f711e96ff6e855c59f500e0c8
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-09-03 18:38:41 -0300

    [ADD] Popula desafio1.sql com os devidos dados

--- desafio1.sql
+++ desafio1.sql
@@ -4,40 +4,87 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE plans (
-plan_id INT PRIMARY KEY,
+plan_id INT PRIMARY KEY AUTO_INCREMENT,
 plan_name VARCHAR(50) NOT NULL,
 plan_price DECIMAL(5, 2) NOT NULL
 );
 
+INSERT INTO plans (plan_name, plan_price)
+VALUES ('gratuito', 0),('familiar', 7.99), ('universitário', 5.99);
+
 CREATE TABLE users (
-user_id INT PRIMARY KEY,
+user_id INT PRIMARY KEY AUTO_INCREMENT,
 user_name VARCHAR(50) NOT NULL,
 age INT NOT NULL,
 plan_id INTEGER NOT NULL,
 FOREIGN KEY (plan_id) REFERENCES plans (plan_id)
 );
 
+INSERT INTO users (user_name, age, plan_id)
+VALUES
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
 CREATE TABLE artists (
-artist_id INT PRIMARY KEY,
+artist_id INT PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(50) NOT NULL,
 last_name VARCHAR(50) NOT NULL
 );
 
+INSERT INTO artists (first_name, last_name)
+VALUES
+('Walter', 'Phoenix'),
+('Peter', 'Strong'),
+('Lance', 'Day'),
+('Freedie', 'Shannon');
+
 CREATE TABLE albuns (
-album_id INT PRIMARY KEY,
+album_id INT PRIMARY KEY AUTO_INCREMENT,
 album_name VARCHAR(100) NOT NULL,
 artist_id INTEGER NOT NULL,
 FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 );
 
+INSERT INTO albuns (album_name, artist_id)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
 CREATE TABLE songs (
-song_id INT PRIMARY KEY NOT NULL,
+song_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 song_name VARCHAR(100) NOT NULL,
 artist_id INTEGER NOT NULL,
 album_id INTEGER NOT NULL,
 FOREIGN KEY (artist_id) REFERENCES artists (artist_id),
 FOREIGN KEY (album_id) REFERENCES albuns (album_id)
 );
+
+INSERT INTO songs (song_name, artist_id, album_id)
+VALUES
+('Soul For Us', 1, 1),
+('Reflections Of Magic', 1, 1),
+('Dance With Her Own', 1, 1),
+('Troubles Of My Inner Fire', 1, 2),
+('Time Fireworks', 1, 2),
+('Magic Circus', 2, 3),
+('Honey, So Do I', 2, 3),
+('Sweetie Let\'s Go Wild', 2 ,3),
+('She Knows', 2, 3), 
+('Fantasy For Me', 3, 4),
+('Celebration Of More', 3, 4),
+('Rock His Everything', 3, 4),
+('Home Forever', 3, 4),
+('Diamond Power', 3, 4),
+('Honey', 3, 4),
+('Let\'s Be Silly', 3, 4),
+('Thang Of Thunder', 4, 5),
+('Words Of Her Life', 4, 5),
+('Without My Streets', 4, 5);
 
 CREATE TABLE reproduction_history (
 user_id INTEGER NOT NULL,
@@ -47,6 +94,19 @@ FOREIGN KEY (user_id) REFERENCES users (user_id),
 FOREIGN KEY (song_id) REFERENCES songs (song_id)
 );
 
+INSERT INTO reproduction_history (user_id, song_id)
+VALUES (1, 1), (1, 6), (1, 14), (1, 17),
+(2, 13),
+(2, 18),
+(2, 2),
+(2, 8),
+(3, 4),
+(3, 17),
+(3, 6),
+(4, 3),
+(4, 19),
+(4, 11);
+
 CREATE TABLE user_following (
 user_id INTEGER NOT NULL,
 artist_id INTEGER NOT NULL,
@@ -54,3 +114,6 @@ CONSTRAINT PRIMARY KEY (user_id, artist_id),
 FOREIGN KEY (user_id) REFERENCES users (user_id),
 FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 );
+
+INSERT INTO user_following (user_id, artist_id)
+VALUES (1, 1),(1, 4),(1, 3),(2, 1),(2, 3),(3, 2),(3, 1),(4, 4);
