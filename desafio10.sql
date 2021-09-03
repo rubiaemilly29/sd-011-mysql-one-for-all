eb313dd818c9022dbe962505700fb02b8e299c04
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 19:35:56 -0300

    FIX: DESAFIO 10, corrigindo o escopo

--- desafio10.sql
+++ desafio10.sql
@@ -1,18 +1,12 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(id_artista INT)
+CREATE FUNCTION quantidade_musicas_no_historico(id_user INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE qtd_musicas INT;
-SET qtd_musicas = (SELECT COUNT(*)
-FROM SpotifyClone.cancoes AS c
-INNER JOIN
-SpotifyClone.albuns AS alb
-ON alb.album_id = c.album_id
-INNER JOIN
-SpotifyClone.artistas AS art
-ON art.artista_id = alb.artista_id
-WHERE id_artista =  art.artista_id);
+SELECT COUNT(*)
+FROM SpotifyClone.historico_de_reproducoes
+WHERE id_user = usuario_id INTO qtd_musicas;
 RETURN qtd_musicas;
 END $$
 
