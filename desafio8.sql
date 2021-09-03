14e69512882fd15f453b707143fbc8f9e5347e5b
rodolforezende <rodolforrcont@gmail.com>
2021-09-03 19:46:48 -0300

    Desafio 8 - Ajustado

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ BEFORE DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.historico 	WHERE usuario_id = OLD.usuario_id;
-DELETE FROM SpotifyClone.seguidores WHERE usuario_id = OLD.user_id;
+DELETE FROM SpotifyClone.seguidores WHERE usuario_id = OLD.usuario_id;
 END	
 
 DELIMITER ;
