20838f6ba11e0be5af399f5ab0b916e02085b103
Hugosomers <hugosomersla@gmail.com>
2021-09-03 16:48:08 -0300

    Fix desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,3 @@
-USE spotifyclone;
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(songs.song_id) AS cancoes, MAX(artists.artist_id) AS artistas, MAX(alb.album_id) AS albuns
 FROM spotifyclone.songs AS songs
@@ -6,5 +5,3 @@ INNER JOIN spotifyclone.artists AS artists
 ON artists.artist_id = songs.artist_id
 INNER JOIN spotifyclone.albuns AS alb
 ON alb.album_id = songs.album_id;
-
-SELECT * FROM estatisticas_musicais;
