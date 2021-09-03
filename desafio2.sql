b01890bc3b5c99e84fee04b457ac03f25d141e73
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-09-03 16:32:23 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+SELECT
+COUNT(nome_cancao) AS cancoes,
+(
+SELECT COUNT(nome_artista)
+FROM SpotifyClone.Artistas
+) AS artistas,
+(
+SELECT COUNT(nome_album)
+FROM SpotifyClone.Albuns
+) AS albuns
+FROM SpotifyClone.Cancoes;
