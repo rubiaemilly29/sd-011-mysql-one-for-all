415ac463b33733ec192c96d2a7dbde463b77a807
Vitor <vitorguima@gmail.com>
2021-09-03 18:29:37 -0300

    requisito3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+U.nome AS 'usuario',
+C.nome AS 'nome'
+FROM SpotifyClone.historico_reproducao AS HR
+LEFT JOIN SpotifyClone.usuario AS U
+ON HR.usuario_id = U.usuario_id
+LEFT JOIN SpotifyClone.cancao AS C
+ON HR.cancao_id = C.cancao_id;
