0b10c2d42b6a7ef09acf046149e6a9a5c836e4bc
Diogo Augusto <diogotrescastro@gmai.com>
2021-09-03 18:07:07 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT U.usuario AS 'usuario',
+C.cancao AS nome
+FROM SpotifyClone.historico AS H
+INNER JOIN SpotifyClone.usuarios AS U
+INNER JOIN SpotifyClone.cancoes AS C
+ON H.usuario_id = U.usuario_id AND H.cancao_id = C.cancao_id
+ORDER BY usuario, nome;
