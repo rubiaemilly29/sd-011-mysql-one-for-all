99381756f0c67732764f4f80b67d05683218a595
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-09-03 15:06:36 -0300

    Fix code climate error

--- desafio1.sql
+++ desafio1.sql
@@ -5,112 +5,111 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE plans (
-  plan_id INT PRIMARY KEY AUTO_INCREMENT,
-  plan_name VARCHAR(50) NOT NULL,
-  plan_price DOUBLE NOT NULL
+plan_id INT PRIMARY KEY AUTO_INCREMENT,
+plan_name VARCHAR(50) NOT NULL,
+plan_price DOUBLE NOT NULL
 );
 
 INSERT INTO plans (plan_name, plan_price)
 VALUES ('gratuito', 0), ('familiar', 7.99), ('universitário', 5.99);
 
 CREATE TABLE users (
-  user_id INT PRIMARY KEY AUTO_INCREMENT,
-  user_name VARCHAR(100) NOT NULL,
-  age TINYINT NOT NULL,
-  plan_id INT NOT NULL,
-  FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+user_id INT PRIMARY KEY AUTO_INCREMENT,
+user_name VARCHAR(100) NOT NULL,
+age TINYINT NOT NULL,
+plan_id INT NOT NULL,
+FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
 );
 
 INSERT INTO users (user_name, age, plan_id)
 VALUES ('Thati', 23, 1), ('Cintia', 25, 2), ('Bill', 20, 3), ('Roger', 45, 1);
 
 CREATE TABLE artists (
-  artist_id INT PRIMARY KEY AUTO_INCREMENT,
-  artist_name VARCHAR(50) NOT NULL
+artist_id INT PRIMARY KEY AUTO_INCREMENT,
+artist_name VARCHAR(50) NOT NULL
 );
 
 INSERT INTO artists (artist_name)
 VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
 
 CREATE TABLE albuns (
-  album_id INT PRIMARY KEY AUTO_INCREMENT,
-  album_name VARCHAR(50) NOT NULL,
-  artist_id INT NOT NULL,
-  FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+album_id INT PRIMARY KEY AUTO_INCREMENT,
+album_name VARCHAR(50) NOT NULL,
+artist_id INT NOT NULL,
+FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 );
 
 INSERT INTO albuns (album_name, artist_id)
 VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
 
 CREATE TABLE songs (
-  song_id INT PRIMARY KEY AUTO_INCREMENT,
-  song_name VARCHAR(100) NOT NULL,
-  album_id INT NOT NULL,
-  FOREIGN KEY (album_id) REFERENCES albuns(album_id)
+song_id INT PRIMARY KEY AUTO_INCREMENT,
+song_name VARCHAR(100) NOT NULL,
+album_id INT NOT NULL,
+FOREIGN KEY (album_id) REFERENCES albuns(album_id)
 );
 
 INSERT INTO songs (song_name, album_id)
 VALUES
-  ('Soul For Us', 1),
-  ('Reflections Of Magic', 1),
-  ('Dance With Her Own', 1),
-  ('Troubles Of My Inner Fire', 2),
-  ('Time Fireworks', 2),
-  ('Magic Circus', 3),
-  ('Honey, So Do I', 3),
-  ('Sweetie, Let\'s Go Wild', 3),
-  ('She Knows', 3),
-  ('Fantasy For Me', 4),
-  ('Celebration Of More', 4),
-  ('Rock His Everything' ,4),
-  ('Home Forever', 4),
-  ('Diamond Power', 4),
-  ('Honey, Let\'s Be Silly', 4),
-  ('Thang Of Thunder', 5),
-  ('Words Of Her Life', 5),
-  ('Without My Streets', 5);
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+('Sweetie, Let\'s Go Wild', 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything' ,4),
+('Home Forever', 4),
+('Diamond Power', 4),
+('Honey, Let\'s Be Silly', 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
 
 CREATE TABLE artists_followers (
-  artist_id INT NOT NULL,
-  user_id INT NOT NULL,
-  CONSTRAINT PRIMARY KEY (artist_id, user_id),
-  FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
+artist_id INT NOT NULL,
+user_id INT NOT NULL,
+CONSTRAINT PRIMARY KEY (artist_id, user_id),
+FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 INSERT INTO artists_followers (artist_id, user_id)
 VALUES
-  (1, 1),
-  (1, 2),
-  (1, 3),
-  (2, 3),
-  (3, 1),
-  (3, 2),
-  (4, 1),
-  (4, 4);
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 3),
+(3, 1),
+(3, 2),
+(4, 1),
+(4, 4);
   
 CREATE TABLE songs_playback_history	(
-  user_id INT NOT NULL,
-  song_id INT NOT NULL,
-  CONSTRAINT PRIMARY KEY (user_id, song_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (song_id) REFERENCES songs(song_id)
+user_id INT NOT NULL,
+song_id INT NOT NULL,
+CONSTRAINT PRIMARY KEY (user_id, song_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id),
+FOREIGN KEY (song_id) REFERENCES songs(song_id)
 );
 
 INSERT INTO songs_playback_history (user_id, song_id)
 VALUES
-  (1, 1),
-  (1, 6),
-  (1, 14),
-  (1,16),
-  (2, 13),
-  (2, 17),
-  (2, 2),
-  (2, 15),
-  (3, 4),
-  (3, 16),
-  (3, 6),
-  (4, 3),
-  (4, 18),
-  (4, 11);
-
+(1, 1),
+(1, 6),
+(1, 14),
+(1,16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
