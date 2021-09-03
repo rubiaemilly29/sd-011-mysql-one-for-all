a2b052d31ec43e6e7bd4987ac2e8cf8e20a57d20
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 16:17:50 -0300

    FEAT: Desafio 5 FIX: Desafio 1, JSON

--- desafio5.sql
+++ desafio5.sql
@@ -1,1 +1,8 @@
-SELECT * FROM SpotifyClone;
+USE `SpotifyClone`;
+CREATE VIEW top_2_hits_do_momento AS
+SELECT titulo AS `cancao`,
+(SELECT COUNT(*) FROM SpotifyClone.historico_de_reproducoes AS hr
+WHERE c.cancao_id = hr.cancao_id) AS `reproducoes`
+FROM SpotifyClone.cancoes AS c
+ORDER BY `reproducoes` DESC, `cancao`
+LIMIT 2;
