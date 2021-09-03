9d56ca8c53c7757389aa272fb56d08d111b3cd11
Vitor <vitorguima@gmail.com>
2021-09-03 18:37:58 -0300

    ajusta req 3 e finaliza 4

--- desafio3.sql
+++ desafio3.sql
@@ -6,4 +6,5 @@ FROM SpotifyClone.historico_reproducao AS HR
 LEFT JOIN SpotifyClone.usuario AS U
 ON HR.usuario_id = U.usuario_id
 LEFT JOIN SpotifyClone.cancao AS C
-ON HR.cancao_id = C.cancao_id;
+ON HR.cancao_id = C.cancao_id
+ORDER BY U.nome, C.nome;
---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+A.nome,
+COUNT(AC.usuario_id) AS seguidores
+FROM SpotifyClone.artista_seguidores AS AC
+LEFT JOIN SpotifyClone.artista AS A
+ON AC.artista_id = A.artista_id
+GROUP BY A.nome
+ORDER BY COUNT(AC.usuario_id) DESC
+LIMIT 3;
