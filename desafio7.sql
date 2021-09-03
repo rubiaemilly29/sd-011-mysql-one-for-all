335bda74fb504218bbcee10a9f5054717f257813
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-09-03 20:25:10 -0300

    desafio 7 completo

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW perfil_artista AS
+SELECT
+art.`nome` AS 'artista',
+alb.album AS 'album',
+COUNT(s_art.usuario_id) AS 'seguidores'
+FROM
+SpotifyClone.Artista AS art
+INNER JOIN
+SpotifyClone.Album AS alb ON art.id = alb.artista_id
+INNER JOIN
+SpotifyClone.Seguindo_Artista AS s_art ON art.id = s_art.artista_id
+GROUP BY `artista`, `album`
+ORDER BY `seguidores` DESC, `artista`, `album`;
