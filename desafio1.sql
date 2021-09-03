692d5d8b666cf1b09251fbdaa413916102b705e3
guinhoanjos <iago.anjos@hotmail.com>
2021-09-03 20:01:09 -0300

    Update desafio1.sql

--- desafio1.sql
+++ desafio1.sql
@@ -59,4 +59,4 @@ INSERT INTO planos(plano,valor_plano ) VALUES ('Gratuito',0),('Familiar',7.99),(
 INSERT INTO musicas(nome_musica,artista_id, album_id ) VALUES ('Soul For Us',1,1),('Reflections Of Magic',1,1),('Dance With Her Own',1,1),('Troubles Of My Inner Fire',1,2),('Time Fireworks',1,2),('Magic Circus',2,3),('Honey, So Do I',2,3),("Sweetie, Let\'s Go Wild",2,3),('She Knows',2,3),('Fantasy For Me',3,4),('Celebration Of More',3,4),('Rock His Everything',3,4),('Home Forever',3,4),('Diamond Power',3,4),("Honey, Let\'s Be Silly",3,4),("Thang Of Thunder",4,5),('Words Of Her Life',4,5),('Without My Streets',4,5);
 INSERT INTO historico(usuario_id,musica_id)VALUES (1,1),(1,6),(1,14),(1,16),(2,13),(2,17),(2,2),(2,15),(3,4),(3,16),(3,6),(4,3),(4,18),(4,11);
 INSERT INTO artistas_seguidos(usuario_id,artista_id) VALUES (1,1),(2,1),(3,1),(3,2),(1,3),(2,3),(1,4),(4,4);
-INSERT INTO albums VALUES(album_nome,artista_id) ('Envious',1),('Exuberant',1),('Hallowed Steam',2),('Incandescent',3),('Temporary Culture',4);
+INSERT INTO albums(album_nome,artista_id) VALUES ('Envious',1),('Exuberant',1),('Hallowed Steam',2),('Incandescent',3),('Temporary Culture',4);
