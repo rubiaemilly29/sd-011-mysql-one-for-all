15c4dae33b1156b0ef7b84fd843c8aacd6383cef
Filizzola <juliofilizzola@hotmail.com>
2021-09-03 15:55:53 -0300

    fixed - desafio1

--- desafio3.sql
+++ desafio3.sql
@@ -5,5 +5,3 @@ inner join SpotifyClone.usuarios as u
 inner join SpotifyClone.musicas as m
 ON h.usuario_id = u.usuario_id AND h.musica_id = m.musica_id
 ORDER BY usuario, nome;
-
-SELECT * FROM historico_reproducao_usuarios;
