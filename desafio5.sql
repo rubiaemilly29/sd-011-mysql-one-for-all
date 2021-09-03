da97f39acb982b1efa2609cd7cfd9a4a40e8ab98
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-09-03 19:33:40 -0300

    Corrige 5 realiza 6

--- desafio5.sql
+++ desafio5.sql
@@ -4,7 +4,7 @@
 #A segunda coluna deve possuir o alias "reproducoes" e exibir a quantidade de pessoas que já escutaram a canção em questão.
 #Seu resultado deve estar ordenado em ordem decrescente, baseando-se no número de reproduções. 
 #Em caso de empate, ordene os resultados pelo nome da canção em ordem alfabética. Queremos apenas o top 2 de músicas mais tocadas.
-CREATE VIEW top_2_hits AS
+CREATE VIEW top_2_hits_do_momento AS
 SELECT c.cancao AS cancao, COUNT(hm.cancao_id) AS reproducoes FROM SpotifyClone.Cancoes AS c
 INNER JOIN SpotifyClone.HistoricoMusicas AS hm
 ON c.cancao_id = hm.cancao_id
