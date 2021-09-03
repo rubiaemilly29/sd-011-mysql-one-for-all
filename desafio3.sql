019d8f44639eddab4cb3b9ac9b557eef45a9a54d
Marco Galindo <marco1408@gmail.com>
2021-09-03 17:27:13 -0300

    corrigindo desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,9 @@
 create view historico_reproducao_usuarios as
 select t1.usuario as `usuario`,
-t3.musica as `musica`
+t3.musica as `nome`
 from SpotifyClone.usuario as t1
 inner join SpotifyClone.historico_de_reproducoes as t2
 on t1.usuario_id = t2.usuario_id
 inner join SpotifyClone.musica as t3
 on t3.musica_id = t2.musica_id
-order by `usuario`, `musica`;
+order by `usuario`, `nome`;
