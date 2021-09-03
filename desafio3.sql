4bb76fd2225cba6a234a5fd51830450d552be9ea
Filizzola <juliofilizzola@hotmail.com>
2021-09-03 15:37:16 -0300

    desafio3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT 	u.usuario as usuario, m.music_nome AS musica FROM
+SpotifyClone.Historico as h 
+inner join SpotifyClone.usuarios as u
+inner join SpotifyClone.musicas as m
+ON h.usuario_id = u.usuario_id AND h.musica_id = m.musica_id
+ORDER BY usuario, musica;
+
+SELECT * FROM historico_reproducao_usuarios;
