26f112bb6c6fb6a3e5543edc1a495566517a42f8
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-09-03 15:10:54 -0300

    Requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,11 @@
--- CREATE TRIGGER trigger_movie_delete
---     BEFORE DELETE ON movies
---     FOR EACH ROW
--- BEGIN
---     INSERT INTO movies_logs(movie_id, executed_action, log_date)
---     VALUES(OLD.movie_id, 'DELETE', NOW());
--- END $$
--- DELIMITER ;
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON users
+    FOR EACH ROW
+BEGIN
+    DELETE FROM reproduction_history WHERE user_id = OLD.user_id;
+    DELETE FROM followup WHERE user_id = OLD.user_id;
+END $$
+DELIMITER ;
