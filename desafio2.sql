8e66216d9660bfaf15e788da6abe1693dd9d523e
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-09-03 16:39:53 -0300

    Requisito 2 corrigido

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,11 @@
 CREATE VIEW estatisticas_musicais AS
     SELECT
-        COUNT(DISTINCT s.song_id) cancoes,
-        COUNT(DISTINCT a.artist_id) artistas,
-        COUNT(DISTINCT ab.album_id) albuns
+        COUNT(DISTINCT s.song_id) AS cancoes,
+        COUNT(DISTINCT a.artist_id) AS artistas,
+        COUNT(DISTINCT ab.album_id) AS albuns
     FROM
-        songs s
+         SpotifyClone.songs s
             INNER JOIN
-        album ab ON s.album_id = ab.album_id
+         SpotifyClone.album ab ON s.album_id = ab.album_id
             INNER JOIN
-        artist a ON ab.artist_id = a.artist_id;
+         SpotifyClone.artist a ON ab.artist_id = a.artist_id;
