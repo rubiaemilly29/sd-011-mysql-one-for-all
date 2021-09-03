876130ed012cac664ebdde98e372566706783d73
LeoFuna <leo.funa@gmail.com>
2021-09-03 11:36:09 -0300

    desafio 8 e 9

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE PROCEDURE SpotifyClone.albuns_do_artista(IN nomeDoArtista VARCHAR(60))
+BEGIN
+	SELECT artista, album
+    FROM SpotifyClone.artistas AR
+    INNER JOIN SpotifyClone.albuns AL ON AL.artista_id = AR.artista_id
+    WHERE nomeDoArtista = AR.artista;
+END $$
+
+DELIMITER ;
