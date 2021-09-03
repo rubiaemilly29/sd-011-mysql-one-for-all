54800d112f28017de3a985fa5ac4645629a7963a
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 18:03:11 -0300

    FEAT: Desafio 9 FIX: Climate Desafio 8

--- desafio9.sql
+++ desafio9.sql
@@ -1,1 +1,16 @@
-SELECT * FROM SpotifyClone;
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(nome_artista VARCHAR(100))
+BEGIN
+SELECT nome AS `artista`,
+titulo AS `album`
+FROM
+SpotifyClone.artistas AS art
+INNER JOIN
+SpotifyClone.albuns AS alb
+ON art.artista_id = alb.artista_id
+WHERE art.nome = nome_artista
+ORDER BY `album`;
+END $$
+
+DELIMITER ;
