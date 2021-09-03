e4466b498bedbc97f6c19cf1e6a5b533f3cd07db
Pedro Piquini <piquini.pedro@gmail.com>
2021-09-03 18:33:05 -0300

    correçao desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,4 @@
-SELECT * FROM SpotifyClone.cancoes;
+CREATE VIEW estatisticas_musicais AS
+SELECT (SELECT COUNT(*) FROM SpotifyClone.cancoes) AS 'cancoes',
+(SELECT COUNT(*) FROM SpotifyClone.artistas) AS 'artistas',
+(SELECT COUNT(*) FROM SpotifyClone.albuns) AS 'albuns';
