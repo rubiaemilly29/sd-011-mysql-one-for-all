894b372cc188247d860e958ea84545a83042380c
Arthur Hermann <arthurhermannc@gmail.com>
2021-09-03 15:39:43 -0300

    r5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT m.nome AS cancao, COUNT(*) AS reproducoes FROM SpotifyClone.musicas AS m
+INNER JOIN SpotifyClone.historico_de_reproducoes AS hr
+ON hr.musica_id = m.musica_id
+GROUP BY hr.musica_id
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
