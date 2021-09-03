88e0f9f456e913ff8cefabf841f4313adcccea4c
benlopes <rubenssouss@hotmail.com>
2021-09-03 17:53:48 -0300

    minor correction 2

--- desafio8.sql
+++ desafio8.sql
@@ -9,6 +9,3 @@ DELETE FROM SpotifyClone.seguindo S
 WHERE S.usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
-
---  SequelizeDatabaseError: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'RP WHERE RP.usuario_id = OLD.usuario_id;
---     DELETE FROM SpotifyClone.seguindo S WHE' at line 2
