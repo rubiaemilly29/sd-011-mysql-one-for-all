955177251d39897cf9e0ac6550d415c6956e0298
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-09-03 16:20:04 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+CREATE VIEW top_3_artistas AS
+SELECT 
+A.nome_artista AS artista,
+COUNT(DISTINCT U.usuario_id) AS seguidores
+FROM
+artista AS A
+INNER JOIN
+seguidores AS S ON A.artista_id = S.artista_id
+INNER JOIN
+usuario AS U ON S.usuario_id = U.usuario_id
+GROUP BY A.nome_artista
+ORDER BY seguidores DESC, artista ASC;
