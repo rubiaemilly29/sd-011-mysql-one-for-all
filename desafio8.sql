f139d85f44535e2a35fa83b56afa899ecc20ca14
KaioHPs <kaiohpsalternativo@gmail.com>
2021-09-03 20:04:15 -0300

    Correçoes

--- desafio8.sql
+++ desafio8.sql
@@ -4,10 +4,10 @@ CREATE TRIGGER SpotifyClone.trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.play_history AS ph
-WHERE OLD.user_id = ph.user_id;
-DELETE FROM SpotifyClone.user_follows_artist AS ufa
-WHERE OLD.user_id = ufa.user_id;
+DELETE FROM SpotifyClone.play_history
+WHERE OLD.user_id = user_id;
+DELETE FROM SpotifyClone.user_follows_artist
+WHERE OLD.user_id = user_id;
 END $$
 
 DELIMITER ;
