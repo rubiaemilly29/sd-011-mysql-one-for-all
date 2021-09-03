c9c78d104056a389fca4cd4b2064e468b66fefbd
PH Assis <pieit@live.com>
2021-09-03 16:39:19 -0300

    Challenge 8 ready

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,10 @@
+USE SpotifyClone;
+DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-BEFORE DELETE ON account
-       FOR EACH ROW PRECEDES ins_sum
-       SET
-       @deposits = @deposits + IF(NEW.amount>0,NEW.amount,0),
-       @withdrawals = @withdrawals + IF(NEW.amount<0,-NEW.amount,0);
+BEFORE DELETE ON SpotifyClone.USERS
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.LISTENING_HISTORY WHERE user_id = old.user_id;
+DELETE FROM SpotifyClone.FOLLOWING_ARTISTS WHERE user_id = old.user_id;
+END $$
+DELIMITER $$;
