7f35b0e89bc25ca77091c4b0b51c9fff6f92ec17
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-03 17:54:40 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,13 @@
+CREATE VIEW historico_reproducao_usuarios AS
+  SELECT (
+    SELECT user.nome
+    FROM SpotifyClone.usuarios AS user
+    WHERE hist.user_id = user.user_id
+  ) AS `usuario`,
+  (
+    SELECT c.nome
+    FROM SpotifyClone.cancoes AS c
+    WHERE hist.cancao_id = c.cancao_id
+  ) AS `nome`
+  FROM SpotifyClone.historico_de_reproducoes AS hist
+  ORDER BY `usuario`, `nome`;
