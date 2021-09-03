72938a1478c3739b2c7ae150daba07281d668af6
guinhoanjos <iago.anjos@hotmail.com>
2021-09-03 19:27:17 -0300

    outra restruturacao

--- desafio1.sql
+++ desafio1.sql
@@ -11,15 +11,11 @@ CREATE TABLE albums (
   FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) ENGINE=InnoDB;
 
-INSERT INTO albums VALUES (1,'Envious',1),(2,'Exuberant',1),(3,'Hallowed Steam',2),(4,'Incandescent',3),(5,'Temporary Culture',4);
-
 CREATE TABLE artistas (
   artista_id int NOT NULL AUTO_INCREMENT,
   artista_nome varchar(100) NOT NULL,
   PRIMARY KEY (artista_id)
 ) ENGINE=InnoDB;
-
-INSERT INTO artistas VALUES (1,'Walter Phoenix'),(2,'Peter Strong'),(3,'Lance Day'),(4,'Freedie Shannon');
 
 CREATE TABLE artistas_seguidos (
   usuario_id int NOT NULL,
@@ -29,8 +25,6 @@ CREATE TABLE artistas_seguidos (
   FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)
 ) ENGINE=InnoDB;
 
-INSERT INTO artistas_seguidos VALUES (1,1),(2,1),(3,1),(3,2),(1,3),(2,3),(1,4),(4,4);
-
 CREATE TABLE historico (
   historico_id int NOT NULL AUTO_INCREMENT,
   usuario_id int NOT NULL,
@@ -39,8 +33,6 @@ CREATE TABLE historico (
   FOREIGN KEY (usuario_id) REFERENCES usuarios (usuario_id),
   FOREIGN KEY (musica_id) REFERENCES musicas (musica_id)
 ) ENGINE=InnoDB;
-
-INSERT INTO historico VALUES (1,1,1),(2,1,6),(3,1,14),(4,1,16),(5,2,13),(6,2,17),(7,2,2),(8,2,15),(9,3,4),(10,3,16),(11,3,6),(12,4,3),(13,4,18),(14,4,11);
 
 CREATE TABLE musicas (
   musica_id int NOT NULL AUTO_INCREMENT,
@@ -52,17 +44,12 @@ CREATE TABLE musicas (
   FOREIGN KEY (album_id) REFERENCES albums (album_id)
 ) ENGINE=InnoDB;
 
-INSERT INTO musicas VALUES (1,'Soul For Us',1,1),(2,'Reflections Of Magic',1,1),(3,'Dance With Her Own',1,1),(4,'Troubles Of My Inner Fire',1,2),(5,'Time Fireworks',1,2),(6,'Magic Circus',2,3),(7,'Honey, So Do I',2,3),(8,'Sweetie, Let\'s Go Wild',2,3),(9,'She Knows',2,3),(10,'Fantasy For Me',3,4),(11,'Celebration Of More',3,4),(12,'Rock His Everything',3,4),(13,'Home Forever',3,4),(14,'Diamond Power',3,4),(15,'Honey, Let\'s Be Silly',3,4),(16,'Thang Of Thunder',4,5),(17,'Words Of Her Life',4,5),(18,'Without My Streets',4,5);
-
 CREATE TABLE planos (
   plano_id int NOT NULL AUTO_INCREMENT,
   plano varchar(100) NOT NULL,
   valor_plano double unsigned DEFAULT (0),
   PRIMARY KEY (plano_id)
 ) ENGINE=InnoDB;
-
-INSERT INTO planos VALUES (1,'Gratuito',0),(2,'Familiar',7.99),(3,'Universitário',5.99);
-
 
 CREATE TABLE usuarios (
   usuario_id int NOT NULL AUTO_INCREMENT,
@@ -74,3 +61,9 @@ CREATE TABLE usuarios (
 ) ENGINE=InnoDB;
 
 INSERT INTO usuarios VALUES (1,'Thati',23,1),(2,'Cintia',35,2),(3,'Bill',20,3),(4,'Roger',45,1);
+INSERT INTO artistas VALUES (1,'Walter Phoenix'),(2,'Peter Strong'),(3,'Lance Day'),(4,'Freedie Shannon');
+INSERT INTO planos VALUES (1,'Gratuito',0),(2,'Familiar',7.99),(3,'Universitário',5.99);
+INSERT INTO musicas VALUES (1,'Soul For Us',1,1),(2,'Reflections Of Magic',1,1),(3,'Dance With Her Own',1,1),(4,'Troubles Of My Inner Fire',1,2),(5,'Time Fireworks',1,2),(6,'Magic Circus',2,3),(7,'Honey, So Do I',2,3),(8,'Sweetie, Let\'s Go Wild',2,3),(9,'She Knows',2,3),(10,'Fantasy For Me',3,4),(11,'Celebration Of More',3,4),(12,'Rock His Everything',3,4),(13,'Home Forever',3,4),(14,'Diamond Power',3,4),(15,'Honey, Let\'s Be Silly',3,4),(16,'Thang Of Thunder',4,5),(17,'Words Of Her Life',4,5),(18,'Without My Streets',4,5);
+INSERT INTO historico VALUES (1,1,1),(2,1,6),(3,1,14),(4,1,16),(5,2,13),(6,2,17),(7,2,2),(8,2,15),(9,3,4),(10,3,16),(11,3,6),(12,4,3),(13,4,18),(14,4,11);
+INSERT INTO artistas_seguidos VALUES (1,1),(2,1),(3,1),(3,2),(1,3),(2,3),(1,4),(4,4);
+INSERT INTO albums VALUES (1,'Envious',1),(2,'Exuberant',1),(3,'Hallowed Steam',2),(4,'Incandescent',3),(5,'Temporary Culture',4);
