b3cb028b38480c6bf3dfecabcf220249403b3d2e
KaioHPs <kaiohpsalternativo@gmail.com>
2021-09-03 19:56:42 -0300

    Correçoes

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,13 @@
 CREATE VIEW SpotifyClone.faturamento_atual AS
 SELECT
 (SELECT FORMAT(subs_price, 2) FROM SpotifyClone.subscription_plan ORDER BY subs_price ASC LIMIT 1) AS 'faturamento_minimo',
-(SELECT subs_price FROM SpotifyClone.subscription_plan ORDER BY subs_price DESC LIMIT 1) AS 'faturamento_maximo',
+(SELECT FORMAT(subs_price, 2) FROM SpotifyClone.subscription_plan ORDER BY subs_price DESC LIMIT 1) AS 'faturamento_maximo',
 (SELECT FORMAT(ROUND(AVG(sp.subs_price), 2), 2)
 FROM SpotifyClone.users AS u
 INNER JOIN SpotifyClone.subscription_plan AS sp
 ON  u.user_subs = sp.subs_id
 ) AS 'faturamento_medio',
-(SELECT SUM(sp.subs_price)
+(SELECT FORMAT(SUM(sp.subs_price), 2)
 FROM SpotifyClone.users AS u
 INNER JOIN SpotifyClone.subscription_plan AS sp
 ON  u.user_subs = sp.subs_id
--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,3 @@
-USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER SpotifyClone.trigger_usuario_delete
@@ -6,9 +5,9 @@ BEFORE DELETE ON SpotifyClone.users
 FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.play_history AS ph
-WHERE ph.user_id = OLD.user_id;
+WHERE OLD.user_id = ph.user_id;
 DELETE FROM SpotifyClone.user_follows_artist AS ufa
-WHERE ufa.user_id = OLD.user_id;
+WHERE OLD.user_id = ufa.user_id;
 END $$
 
 DELIMITER ;
