73649828eb9217e038c48830fc085853a18955c5
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 18:35:36 -0300

    FEAT: Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,16 @@
-SELECT * FROM SpotifyClone;
+CREATE VIEW cancoes_premium AS
+SELECT titulo AS `nome`,
+COUNT(u.usuario_id) AS `reproducoes`
+FROM SpotifyClone.cancoes AS c
+INNER JOIN
+SpotifyClone.historico_de_reproducoes AS hr
+ON c.cancao_id = hr.cancao_id
+INNER JOIN
+SpotifyClone.usuarios AS u
+ON u.usuario_id = hr.usuario_id
+INNER JOIN
+SpotifyClone.planos AS p
+ON p.plano_id = u.plano_id
+WHERE tipo IN ('familiar', 'universitário')
+GROUP BY titulo
+ORDER BY `nome`;
