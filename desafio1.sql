7584d44ea438fcc3becc7e060cf5b78b0d52f0b3
Arthur Hermann <arthurhermannc@gmail.com>
2021-09-03 15:54:46 -0300

    fix music name

--- desafio1.sql
+++ desafio1.sql
@@ -72,7 +72,7 @@ VALUE
 ('Dance With Her Own', 1),
 ('Troubles Of My Inner Fire', 2),
 ('Time Fireworks', 2),
-('Magic Circuss', 3),
+('Magic Circus', 3),
 ('Honey, So Do I', 3),
 ("Sweetie, Let's Go Wild", 3),
 ('She Knows', 3),
