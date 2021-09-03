97f1a2e5a8670ae259b4990072c9b264cd49ac56
PH Assis <pieit@live.com>
2021-09-03 16:02:45 -0300

    Challenge 7 ready

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+-- CREATE VIEW perfil_artistas AS 
+SELECT a.artist_name as `artista`,
+alb.album_name as `album`,
+COUNT(fa.artist_id) as `seguidores`
+FROM SpotifyClone.ALBUMS as alb
+INNER JOIN SpotifyClone.ARTISTS as a
+ON alb.artist_id = a.artist_id
+INNER JOIN SpotifyClone.FOLLOWING_ARTISTS as fa
+ON a.artist_id = fa.artist_id
+GROUP BY alb.artist_id, alb.album_name
+ORDER BY `seguidores` DESC;
