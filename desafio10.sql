952e0bb4fde3fe42d409120be0fba74527d34963
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 18:14:44 -0300

    FEAT: Desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,19 @@
-SELECT * FROM SpotifyClone;
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(id_artista INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE qtd_musicas INT;
+SELECT COUNT(*)
+FROM SpotifyClone.cancoes AS c
+INNER JOIN
+SpotifyClone.albuns AS alb
+ON alb.album_id = c.album_id
+INNER JOIN
+SpotifyClone.artistas AS art
+ON art.artista_id = alb.artista_id
+WHERE id_artista =  art.artista_id INTO qtd_musicas;
+RETURN qtd_musicas;
+END $$
+
+DELIMITER ;
