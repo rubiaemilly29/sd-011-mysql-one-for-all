890c87219b4c8f30e80ece6ee780ddd806b0806d
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-03 11:45:44 -0700

    Second requiriment done

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT 
+(
+SELECT COUNT(song_id) FROM songs
+) AS 'cancoes',
+(
+SELECT COUNT(artist_id) FROM artist
+) AS 'artistas',
+(
+SELECT COUNT(album_id) FROM album
+)AS 'albuns';
