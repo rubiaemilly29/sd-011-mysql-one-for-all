8b4a1b50fd25df9c244d85115b0faabdc5a276c9
PH Assis <pieit@live.com>
2021-09-03 16:46:02 -0300

    Challenge 9 ready

---
+++ desafio9.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN person varchar(45))
+BEGIN
+
+SELECT 
+a.artist_name as artista,
+alb.album_name as album
+FROM SpotifyClone.ALBUMS alb
+INNER JOIN SpotifyClone.ARTISTS a
+ON alb.artist_id = a.artist_id
+WHERE a.artist_name = person;
+
+END $$
+DELIMITER ;
+
