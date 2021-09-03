ba3555256e41e3d62fa581be718c6512276f64b1
murilodospacos <murilopacos@gmail.com>
2021-09-03 18:49:58 -0300

    corriindo erros espaço

--- desafio1.sql
+++ desafio1.sql
@@ -5,85 +5,85 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE plano(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(20) NOT NULL,
-    valor_plano DECIMAL(5, 2) NOT NULL
+id INT PRIMARY KEY AUTO_INCREMENT,
+tipo VARCHAR(20) NOT NULL,
+valor_plano DECIMAL(5, 2) NOT NULL
 ) engine = InnoDB;
 
 INSERT INTO plano (tipo, valor_plano)
 VALUES
-  ('gratuito', 0),
-  ('familiar', 7.99),
-  ('universitário', 5.99);
+('gratuito', 0),
+('familiar', 7.99),
+('universitário', 5.99);
 
 CREATE TABLE usuario(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    idade TINYINT NOT NULL,
-    plano_id INTEGER,
-	CONSTRAINT FOREIGN KEY (plano_id) REFERENCES plano (id)
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50) NOT NULL,
+idade TINYINT NOT NULL,
+plano_id INTEGER,
+CONSTRAINT FOREIGN KEY (plano_id) REFERENCES plano (id)
 ) engine = InnoDB;
 
 INSERT INTO usuario (nome, idade)
 VALUES
-  ('Thati', 23),
-  ('Cintia', 35),
-  ('Bill', 20 ),
-  ('Roger', 45);
+('Thati', 23),
+('Cintia', 35),
+('Bill', 20 ),
+('Roger', 45);
 
 CREATE TABLE artista(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome_artista VARCHAR(100) NOT NULL
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome_artista VARCHAR(100) NOT NULL
 ) engine = InnoDB;
 
 INSERT INTO artista (nome_artista)
 VALUES
-  ('Walter Phoenix'),
-  ('Peter Strong'),
-  ('Lance Day'),
-  ('Freedie Shannon');
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
   
 CREATE TABLE album(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome_album VARCHAR(100) NOT NULL,
-    artista_id INT NOT NULL,
-    FOREIGN KEY (artista_id) REFERENCES artista (id)
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome_album VARCHAR(100) NOT NULL,
+artista_id INT NOT NULL,
+FOREIGN KEY (artista_id) REFERENCES artista (id)
 ) engine = InnoDB;
 
 INSERT INTO album (nome_album, artista_id)
 VALUES
-	('Envious', 1),
-	('Exuberant', 1),
-	('Hallowed Steam', 2),
-	('Incandescent', 3),
-	('Temporary Culture', 4);
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
 CREATE TABLE seguindo_artistas(
-    usuario_id INT NOT NULL,
-    artista_id INT NOT NULL,
-    CONSTRAINT PRIMARY KEY(usuario_id, artista_id),
-    FOREIGN KEY (artista_id) REFERENCES artista (id),
-	FOREIGN KEY (usuario_id) REFERENCES usuario (id)
+usuario_id INT NOT NULL,
+artista_id INT NOT NULL,
+CONSTRAINT PRIMARY KEY(usuario_id, artista_id),
+FOREIGN KEY (artista_id) REFERENCES artista (id),
+FOREIGN KEY (usuario_id) REFERENCES usuario (id)
 ) engine = InnoDB;
 
 INSERT INTO seguindo_artistas (usuario_id, artista_id)
 VALUES
-	(1, 1),
-	(1, 4),
-	(1, 3),
-	(2, 1),
-	(2, 3),
-	(3, 2),
-	(3, 1),
-	(4, 4);
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
 
 CREATE TABLE cancoes(
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    nome_cancao VARCHAR(50) NOT NULL,
-    album_id INT NOT NULL,
-    artista_id INT NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES album (id),
-    FOREIGN KEY (artista_id) REFERENCES artista (id)
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome_cancao VARCHAR(50) NOT NULL,
+album_id INT NOT NULL,
+artista_id INT NOT NULL,
+FOREIGN KEY (album_id) REFERENCES album (id),
+FOREIGN KEY (artista_id) REFERENCES artista (id)
 ) engine = InnoDB;
 
 INSERT INTO cancoes(nome_cancao, album_id, artista_id)
@@ -109,26 +109,26 @@ VALUES
 
     
 CREATE TABLE historico_de_reproducoes(
-    usuario_id INT NOT NULL,
-    cancao_id INT NOT NULL,
-    CONSTRAINT PRIMARY KEY(usuario_id, cancao_id),
-    FOREIGN KEY (usuario_id) REFERENCES usuario (id),
-    FOREIGN KEY (cancao_id) REFERENCES cancoes (id)
+usuario_id INT NOT NULL,
+cancao_id INT NOT NULL,
+CONSTRAINT PRIMARY KEY(usuario_id, cancao_id),
+FOREIGN KEY (usuario_id) REFERENCES usuario (id),
+FOREIGN KEY (cancao_id) REFERENCES cancoes (id)
 ) engine = InnoDB;
 
 INSERT INTO historico_de_reproducoes (usuario_id, cancao_id)
 VALUES
-	(1,	1),
-	(1,	6),
-	(1,	14),
-	(1,	16),
-	(2,	13),
-	(2,	17),
-	(2,	2),
-	(2,	15),
-	(3,	4),
-	(3,	16),
-	(3,	6),
-	(4,	3),
-	(4,	18),
-	(4,	11);
+(1,	1),
+(1,	6),
+(1,	14),
+(1,	16),
+(2,	13),
+(2,	17),
+(2,	2),
+(2,	15),
+(3,	4),
+(3,	16),
+(3,	6),
+(4,	3),
+(4,	18),
+(4,	11);
