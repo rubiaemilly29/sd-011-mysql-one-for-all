df9d7b31cc77c52668e6ae0f78aa1d6ef7f6b5cc
PH Assis <pieit@live.com>
2021-09-03 15:12:59 -0300

    Challenge 5 ready

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+-- CREATE VIEW top_2_hits_do_momento AS
+SELECT 
+s.song_name as `cancao`,
+COUNT(lh.song_id) as `reproducoes`
+FROM SpotifyClone.LISTENING_HISTORY as lh
+INNER JOIN SpotifyClone.SONGS as s on s.song_id = lh.song_id
+GROUP BY lh.song_id
+ORDER BY `reproducoes` DESC
+LIMIT 2;  
