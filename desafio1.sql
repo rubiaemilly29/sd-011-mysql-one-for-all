b63319c0fc10625d4a07e174130a94fc9afba77a
rodolforezende <rodolforrcont@gmail.com>
2021-09-03 16:54:42 -0300

    Desafio 1 - Formatado

--- desafio1.sql
+++ desafio1.sql
@@ -55,92 +55,92 @@ FOREIGN KEY (musica_id) REFERENCES musicas(musica_id)
 
 INSERT INTO planos (plano_nome, plano_valor)
 VALUES
-  ('gratuito', 0.00),
-  ('familiar', 7.99),
-  ('univesitário', 5.99);
+('gratuito', 0.00),
+('familiar', 7.99),
+('univesitário', 5.99);
   
 SELECT * FROM planos;
 
 INSERT INTO usuarios (usuario_nome, usuario_idade, plano_id)
 VALUES
-  ('Thati', 23, 1),
-  ('Cintia', 35, 2),
-  ('Bill', 20, 3),
-  ('Roger', 45, 1);
+('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
   
 SELECT * FROM usuarios;
 
 INSERT INTO artistas (artista_nome)
 VALUES
-  ('Walter Phoenix'),
-  ('Peter Strong'),
-  ('Lance Day'),
-  ('Freedie Shannon');
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
   
 SELECT * FROM artistas;
 
 INSERT INTO albuns (album_nome, artista_id)
 VALUES
-  ('Envious', 1),
-  ('Exuberant', 1),
-  ('Hallowed Steam', 2),
-  ('Incandescent', 3),
-  ('Temporary Culture', 4);
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
   
 SELECT * FROM albuns;
 
 INSERT INTO seguidores (usuario_id, artista_id)
 VALUES
-	(1, 1),
-    (1, 4),
-    (1, 3),
-    (2, 1),
-    (2, 3),
-    (3, 2),
-    (3, 1),
-    (4, 4);
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
     
 SELECT * FROM seguidores;
 
 INSERT INTO musicas (musica_nome, album_id)
 VALUES
-	('Soul For Us', 1),
-    ('Reflections Of Magic', 1),
-    ('Dance With Her Own', 1),
-    ('Troubles Of My Inner Fire', 2),
-    ('Time Fireworks', 2),
-    ('Magic Circus', 3),
-    ('Honey, So Do I', 3),
-    ("Sweetie, Let's Go Wild", 3),
-    ('She Knows', 3),
-    ('Fantasy For Me', 4),
-    ('Celebration Of More', 4),
-    ('Rock His Everything', 4),
-    ('Home Forever', 4),
-    ('Diamond Power', 4),
-    ("Honey, Let's Be Silly", 4),
-    ('Thang Of Thunder', 5),
-    ('Words Of Her Life', 5),
-    ('Without My Streets', 5);
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
     
 SELECT * FROM musicas;
 
 INSERT INTO historico (usuario_id, musica_id)
 VALUES
-	(1, 1),
-    (1, 6),
-    (1, 14),
-    (1, 16),
-    (2, 13),
-    (2, 17),
-    (2, 2),
-    (2, 15),
-    (3, 4),
-    (3, 16),
-    (3, 6),
-    (4, 3),
-    (4, 18),
-    (4, 11);
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
     
 SELECT * FROM historico;
     
