0c903ce41145c0431c3d46f73645542ab165fef1
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-03 15:29:18 -0300

    FEAT: Desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,1 +1,15 @@
-SELECT * FROM SpotifyClone;
+USE `SpotifyClone`;
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT nome AS 'usuario',
+titulo AS 'nome'
+FROM
+SpotifyClone.usuarios AS u
+INNER JOIN
+SpotifyClone.historico_de_reproducoes AS hr
+ON
+hr.usuario_id = u.usuario_id
+INNER JOIN
+SpotifyClone.cancoes AS c
+ON
+c.cancao_id = hr.cancao_id
+ORDER BY u.nome, titulo;
