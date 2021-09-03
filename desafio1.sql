9dd3625ca0a7d0a7f53230da8ae223214b71e060
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-03 17:08:31 -0300

    Desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -3,37 +3,68 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE planos (
-  plano_id INT PRIMARY KEY,
+  plano_id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(50),
   valor DOUBLE
 ) ENGINE=InnoDb;
 
+INSERT INTO planos (nome, valor) 
+VALUES ('gratuito', 0.00),
+  ('universitário', 5.99),
+  ('familiar', 7.99);
+
 CREATE TABLE usuarios (
-  user_id INT PRIMARY KEY,
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(50),
   idade INT UNSIGNED NOT NULL,
   plano_id INT NOT NULL,
   FOREIGN KEY (plano_id) REFERENCES planos (plano_id)
 ) ENGINE=InnoDb;
 
+INSERT INTO usuarios (nome, idade, plano_id) 
+VALUES ('Thati', 21, 1),
+  ('Cintia', 35, 3),
+  ('Bill', 20, 2),
+  ('Roger', 45, 1);
+
 CREATE TABLE artistas (
-  artista_id INT PRIMARY KEY,
+  artista_id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(50)
 ) ENGINE=InnoDb;
 
+INSERT INTO artistas (nome) 
+VALUES ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+
 CREATE TABLE albuns (
-  album_id INT PRIMARY KEY,
+  album_id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(50),
   artista_id INT NOT NULL,
   FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)
 ) ENGINE=InnoDb;
 
+INSERT INTO albuns (nome, artista_id) VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
 CREATE TABLE cancoes (
-  cancao_id INT PRIMARY KEY,
+  cancao_id INT PRIMARY KEY AUTO_INCREMENT,
   nome VARCHAR(50),
   album_id INT NOT NULL,
   FOREIGN KEY (album_id) REFERENCES albuns (album_id)
 ) ENGINE=InnoDb;
+
+INSERT INTO cancoes (nome, album_id) 
+VALUES ('Soul For Us', 1), ('Reflections Of Magic', 1), ('Dance With Her Own', 1), 
+  ('Troubles Of My Inner Fire', 2), ('Time Fireworks', 2),
+  ('Magic Circus', 3), ('Honey, So Do I', 3), ("Sweetie, Let's Go Wild", 3), ('She Knows', 3),
+  ('Fantasy For Me', 4), ('Celebration Of More', 4), ('Rock His Everything', 4), ('Home Forever"', 4), ('Diamond Power', 4), ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5), ('Words Of Her Life', 5), ('Without My Streets', 5);
 
 CREATE TABLE seguindo_artistas (
   user_id INT NOT NULL,
@@ -43,6 +74,12 @@ CREATE TABLE seguindo_artistas (
   FOREIGN KEY (artista_id) REFERENCES artistas (artista_id)
 ) ENGINE=InnoDb;
 
+INSERT INTO seguindo_artistas (user_id, artista_id) 
+VALUES (1, 1), (1, 4), (1, 3),
+  (2, 1), (2, 3),
+  (3, 2), (3, 1),
+  (4, 4);
+
 CREATE TABLE historico_de_reproducoes (
   user_id INT NOT NULL,
   cancao_id INT NOT NULL,
@@ -50,3 +87,9 @@ CREATE TABLE historico_de_reproducoes (
   FOREIGN KEY (user_id) REFERENCES usuarios (user_id),
   FOREIGN KEY (cancao_id) REFERENCES cancoes (cancao_id)
 ) ENGINE=InnoDb;
+
+INSERT INTO historico_de_reproducoes (user_id, cancao_id) 
+VALUES (1, 1), (1, 6), (1, 14), (1, 16),
+  (2, 13), (2, 17), (2, 2), (2, 15),
+  (3, 4), (3, 16), (3, 6),
+  (4, 3), (4, 18), (4, 11);
