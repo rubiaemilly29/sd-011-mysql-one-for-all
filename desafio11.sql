56fcef13c2ddc1c6691e50038701522c98cc921b
Matheus Alexandre <alexandremhm@gmail.com>
2021-09-03 19:12:06 -0300

    Add 11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+CREATE VIEW cancoes_premium AS 
+SELECT a.musica AS cancao, COUNT(b.musicaId) AS reproducoes FROM musicas AS a
+INNER JOIN historico AS b
+ON a.musicaId = b.musicaId
+INNER JOIN usuarios AS c
+ON c.usuarioId = b.usuarioId
+WHERE c.planoId = 2 OR c.planoId = 3
+GROUP BY a.musicaId
+ORDER BY cancao;
