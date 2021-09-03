c8cad0ba8af0d227835da02d14b6272671c752c5
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-09-03 18:20:57 -0300

    task 1 structures

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,17 @@
--- initial commit
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE planos(
+  plano_id INT PRIMARY KEY AUTO_INCREMENT,
+  nome_plano VARCHAR(100) NOT NULL,
+  valor_plano DOUBLE NOT NULL
+) engine = InnoDB;
+
+INSERT INTO planos (nome_plano, valor_plano)
+VALUES
+('gratuito', 0.00),
+('familiar', 7.99),
+('universitário', 5.99);
