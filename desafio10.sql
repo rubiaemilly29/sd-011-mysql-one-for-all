dc74a0627b8a8f1194d7851c4caacf8497395f60
PH Assis <pieit@live.com>
2021-09-03 16:51:24 -0300

    Challenge 10 ready

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(user VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total_amount INT;
+SELECT COUNT(*) FROM SpotifyClone.LISTENING_HISTORY WHERE user_id = user INTO total_amount;
+RETURN total_amount;
+END $$
+
+DELIMITER ;
