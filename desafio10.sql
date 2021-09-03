e7744baaa8b84feb9049b4ca2b96a49a3fb2bfd7
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-09-03 15:39:51 -0300

    Fix code climate challenge 10 error

--- desafio10.sql
+++ desafio10.sql
@@ -5,12 +5,12 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(search_user_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE quantidade_musicas_no_historico INT;
-    SELECT
-      COUNT(user_id)
-	FROM songs_playback_history
-    WHERE user_id = search_user_id INTO quantidade_musicas_no_historico;
-    RETURN quantidade_musicas_no_historico;
+  DECLARE quantidade_musicas_no_historico INT;
+  SELECT
+    COUNT(user_id)
+  FROM songs_playback_history
+  WHERE user_id = search_user_id INTO quantidade_musicas_no_historico;
+  RETURN quantidade_musicas_no_historico;
 END $$
 
 DELIMITER ;
