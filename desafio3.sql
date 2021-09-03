0ca2e9350ccf2e8d488bbced66fde6930af1eaa4
Arthur Hermann <arthurhermannc@gmail.com>
2021-09-03 15:38:52 -0300

    r3

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.nome AS usuario, m.nome AS nome FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico_de_reproducoes AS hr
+ON hr.usuario_id = u.usuario_id
+INNER JOIN SpotifyClone.musicas AS m
+ON m.musica_id = hr.musica_id
+ORDER BY usuario, nome;
