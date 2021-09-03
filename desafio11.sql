df4ba853019d855d3dcd754b7209bbe7e2be229f
rodolforezende <rodolforrcont@gmail.com>
2021-09-03 20:02:08 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+CREATE VIEW cancoes_premium AS
+SELECT m.musica_nome AS nome,
+COUNT(*) AS reproducoes
+FROM spotifyclone.musicas AS m
+INNER JOIN SpotifyClone.historico AS h
+INNER JOIN SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.planos AS p
+ON h.musica_id = m.musica_id
+AND u.usuario_id = h.usuario_id
+AND u.plano_id = p.plano_id
+WHERE p.plano_id IN ('2', '3')
+GROUP BY nome
+ORDER BY nome;
