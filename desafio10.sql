3dfb7bf75811c93a7921053d194e0a91cb76faa3
LeoFuna <leo.funa@gmail.com>
2021-09-03 11:43:05 -0300

    desafio 10 com restriçoes

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantidade INT;
+SELECT COUNT(*) FROM SpotifyClone.usuario_musica
+WHERE usuario_id = id
+INTO quantidade;
+RETURN quantidade;
+END $$
+
+DELIMITER ;
--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER SpotifyClone.trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.usuario_seguindo_artista WHERE usuario_id = OLD.usuario_id;
-    DELETE FROM SpotifyClone.usuario_musica WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.usuario_seguindo_artista WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.usuario_musica WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
