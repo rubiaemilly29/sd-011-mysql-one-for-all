da05484dd4b0a91639f3bd26988987223a250de4
Prih Scapin <prih.scapin@gmail.com>
2021-09-03 18:58:12 -0300

    Desafio 1 - Code Climate ¬¬

--- desafio1.sql
+++ desafio1.sql
@@ -7,54 +7,54 @@ DEFAULT COLLATE utf8_general_ci;
 USE SpotifyClone;
 
 CREATE TABLE plans (
-	plan_id INT PRIMARY KEY AUTO_INCREMENT,
-	plan_name VARCHAR(20) NOT NULL,
-    plan_value DECIMAL(5,2) NOT NULL
- ) ENGINE = InnoDB;    
+  plan_id INT PRIMARY KEY AUTO_INCREMENT,
+  plan_name VARCHAR(20) NOT NULL,
+  plan_value DECIMAL(5,2) NOT NULL
+  ) ENGINE = InnoDB;    
  
 CREATE TABLE users (
-	user_id INT PRIMARY KEY AUTO_INCREMENT,
-	user_name VARCHAR(30) NOT NULL,
-    user_age TINYINT(2) NOT NULL,
-    plan_id INT,
-    FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_name VARCHAR(30) NOT NULL,
+  user_age TINYINT(2) NOT NULL,
+  plan_id INT,
+  FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
 ) ENGINE = InnoDB; 
 
 CREATE TABLE artists (
-	artist_id INT PRIMARY KEY AUTO_INCREMENT,
-	artist_name VARCHAR(30) NOT NULL
-) ENGINE = InnoDB; 
+  artist_id INT PRIMARY KEY AUTO_INCREMENT,
+  artist_name VARCHAR(30) NOT NULL
+  ) ENGINE = InnoDB; 
 
 CREATE TABLE albuns (
-	album_id INT PRIMARY KEY AUTO_INCREMENT,
-	album_name VARCHAR(30) NOT NULL,
-    artist_id INT,
-    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
-) ENGINE = InnoDB; 
+  album_id INT PRIMARY KEY AUTO_INCREMENT,
+  album_name VARCHAR(30) NOT NULL,
+  artist_id INT,
+  FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+  ) ENGINE = InnoDB; 
 
 CREATE TABLE songs (
-	song_id INT PRIMARY KEY AUTO_INCREMENT,
-	song_name VARCHAR(30) NOT NULL,
-    album_id INT,
-    FOREIGN KEY (album_id) REFERENCES albuns(album_id)
-) ENGINE = InnoDB; 
+  song_id INT PRIMARY KEY AUTO_INCREMENT,
+  song_name VARCHAR(30) NOT NULL,
+  album_id INT,
+  FOREIGN KEY (album_id) REFERENCES albuns(album_id)
+  ) ENGINE = InnoDB; 
 
 CREATE TABLE artist_followers (
-	artist_id INT,
-    user_id INT,
-    FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    CONSTRAINT PRIMARY KEY (artist_id, user_id)
-) ENGINE = InnoDB;
+  artist_id INT,
+  user_id INT,
+  FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  CONSTRAINT PRIMARY KEY (artist_id, user_id)
+  ) ENGINE = InnoDB;
 
 
 CREATE TABLE reproduction_history (
-	user_id INT,
-    song_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (song_id) REFERENCES songs(song_id),
-    CONSTRAINT PRIMARY KEY (user_id, song_id)
-) ENGINE = InnoDB;
+  user_id INT,
+  song_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (song_id) REFERENCES songs(song_id),
+  CONSTRAINT PRIMARY KEY (user_id, song_id)
+  ) ENGINE = InnoDB;
 
 INSERT INTO plans(plan_name, plan_value)
 VALUES
@@ -132,3 +132,4 @@ VALUES
   (4, 3),
   (4, 18),
   (4, 11);
+  
