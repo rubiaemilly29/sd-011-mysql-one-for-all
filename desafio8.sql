6ced2b3e653dbc2b013d234e3823989b5aa37567
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-09-03 18:56:31 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON SpotifyClone.Usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.Historico_Reproducoes WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.Historico_Seguidores WHERE usuario_id = OLD.usuario_id;
+END $$
+DELIMITER $$ ;
