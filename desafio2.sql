d65d8ef5942cb63834b291ff5e3266c409841441
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-09-03 16:04:55 -0300

    Desafio 2 correcao nome spotify

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(M.musica_id) AS cancoes, COUNT(DISTINCT A.artista_id) AS artistas, COUNT(DISTINCT AL.album_id) AS albuns
-FROM spotifyclone.musicas AS M
-INNER JOIN spotifyclone.albuns AS Al
+FROM SpotifyClone.musicas AS M
+INNER JOIN SpotifyClone.albuns AS Al
 ON M.album_id = AL.album_id
-INNER JOIN spotifyclone.artistas AS A
+INNER JOIN SpotifyClone.artistas AS A
 ON AL.artista_id = A.artista_id;
 SELECT * FROM estatisticas_musicais;
