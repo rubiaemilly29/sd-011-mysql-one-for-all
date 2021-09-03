dcd008ce0f44e05539a5364bd4dc5cc64a1970ed
anaclarabck <ackyotoku@gmail.com>
2021-09-03 17:57:27 -0300

    [REMOVE] requirement 10

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN artist VARCHAR(40))
+BEGIN
+SELECT ar.name AS 'artista', al.name
+FROM SpotifyClone.artists ar
+INNER JOIN SpotifyClone.albuns al
+ON ar.artist_id = al.artist_id
+WHERE ar.name = artist;
+END $$
+DELIMITER ;
