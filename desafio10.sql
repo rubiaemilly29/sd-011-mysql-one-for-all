388ae60e8c13db2af7597e2636d6179ebfc4e210
Arthur Hermann <arthurhermannc@gmail.com>
2021-09-03 15:44:35 -0300

    fix codeclimates

--- desafio10.sql
+++ desafio10.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(usuario_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
- DECLARE musicas_total INT;
+  DECLARE musicas_total INT;
 SELECT COUNT(*) AS quantidade_musicas_no_historico FROM SpotifyClone.musicas AS m
 INNER JOIN SpotifyClone.historico_de_reproducoes AS hr
 ON hr.musica_id = m.musica_id AND hr.usuario_id = usuario_id
