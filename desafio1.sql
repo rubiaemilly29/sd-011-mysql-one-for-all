ac261893717434a2849fa8cd1a974b61c1977d1d
Hamander <1hamander@gmail.com>
2021-09-03 12:34:01 -0300

    Error

--- desafio1.sql
+++ desafio1.sql
@@ -24,16 +24,16 @@ CREATE TABLE artistas(
 ) engine = InnoDB;
 
 CREATE TABLE albuns(
-album_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-album_nome VARCHAR(50),
-artista_id INT,
+    album_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    album_nome VARCHAR(50),
+    artista_id INT,
 FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) engine = InnoDB;
 
 CREATE TABLE musicas(
-musica_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-musica_nome VARCHAR(50),
-album_id INT,
+    musica_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    musica_nome VARCHAR(50),
+    album_id INT,
 FOREIGN KEY (album_id) REFERENCES albuns(album_id)
 ) engine = InnoDB;
 
