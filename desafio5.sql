9ec9c22568432b5860247871504498ccd5503651
Vitor <vitorguima@gmail.com>
2021-09-03 18:43:59 -0300

    req 5 finalizado

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT
+C.nome AS 'cancao',
+COUNT(HR.cancao_id) AS 'reproducoes'
+FROM SpotifyClone.historico_reproducao AS HR
+LEFT JOIN SpotifyClone.cancao AS C
+ON HR.cancao_id = C.cancao_id
+GROUP BY C.nome
+ORDER BY COUNT(HR.cancao_id) DESC, C.nome
+LIMIT 2;
