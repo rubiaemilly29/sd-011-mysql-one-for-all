39dc094c556225d1632c7dbffa6d38729b34ddd3
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-09-03 16:39:15 -0300

    adiciona requisito 03

---
+++ desafio3.sql
@@ -0,0 +1,13 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+Usuario.usuario,
+Musica.nome
+FROM
+SpotifyClone.Usuario_Musica UsuarioMusica
+INNER JOIN SpotifyClone.Usuario Usuario
+ON UsuarioMusica.usuario_id = Usuario.usuario_id
+INNER JOIN SpotifyClone.Musica Musica
+ON UsuarioMusica.musica_id = Musica.musica_id
+ORDER BY
+Usuario.usuario,
+Musica.nome;
