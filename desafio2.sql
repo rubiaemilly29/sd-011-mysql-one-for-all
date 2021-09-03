a4859c489da657e6513b06e909ff4aea7e9a8a9c
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-09-03 16:52:49 -0300

    Requisito 2 corrigindo climate

--- desafio2.sql
+++ desafio2.sql
@@ -4,8 +4,8 @@ CREATE VIEW estatisticas_musicais AS
         COUNT(DISTINCT a.artist_id) AS artistas,
         COUNT(DISTINCT ab.album_id) AS albuns
     FROM
-         SpotifyClone.songs s
-            INNER JOIN
-         SpotifyClone.album ab ON s.album_id = ab.album_id
-            INNER JOIN
-         SpotifyClone.artist a ON ab.artist_id = a.artist_id;
+          SpotifyClone.songs s
+             INNER JOIN
+          SpotifyClone.album ab ON s.album_id = ab.album_id
+             INNER JOIN
+          SpotifyClone.artist a ON ab.artist_id = a.artist_id;
