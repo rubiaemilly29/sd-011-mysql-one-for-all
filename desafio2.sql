b3482d0ec1dce73babedd575272d27c1ad1bdfe3
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-09-03 15:10:29 -0300

    Add challenge 2

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
+SELECT
+  COUNT(*) AS cancoes,
+  (
+    SELECT
+      COUNT(*)
+      FROM SpotifyClone.artists
+  ) AS artistas,
+  (
+  SELECT
+    COUNT(*)
+    FROM SpotifyClone.albuns
+  ) AS albuns
+FROM SpotifyClone.songs;
