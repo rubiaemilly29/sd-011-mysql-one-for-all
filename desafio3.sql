5af88daa927bfc07e6a3faaedbe76676e17c0927
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-03 15:30:07 -0300

    Add desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+
+SELECT
+(SELECT nome_usuario FROM SpotifyClone.usuarios WHERE id_usuario=t1.id_usuario) AS `usuario`,
+(SELECT nome_cancao FROM SpotifyClone.cancoes WHERE id_cancao=t1.id_cancao) AS `nome`
+FROM SpotifyClone.historico AS t1 ORDER BY `usuario`, `nome`;
