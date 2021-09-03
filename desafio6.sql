20efe6025de534254b354210096f7c2595fbd91a
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-09-03 18:49:15 -0400

    Desafio 6, feito.

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+SELECT
+MIN(p.price) AS faturamento_minimo,
+MAX(p.price) AS faturamento_maximo,
+ROUND(AVG(p.price), 2) AS faturamento_medio,
+ROUND(SUM(p.price), 2) AS faturamento_total 
+FROM SpotifyClone.User AS u
+INNER JOIN SpotifyClone.Plan AS p ON p.plan_id = u.plan_id;
