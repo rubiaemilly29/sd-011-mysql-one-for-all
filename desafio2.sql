5c77ab0e44c7aecf365cc1337650abee8ce96ef2
Vitor <vitorguima@gmail.com>
2021-09-03 18:23:21 -0300

    ajusta nome da coluna para cancoes req. 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT
-(SELECT COUNT(cancao_id) FROM SpotifyClone.cancao) AS 'cancao',
+(SELECT COUNT(cancao_id) FROM SpotifyClone.cancao) AS 'cancoes',
 (SELECT COUNT(artista_id) FROM SpotifyClone.artista) AS 'artistas',
 COUNT(album_id) AS 'albuns' 
 FROM SpotifyClone.album;
