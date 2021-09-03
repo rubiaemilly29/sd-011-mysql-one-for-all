0e83839b028e1ee9414143988f9e465019c0bbc3
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-09-03 15:17:08 -0300

    Add Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT
+COUNT(nome_cancao) AS 'cancoes',
+(SELECT COUNT(nome_artista) FROM SpotifyClone.artistas) AS 'artistas',
+(SELECT COUNT(nome_album) FROM SpotifyClone.albuns) AS 'albuns'
+FROM SpotifyClone.cancoes;
