aa7514fe31a3d8126c39b0c326992001f00424ae
Hugosomers <hugosomersla@gmail.com>
2021-09-03 19:25:47 -0300

    Fix desafio7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 CREATE VIEW perfil_artistas AS
 SELECT artist.`name` AS artista, album.`name` AS album, COUNT(ua.artist_id) AS seguidores
-FROM SpotifyClone.artists AS artist
-INNER JOIN SpotifyClone.albuns AS album
+FROM SpotifyClone.albuns AS album
+INNER JOIN SpotifyClone.artists AS artist
 ON album.artist_id = artist.artist_id
 INNER JOIN SpotifyClone.users_artists AS ua
 ON ua.artist_id = artist.artist_id
