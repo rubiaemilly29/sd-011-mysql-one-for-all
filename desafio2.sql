5a867d667b99aaef8823c8ee3e02a1e82e51da6f
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-03 21:01:10 -0300

    Fixes indentation in desafio2.sql

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 USE SpotifyClone;
 DROP VIEW IF EXISTS estatisticas_musicais;
-CREATE VIEW `estatisticas_musicais` AS
+CREATE VIEW estatisticas_musicais AS
 SELECT
 	(SELECT COUNT(song_id) AS cancoes FROM Songs),
 	(SELECT COUNT(artist_id) AS artistas FROM Artists),
