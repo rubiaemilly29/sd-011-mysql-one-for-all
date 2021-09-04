c95b4caa8c19fb79d855c87091fd9ed3ea2078ac
Daniel <danielfdoficial@gmail.com>
2021-09-04 10:03:43 -0300

    add desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT us.nome_usuario AS `usuario`,
+(SELECT msc.nome_musica FROM SpotifyClone.musicas AS msc WHERE msc.musica_id = hrep.musica_id) AS nome
+FROM SpotifyClone.usuarios AS us
+INNER JOIN SpotifyClone.historico_reproducoes AS hrep ON us.usuario_id = hrep.usuario_id
+ORDER BY `usuario`;
