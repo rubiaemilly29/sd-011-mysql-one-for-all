39b5371e80e5172140144672e0c6ad61620e1ade
OldRoge <rogeriomrjr@icloud.com>
2021-09-04 20:37:22 -0300

    Unexpected tabs

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 CREATE VIEW top_3_artistas AS
 SELECT singer.singer AS artistas,
-		COUNT(followers.user_id) AS seguidores
+        COUNT(followers.user_id) AS seguidores
 FROM singer AS singer
 INNER JOIN followers AS followers
 ON singer.singer_id = followers.singer_id
