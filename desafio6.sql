16ef1e28f6a6c9d49ff953fd8ce52d8318565fc1
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-09-04 14:24:23 -0300

    cORRIGE req. 6 - Adiciona decimal no select

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,9 @@
+-- https://pt.stackoverflow.com/questions/38455/retorno-de-casas-decimais-em-consultas-do-sqlserver
 CREATE VIEW faturamento_atual AS
-SELECT MIN(cost) AS faturamento_minimo,
-ROUND(MAX(p.cost),2) AS faturamento_maximo,
-ROUND(AVG(p.cost),2) AS faturamento_medio,
-ROUND(SUM(p.cost),2) AS faturamento_total
+SELECT CAST(MIN(cost) AS DECIMAL(10, 2)) AS faturamento_minimo,
+CAST(MAX(p.cost) AS DECIMAL(10, 2)) AS faturamento_maximo,
+CAST(AVG(p.cost) AS DECIMAL(10, 2)) AS faturamento_medio,
+CAST(SUM(p.cost) AS DECIMAL(10, 2)) AS faturamento_total
 FROM SpotifyClone.plan AS p
 INNER JOIN SpotifyClone.user AS u
 ON p.plan_id = u.plan_id;
