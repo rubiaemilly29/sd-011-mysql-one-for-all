9c5e12ea980f5d257638bea5030f26b2278c6a9a
Savio Moraes <smmsavio@hotmail.com>
2021-09-04 12:33:49 -0300

    Inicia desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+SELECT u.nome AS usuario, c.titulo AS nome 
+FROM SpotifyClone.historico_cancoes AS h
+INNER JOIN SpotifyClone.usuarios AS u ON h.usuario_id = u.usuario_id
+INNER JOIN SpotifyClone.cancoes AS c ON h.cancao_id = c.cancao_id
+ORDER BY usuario, nome;
