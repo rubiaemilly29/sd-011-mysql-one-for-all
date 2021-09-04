88fc3808a9b3a70cef460850f8088c4e557d0447
IsleneGomes <islenegomes@gmail.com>
2021-09-03 22:42:00 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(
+IN nomeartista VARCHAR(100)
+)
+BEGIN
+SELECT a.artista AS 'artista',
+al.nome AS 'album'
+FROM SpotifyClone.artista AS a
+INNER JOIN SpotifyClone.album AS al ON al.artistaId = a.artistaId
+WHERE nomeartista = a.artista;
+END $$
+
+DELIMITER ;
+
+CALL albuns_do_artista('Walter Phoenix');
