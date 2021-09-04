b9d2852f7bfa385396002823f601725c0b440294
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-09-03 21:02:02 -0300

    fix desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,1 +1,10 @@
-SELECT * FROM SpotifyClone.top_2_hits_do_momento;
+CREATE VIEW top_2_hits_do_momento AS
+SELECT s.name AS 'cancao',
+COUNT(u.user_id) as 'reproducoes'
+FROM Reproduction_history rh
+INNER JOIN Song s
+INNER JOIN User u
+ON rh.user_id = u.user_id AND rh.song_id = s.song_id
+GROUP BY `cancao`
+ORDER BY `reproducoes` DESC, `cancao`
+LIMIT 2;
