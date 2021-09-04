9ef9bd98f38df3a4c60c06c414a5beecc983d6bb
Daniel <danielfdoficial@gmail.com>
2021-09-04 17:26:06 -0300

    add format

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,12 @@
-CREATE VIEW faturamento_atual AS
+CREATE VIEW faturamento_atual AS;
 SELECT
 FORMAT(MIN(pl.valor_plano), 2) AS `faturamento_minimo`,
 
-MAX(pl.valor_plano) AS `faturamento_maximo`,
+FORMAT(MAX(pl.valor_plano), 2) AS `faturamento_maximo`,
 
 FORMAT(ROUND(AVG(pl.valor_plano), 2), 2) AS `faturamento_medio`,
 
-SUM(pl.valor_plano) AS `faturamento_total`
+FORMAT(SUM(pl.valor_plano), 2) AS `faturamento_total`
 
 FROM SpotifyClone.planos AS pl
 
