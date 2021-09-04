f9d92c16f146b5ad2578e19906eb348cde48210b
Savio Moraes <smmsavio@hotmail.com>
2021-09-04 13:23:48 -0300

    padroniza com aspas duplas as strings de cancoes para utilizar aspas simples como apóstrofo

--- desafio1.sql
+++ desafio1.sql
@@ -69,24 +69,24 @@ FOREIGN KEY (album_id) REFERENCES albuns (album_id)
 
 INSERT INTO cancoes (titulo, artista_id, album_id)
 VALUES
-('Soul For Us', 1, 1),
-('Reflections Of Magic', 1, 1),
-('Dance With Her Own', 1, 1),
-('Troubles Of My Inner Fire', 1, 2),
-('Time Fireworks', 1, 2),
-('Magic Circus', 2, 3),
-('Honey, So do I', 2, 3),
+("Soul For Us", 1, 1),
+("Reflections Of Magic", 1, 1),
+("Dance With Her Own", 1, 1),
+("Troubles Of My Inner Fire", 1, 2),
+("Time Fireworks", 1, 2),
+("Magic Circus", 2, 3),
+("Honey, So do I", 2, 3),
 ("Sweetie, Let's Go Wild", 2, 3),
-('She Knows', 2, 3),
-('Fantasy For Me', 3, 4),
-('Celebration Of More', 3, 4),
-('Rock His Everything', 3, 4),
-('Home Forever', 3, 4),
-('Diamond Power', 3, 4),
+("She Knows", 2, 3),
+("Fantasy For Me", 3, 4),
+("Celebration Of More", 3, 4),
+("Rock His Everything", 3, 4),
+("Home Forever", 3, 4),
+("Diamond Power", 3, 4),
 ("Honey, Let's Be Silly", 3, 4),
-('Thang Of Thunder', 4, 5),
-('Words Of Her Life', 4, 5),
-('Without My Streets', 4, 5);
+("Thang Of Thunder", 4, 5),
+("Words Of Her Life", 4, 5),
+("Without My Streets", 4, 5);
 
 CREATE TABLE seguindo(
 usuario_id INT NOT NULL,
