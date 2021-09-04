eaa9a4249be8479b0cb1648da7283d7e36a211a0
Thiagofox <thiagofox00@gmail.com>
2021-09-04 16:15:44 -0300

    Desafio11.sql envio 1

---
+++ desafio11.sql
@@ -0,0 +1,19 @@
+CREATE VIEW SpotifyClone.cancoes_premium AS
+    SELECT 
+        mus.nome_musica AS nome, 
+        COUNT(*) AS reproducoes
+    FROM
+        SpotifyClone.Historico AS hist
+            INNER JOIN
+        SpotifyClone.Musica AS mus ON hist.musica_id = mus.musica_id
+            INNER JOIN
+        SpotifyClone.Usuario AS usr ON hist.usuario_id = usr.usuario_id
+            INNER JOIN
+        SpotifyClone.Plano AS pln ON usr.plano_id = pln.plano_id
+    WHERE
+        pln.nome_plano IN ('familiar' , 'universitário')
+    GROUP BY mus.nome_musica
+    ORDER BY nome;
+
+    DROP view SpotifyClone.cancoes_premium;
+    
