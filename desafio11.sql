21cb2dccec1172707855fc4354bb76bf5992911d
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-04 01:01:41 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+CREATE VIEW cancoes_premium AS 
+  SELECT (
+    SELECT cancao_nome
+    FROM SpotifyClone.cancoes
+    WHERE cancao_id = hist.cancao_id
+  ) AS `nome`,
+  COUNT(user_id) AS `reproducoes`
+  FROM SpotifyClone.historico_de_reproducoes AS hist
+  WHERE hist.user_id IN (
+    SELECT user_id
+    FROM SpotifyClone.usuarios
+    WHERE plano_id != 1
+  )
+  GROUP BY `nome`;
