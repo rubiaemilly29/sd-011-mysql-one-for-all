c6bcbb2cd8c797e6ea8765636efd3a72e636c438
Renan <renan.vamo@gmail.com>
2021-09-04 16:34:55 -0300

    adiciona o req 10

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION quantidade_musicas_no_historico(username VARCHAR(20))
+RETURNS VARCHAR(50) READS SQL DATA
+BEGIN
+DECLARE contagem INT;
+SELECT COUNT(hr.usuario_id)
+FROM historico_reproducoes hr
+INNER JOIN usuarios u
+ON u.usuario_id = hr.usuario_id
+WHERE username = u.usuario
+INTO contagem;
+RETURN contagem;
+END //
+DELIMITER ;
--- desafio9.sql
+++ desafio9.sql
@@ -1,11 +1,11 @@
 DELIMITER //
 CREATE PROCEDURE albuns_do_artista(IN nomeArtista VARCHAR(20))
 BEGIN
-	SELECT a.artista, ab.album
-  FROM artistas a
-  INNER JOIN albuns ab
-  ON a.artista_id = ab.artista_id
-  WHERE nomeArtista = a.artista
-  ORDER BY ab.album;
+SELECT a.artista, ab.album
+FROM artistas a
+INNER JOIN albuns ab
+ON a.artista_id = ab.artista_id
+WHERE nomeArtista = a.artista
+ORDER BY ab.album;
 END //
 DELIMITER ;
