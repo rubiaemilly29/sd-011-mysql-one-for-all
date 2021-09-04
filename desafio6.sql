bd97034dfdaff057585fa9b34cadc7535f978318
Jean Esteves <jean.esteves2001@gmail.com>
2021-09-04 12:24:52 -0300

    fix: desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,5 +4,5 @@ ROUND(MIN(p.preco), 2) AS faturamento_minimo,
 ROUND(MAX(p.preco), 2) AS faturamento_maximo,
 ROUND(AVG(p.preco), 2) AS faturamento_medio,
 ROUND(SUM(p.preco), 2) AS faturamento_total
-FROM spotifyclone.usuarios AS u
-INNER JOIN spotifyclone.planos AS p ON p.plano_id = u.plano_id;
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.planos AS p ON p.plano_id = u.plano_id;
