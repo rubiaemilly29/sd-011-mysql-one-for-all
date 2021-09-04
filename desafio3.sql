a9111286152e3ed6e2a4b0657d3b284ce4c23243
Tulio Sploradori <tuliospl@gmail.com>
2021-09-04 10:27:34 -0300

    Req 3

---
+++ desafio3.sql
@@ -0,0 +1,14 @@
+CREATE VIEW
+historico_reproducao_usuarios
+AS
+SELECT
+t1.name_user AS 'usuario',
+t3.name_song AS 'nome'
+FROM SpotifyClone.user AS t1
+INNER JOIN SpotifyClone.musics_history AS t2
+ON t1.user_id = t2.user_id
+INNER JOIN SpotifyClone.songs AS t3
+ON t2.song_id = t3.song_id
+ORDER BY
+t1.name_user,
+t3.name_song;
