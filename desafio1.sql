d344d716f4736bacb25ffea38f932574534d2e70
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-03 21:41:15 -0300

    ajuste desafio1 e json

--- desafio1.sql
+++ desafio1.sql
@@ -4,6 +4,11 @@ CREATE DATABASE SpotifyClone;
 
 USE SpotifyClone;
 
+CREATE TABLE SpotifyClone.artistas (
+artista_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+artista VARCHAR(45) NOT NULL
+) ENGINE = InnoDB;
+
 CREATE TABLE SpotifyClone.planos (
 plano_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 plano VARCHAR(25) NOT NULL,
@@ -18,6 +23,19 @@ plano_id SMALLINT NOT NULL,
 FOREIGN KEY (plano_id) REFERENCES SpotifyClone.planos(plano_id)
 ) ENGINE = InnoDB;
 
+CREATE TABLE SpotifyClone.albuns (
+album_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+album VARCHAR(45) NOT NULL,
+artista_id SMALLINT NOT NULL,
+FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas(artista_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE SpotifyClone.cancoes (
+cancoes_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+cancoes VARCHAR(45) NOT NULL,
+artista_id SMALLINT NOT NULL,
+FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas(artista_id) 
+) ENGINE = InnoDB;
 
 CREATE TABLE SpotifyClone.historico_reproducoes (
 historico_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -31,25 +49,6 @@ seguindo_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 seguindo_artistas VARCHAR(100),
 usuario_id SMALLINT NOT NULL,
 FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios(usuario_id)
-) ENGINE = InnoDB;
-
-CREATE TABLE SpotifyClone.artistas (
-artista_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-artista VARCHAR(45) NOT NULL
-) ENGINE = InnoDB;
-
-CREATE TABLE SpotifyClone.albuns (
-album_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-album VARCHAR(45) NOT NULL,
-artista_id SMALLINT NOT NULL,
-FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas(artista_id)
-) ENGINE = InnoDB;
-
-CREATE TABLE SpotifyClone.cancoes (
-cancoes_id SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-cancoes VARCHAR(45) NOT NULL,
-artista_id SMALLINT NOT NULL,
-FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas(artista_id) 
 ) ENGINE = InnoDB;
 
 INSERT INTO SpotifyClone.planos (plano, valor_plano)
