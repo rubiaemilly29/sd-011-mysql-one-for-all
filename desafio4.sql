0f36b76b7979f6089cdcc2eaf0195c258af3f23c
marlon307 <marlon-ramosb@hotmail.com>
2021-09-04 10:40:30 -0300

    Desafio 4 update

--- desafio4.sql
+++ desafio4.sql
@@ -1,3 +1,4 @@
+CREATE VIEW top_3_artistas AS
 SELECT A.c_artist AS artista,
 COUNT(U.c_user) AS seguidores
 FROM t_artists AS A
@@ -6,4 +7,5 @@ ON A.id_artist = F.fk_id_artist
 INNER JOIN t_users AS U
 ON F.fk_id_user = U.id_user
 GROUP BY A.c_artist
-ORDER BY seguidores DESC, artista;
+ORDER BY seguidores DESC, artista
+LIMIT 3;
