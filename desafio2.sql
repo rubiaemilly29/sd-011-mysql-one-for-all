135da64409a172422c18da6b6e736075a25c74e5
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-03 21:16:32 -0300

    Fixes alias bug in desafio2.sql

--- desafio2.sql
+++ desafio2.sql
@@ -2,6 +2,6 @@ USE SpotifyClone;
 DROP VIEW IF EXISTS estatisticas_musicais;
 CREATE VIEW estatisticas_musicais AS
 SELECT
-(SELECT COUNT(song_id) AS cancoes FROM Songs),
-(SELECT COUNT(artist_id) AS artistas FROM Artists),
-(SELECT COUNT(album_id) AS albuns FROM Albums);
+(SELECT COUNT(song_id) FROM Songs) AS cancoes,
+(SELECT COUNT(artist_id) FROM Artists) AS artistas,
+(SELECT COUNT(album_id) FROM Albums) AS albuns;
