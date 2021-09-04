2ba7ff0debf02f35e345652b5b5feba8a50fab7e
Savio Moraes <smmsavio@hotmail.com>
2021-09-04 12:49:05 -0300

    Cria a view historico_reproducao_usuarios

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,4 @@
+CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.nome AS usuario, c.titulo AS nome 
 FROM SpotifyClone.historico_cancoes AS h
 INNER JOIN SpotifyClone.usuarios AS u ON h.usuario_id = u.usuario_id
