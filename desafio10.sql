8a8684a5bac7d1536c30058b556bb0d910e5a4bc
IsleneGomes <islenegomes@gmail.com>
2021-09-03 23:23:36 -0300

    desafio 10 corrigindo

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,13 @@
-USE SpotifyClone;
 DELIMITER $$
 CREATE FUNCTION quantidade_de_musicas_no_historico(usuarioId INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE quantidade_musicas_no_historico INT;
-SELECT COUNT(*)
-FROM SpotifyClone.historico
-WHERE SpotifyClone.historico.usuarioId = usuarioId INTO quantidade_musicas_no_historico;
-RETURN quantidade_musicas_no_historico;
+DECLARE quantidade_total INT;
+SELECT COUNT(h.cancoesId) AS quantidade_de_musicas_no_historico
+FROM SpotifyClone.historico AS h
+INNER JOIN SpotifyClone.usuario AS u ON u.usuarioId = h.usuarioId
+WHERE h.usuarioId = usuarioId INTO quantidade_total;
+RETURN quantidade_total;
 END $$
 DELIMITER ;
 SELECT quantidade_de_musicas_no_historico(3);
