4c13165a8f0466d28d3b54a84905bf789a1e0adb
Bruno Duarte <brunoarduarte@gmail.com>
2021-09-04 00:06:03 -0300

    Desafio 6 ajuste linter

--- desafio6.sql
+++ desafio6.sql
@@ -5,5 +5,3 @@ FORMAT(AVG(CEIL(plan_price)), 2) AS faturamento_medio,
 ROUND(SUM(plan_price), 2) AS faturamento_total
 FROM plans AS P
 INNER JOIN users AS U ON U.plan_id = P.plan_id;
-
-SELECT * FROM faturamento_atual;
