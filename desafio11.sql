5d23b66e7cc202771759b2eec6d6155c38e21fd7
marlon307 <marlon-ramosb@hotmail.com>
2021-09-04 14:26:29 -0300

    Desafio 11 update

--- desafio11.sql
+++ desafio11.sql
@@ -2,11 +2,12 @@ CREATE VIEW cancoes_premium AS
 SELECT S.c_songs AS nome,
 COUNT(*) AS reproducoes
 FROM t_songs AS S
+INNER JOIN t_h_reproductions AS H
 INNER JOIN t_users AS U
 INNER JOIN t_flat AS F
-INNER JOIN t_h_reproductions AS H
-ON H.fk_id_user = U.id_user
+ON S.id_songs = H.fk_id_songs
 AND F.id_flat = U.fk_c_flat
-AND S.id_songs = H.fk_id_songs
+AND H.fk_id_user = U.id_user
+WHERE F.id_flat IN('2', '3')
 GROUP BY nome
 ORDER BY nome ASC;
