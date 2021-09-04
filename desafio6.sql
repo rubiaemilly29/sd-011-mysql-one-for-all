45744bb2a877dcf0daffa149432c2b8105d5e7b6
Bruno Duarte <brunoarduarte@gmail.com>
2021-09-04 00:04:04 -0300

    Desafio 6 concluido

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,9 @@
 CREATE VIEW faturamento_atual AS
-SELECT MIN(plan_price) AS faturamento_minimo,
-MAX(plan_price) AS faturamento_maximo,
-ROUND(AVG(CEIL(plan_price)), 2) AS faturamento_medio,
+SELECT FORMAT(MIN(plan_price), 2) AS faturamento_minimo,
+FORMAT(MAX(plan_price), 2) AS faturamento_maximo,
+FORMAT(AVG(CEIL(plan_price)), 2) AS faturamento_medio,
 ROUND(SUM(plan_price), 2) AS faturamento_total
 FROM plans AS P
 INNER JOIN users AS U ON U.plan_id = P.plan_id;
+
+SELECT * FROM faturamento_atual;
