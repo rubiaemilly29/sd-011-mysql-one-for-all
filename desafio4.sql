a393006d550ba84e6b62a473910c8573fc94a1e6
Thiagofox <thiagofox00@gmail.com>
2021-09-04 11:24:04 -0300

    Desafio4.sql envio 1

--- desafio4.sql
+++ desafio4.sql
@@ -5,3 +5,16 @@
 -- 2. A segunda coluna deve ter o alias "seguidores" e exibir a quantidade de pessoas que estão seguindo aquela pessoa artista.
 
 -- Seu resultado deve estar ordenado em ordem decrescente, baseando-se na quantidade de seguidores. Em caso de empate, ordene os resultados pelo nome da pessoa artista em ordem alfabética.
+
+-- DROP VIEW `top_3_artistas`;
+CREATE VIEW `top_3_artistas` AS 
+    SELECT 
+        art.nome_artista as `artista`,
+        COUNT(seg.artista_id) as `seguidores`
+    FROM SpotifyClone.Usuario AS usr
+    INNER JOIN SpotifyClone.Seguidores AS seg ON usr.usuario_id = seg.usuario_id
+    INNER JOIN SpotifyClone.Artista AS art ON seg.artista_id = art.artista_id
+    GROUP BY seg.artista_id
+    ORDER BY `seguidores` DESC, `artista`
+    LIMIT 3;
+    
