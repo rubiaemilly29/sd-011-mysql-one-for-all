fbd4d96939cf406e6fd1b0d860fdfdc99c732ed8
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-04 05:07:15 -0300

    Changes database reference

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,11 @@
-USE SpotifyClone;
-CREATE VIEW `historico_reproducao_usuarios` AS
-SELECT u.name AS 'usuario', s.name AS 'nome'
-FROM History AS h
-INNER JOIN Users AS u
+CREATE VIEW SpotifyClone.historico_reproducao_usuarios AS
+SELECT
+u.name AS usuario,
+s.name AS nome
+FROM SpotifyClone.History AS h
+INNER JOIN SpotifyClone.Users AS u
 ON u.user_id = h.user_id
-INNER JOIN Songs AS s
+INNER JOIN SpotifyClone.Songs AS s
 ON s.song_id = h.song_id
 ORDER BY
 u.name ASC,
