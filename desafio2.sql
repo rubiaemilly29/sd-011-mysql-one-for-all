718d8baf9faa2e948d949bdc302a4c241868f97f
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-03 22:32:13 -0300

    Adds some tabs

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 USE SpotifyClone;
 CREATE VIEW `estatisticas_musicais` AS
-SELECT
-(SELECT COUNT(song_id) FROM Songs) AS cancoes,
-(SELECT COUNT(artist_id) FROM Artists) AS artistas,
-(SELECT COUNT(album_id) FROM Albums) AS albuns;
+	SELECT
+		(SELECT COUNT(song_id) FROM Songs) AS cancoes,
+		(SELECT COUNT(artist_id) FROM Artists) AS artistas,
+		(SELECT COUNT(album_id) FROM Albums) AS albuns;
