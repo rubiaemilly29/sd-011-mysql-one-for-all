aa3e5e1c2645d87349df5cd7f72fa3be8c0c1ea9
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-03 21:31:00 -0300

    Adds backtick to view creation

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 USE SpotifyClone;
-DROP VIEW IF EXISTS estatisticas_musicais;
-CREATE VIEW estatisticas_musicais AS
+DROP VIEW IF EXISTS estatisticas_musicais
+CREATE VIEW `estatisticas_musicais` AS
 SELECT
 (SELECT COUNT(song_id) FROM Songs) AS cancoes,
 (SELECT COUNT(artist_id) FROM Artists) AS artistas,
