44ea4e22820e5a412a850212302647ea3d69b4d9
murilodospacos <murilopacos@gmail.com>
2021-09-04 15:32:39 -0300

    requisito 3 mais espaços

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 -- CREATE VIEW historico_reproducao_usuarios AS
- SELECT u.nome AS 'usuario', c.nome_cancao AS 'nome'
+SELECT u.nome AS 'usuario', c.nome_cancao AS 'nome'
 FROM SpotifyClone.usuario u
 INNER JOIN SpotifyClone.seguindo_artistas sa ON sa.usuario_id = u.id
 INNER JOIN SpotifyClone.cancoes c ON c.artista_id = sa.artista_id
