187955758a58267a04370171c2181551bd784bd8
Savio Moraes <smmsavio@hotmail.com>
2021-09-04 20:14:37 -0300

    test 3

--- desafio7.sql
+++ desafio7.sql
@@ -3,5 +3,5 @@ SELECT ar.nome AS artista, titulo AS album, COUNT(s.artista_id) AS seguidores
 FROM SpotifyClone.artistas AS ar
 INNER JOIN SpotifyClone.albuns AS al ON ar.artista_id = al.artista_id
 INNER JOIN SpotifyClone.seguindo AS s ON ar.artista_id = s.artista_id
-GROUP BY artista, titulo
+GROUP BY titulo, artista
 ORDER BY seguidores DESC, artista ASC;
