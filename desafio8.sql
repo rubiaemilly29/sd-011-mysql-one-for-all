bc323b0263f2fc7e6da23c941ce5cbe44c24fba7
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-09-03 22:23:59 -0300

    desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,13 @@
 DELIMITER $$
 
 CREATE TRIGGER `trigger_usuario_delete`
-BEFORE  DELETE ON SpotifyClone.Usuarios
+AFTER DELETE ON SpotifyClone.Usuarios
 FOR EACH ROW
 BEGIN
-    DELETE FROM SpotifyClone.Historico AS H
-    WHERE H.usuario_id = OLD.usuario_id;
-    DELETE FROM SpotifyClone.Usuario_Artista AS U_A
-    WHERE U_A.usuario_id = OLD.usuario_id;
+    DELETE FROM SpotifyClone.Historico 
+    WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM SpotifyClone.Usuario_Artista 
+    WHERE usuario_id = OLD.usuario_id;
 END $$
 
-DELIMITER ;
+DELIMITER;
