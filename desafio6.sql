f4701f10e52788eb409b2fccfa7af56ede513502
marlon307 <marlon-ramosb@hotmail.com>
2021-09-04 11:27:22 -0300

    Desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+SELECT FORMAT(MIN(F.c_preice), 2) AS faturamento_minimo, 
+FORMAT(MAX(F.c_preice), 2) AS faturamento_maximo,
+FORMAT(AVG(F.c_preice) , 2) AS faturamento_medio,
+FORMAT(SUM(F.c_preice), 2) AS faturamento_total 
+FROM t_users AS U
+INNER JOIN t_flat AS F
+ON U.fk_c_flat = F.id_flat;
