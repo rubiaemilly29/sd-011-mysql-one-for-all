808ce1a674679d373c6d957a68e9644393d82ad4
Diego Ribeiro <dihrm2001@gmail.com>
2021-09-03 21:02:12 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+CREATE VIEW perfil_artistas AS
+  SELECT
+    art.nome AS `artista`,
+    alb.nome AS `album`,
+    (
+      SELECT COUNT(user_id)
+      FROM SpotifyClone.seguindo_artistas AS seg
+      WHERE alb.artista_id = seg.artista_id
+    ) AS `seguidores`
+  FROM SpotifyClone.albuns alb
+  INNER JOIN SpotifyClone.artistas AS art
+  ON alb.artista_id = art.artista_id
+  ORDER BY `seguidores` DESC, `artista`, `album`;
