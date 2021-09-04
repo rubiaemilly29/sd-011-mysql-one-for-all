255e56b58de26f159fdf002dbd91f37c45c6ed3e
marlon307 <marlon-ramosb@hotmail.com>
2021-09-04 13:41:47 -0300

    Desafio 10 update 2

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,13 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(actor_name VARCHAR(100))
-RETURNS VARCHAR(100) READS SQL DATA
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total INT;
 SELECT COUNT(H.fk_id_user) AS quantidade_musicas_no_historico
 FROM t_h_reproductions AS H
 INNER JOIN t_users AS U
 ON U.id_user = H.fk_id_user
-WHERE U.c_user = actor_name INTO total ;
+WHERE U.id_user = id INTO total ;
 RETURN total;
 END $$
 DELIMITER ;
