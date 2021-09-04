7aa9fca36f9c98af1350fb5aa5f92bcfb73c4c6b
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-09-04 10:22:21 -0300

    corrigindo erros de code climate

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,11 @@
 DELIMITER $$
-
 CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-    DECLARE quantidade_musicas INT;
-    SELECT COUNT(*) AS quantidade_musicas_no_historico
-    FROM spotifyclone.historico_reproducoes
-    WHERE usuario_id = user_id INTO quantidade_musicas;
-    RETURN quantidade_musicas;
+DECLARE quantidade_musicas INT;
+SELECT COUNT(*) AS quantidade_musicas_no_historico
+FROM spotifyclone.historico_reproducoes
+WHERE usuario_id = user_id INTO quantidade_musicas;
+RETURN quantidade_musicas;
 END $$
-
 DELIMITER ;
--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,9 @@
 DELIMITER $$
-
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON spotifyclone.usuarios
 FOR EACH ROW
 BEGIN
-    DELETE FROM spotifyclone.historico_reproducoes WHERE usuario_id = OLD.usuario_id;
-    DELETE FROM spotifyclone.seguindo_artistas WHERE usuario_id = OLD.usuario_id;
+DELETE FROM spotifyclone.historico_reproducoes WHERE usuario_id = OLD.usuario_id;
+DELETE FROM spotifyclone.seguindo_artistas WHERE usuario_id = OLD.usuario_id;
 END $$
-
 DELIMITER ;
