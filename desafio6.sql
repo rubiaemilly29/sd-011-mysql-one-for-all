abd4bff905137228d5b9b293d2791a48d118c1ca
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-03 18:56:23 -0700

    fixing

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,11 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-FORMAT(MIN(p.plan_price), 2) AS 'faturamento_minimo',
+ROUND(MIN(p.plan_price), 2) AS 'faturamento_minimo',
 ROUND(MAX(p.plan_price), 2) AS 'faturamento_maximo',
 ROUND(AVG(p.plan_price), 2) AS 'faturamento_medio',
 ROUND(SUM(p.plan_price), 2) AS 'faturamento_total'
 FROM plan as p;
+
+DROP VIEW faturamento_atual;
+
+SELECT * FROM faturamento_atual;
