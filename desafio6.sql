8ced433d8c8d8d6d0ef2dde7e34cefd6fc310830
Renan <renan.vamo@gmail.com>
2021-09-04 14:42:24 -0300

    refatora o ex 6 e 7

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
-SELECT MIN(p.valor) `faturamento_minimo`,
-MAX(p.valor) `faturamento_maximo`,
+SELECT ROUND(MIN(p.valor), 2) `faturamento_minimo`,
+ROUND(MAX(p.valor), 2) `faturamento_maximo`,
 ROUND(AVG(p.valor), 2) `faturamento_medio`,
-SUM(p.valor) `faturamento_total`
+ROUND(SUM(p.valor), 2) `faturamento_total`
 FROM planos p
 INNER JOIN usuarios u
 ON p.plano_id = u.plano_id;
