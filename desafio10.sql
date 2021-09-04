e62a00e4a773b56b81ad9b52c5e99646e1a17f82
Thiagofox <thiagofox00@gmail.com>
2021-09-04 15:45:08 -0300

    Desafio10.sql envio 10

--- desafio10.sql
+++ desafio10.sql
@@ -4,15 +4,15 @@
 
 DELIMITER $$
 
-CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico (new_id INT)
+CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico (user_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE qtd_musicas_historico INT;
-    SELECT COUNT(hist.musica_id) AS `quantidade_musicas_no_historico`
+    SELECT COUNT(hist.usuario_id) AS `quantidade_musicas_no_historico`
     FROM SpotifyClone.Historico AS hist 
     INNER JOIN SpotifyClone.Musica AS mus ON mus.musica_id = hist.musica_id
     INNER JOIN SpotifyClone.Usuario AS usr ON hist.usuario_id = usr.usuario_id
-    WHERE usr.usuario_id = new_id
+    WHERE usr.usuario_id = user_id
     GROUP BY hist.usuario_id
     INTO qtd_musicas_historico;
 RETURN qtd_musicas_historico;
