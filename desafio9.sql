329961234c6ee7c3bf0f9c4bb2a05309bb211824
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-09-04 00:18:30 -0400

    Corrigindo erros de code climate.

--- desafio9.sql
+++ desafio9.sql
@@ -1,11 +1,11 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN singer VARCHAR(30))
 BEGIN
-	SELECT s.first_name AS artista, a.album_name AS album
-	FROM SpotifyClone.Album AS a
-	INNER JOIN SpotifyClone.Singer AS s ON s.singer_id= a.singer_id
-	WHERE s.first_name = singer
-  ORDER BY album;
+SELECT s.first_name AS artista, a.album_name AS album
+FROM SpotifyClone.Album AS a
+INNER JOIN SpotifyClone.Singer AS s ON s.singer_id= a.singer_id
+WHERE s.first_name = singer
+ORDER BY album;
 END $$
 
 DELIMITER ;
