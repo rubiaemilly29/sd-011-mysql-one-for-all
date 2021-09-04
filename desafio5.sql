2f4879676d9ab251d90013f8b1c9a35a696d77df
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-09-04 13:38:50 -0300

    Arrisca req. 5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT music_name AS cancao,
+COUNT(*) AS reproducoes
+FROM SpotifyClone.music AS m
+INNER JOIN SpotifyClone.reproduction_history AS r
+ON m.music_id = r.music_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
