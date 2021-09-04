00318b0b33950906c1b9e1b02a26a5de09fe6f33
Thiagofox <thiagofox00@gmail.com>
2021-09-04 12:58:35 -0300

    Desafio8.sql envio 2

--- desafio8.sql
+++ desafio8.sql
@@ -13,7 +13,7 @@ BEFORE DELETE ON SpotifyClone.Usuario
 FOR EACH ROW
 BEGIN
     DELETE FROM SpotifyClone.Historico WHERE usuario_id = OLD.usuario_id;
-    DELETE FROM SpotifyClone.Seguidores WHERE usuario_id = OLD.usuario_id;
+        DELETE FROM SpotifyClone.Seguidores WHERE usuario_id = OLD.usuario_id;
 END $$
 
 DELIMITER ;
