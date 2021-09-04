5a741e8fa0f119941ad809822ba76f530a2a922c
Diego <dihrm2001@gmail.com>
2021-09-03 23:48:57 -0300

    Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista (IN nome_do_artista VARCHAR(45))
+BEGIN
+  SELECT nome_do_artista AS `artista`,
+  album_nome AS `album`
+  FROM albuns
+  WHERE  artista_id IN (
+    SELECT artista_id
+    FROM artistas
+    WHERE artista_nome = nome_do_artista
+  )
+  ORDER BY `album`;
+END $
+DELIMITER ;
