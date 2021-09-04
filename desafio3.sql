46b3dbf6c7cecd67212f6bce9a83477426237665
OldRoge <rogeriomrjr@icloud.com>
2021-09-04 16:34:19 -0300

    Fixing requirement 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,12 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT user_name AS usuario,
-        songs.songs AS nome
+SELECT songs.songs AS nome,
+        user_name AS usuario
 FROM user AS user
 INNER JOIN reproduction_history AS reproduction_history
 ON reproduction_history.user_id = user.user_id
 INNER JOIN songs AS songs
 ON songs.songs_id = reproduction_history.songs_id
 ORDER BY `usuario`, `nome`;
+
+drop view historico_reproducao_usuarios;
+select * from historico_reproducao_usuarios;
