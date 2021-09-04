4aaf1c0ed075935afd8f99e87e73883de385e720
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-09-03 22:00:56 -0300

    correcao 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,7 +3,7 @@
 #A primeira coluna deve exibir o nome da canção, com o alias "nome";
 #A segunda coluna deve exibir a quantidade de pessoas que já escutaram aquela canção, com o alias "reproducoes";
 #Seus resultados devem estar agrupados pelo nome da canção e ordenados em ordem alfabética.
-USE SpotifyClone;
+
 CREATE VIEW cancoes_premium AS
 SELECT c.cancao AS nome, COUNT(hm.cancao_id) AS reproducoes FROM SpotifyClone.Cancoes AS c
 INNER JOIN SpotifyClone.HistoricoMusicas AS hm
