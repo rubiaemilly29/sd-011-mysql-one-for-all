dffddf0adfcfeee5bcbc11ec9d937ae1ed3e33ac
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-09-04 15:41:33 -0300

    remove tab

--- desafio1.sql
+++ desafio1.sql
@@ -11,7 +11,7 @@ CREATE TABLE Planos(
 ) engine = InnoDB;
 
 CREATE TABLE Artistas(
-	artista_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+artista_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   artista VARCHAR(50) NOT NULL
 ) engine = InnoDB;
 
