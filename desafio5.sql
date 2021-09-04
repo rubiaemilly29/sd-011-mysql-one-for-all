21981f6adc31677a28992ff924f1da44230ff268
Daniel Leon <daniel.radialista@gmail.com>
2021-09-04 08:52:18 -0300

    desafio 5, pronto, rodando nos testes locais

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT c.cancao AS 'cancao',
+COUNT(h.cancao_id) AS 'reproducoes'
+FROM SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.historico AS h
+ON c.cancao_id = h.cancao_id
+GROUP BY `cancao`
+ORDER BY `reproducoes` DESC, `cancao`
+LIMIT 2;
