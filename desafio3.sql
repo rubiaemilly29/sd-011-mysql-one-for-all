88d994b87b34556e2c67c10678366edde6c0668e
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-04 04:49:59 -0300

    I don't understand what is the syntax error

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,12 @@
 USE SpotifyClone;
 CREATE VIEW `historico_reproducao_usuarios` AS
 SELECT
-u.name as usuario,
-s.name as nome
-FROM History as h
-INNER JOIN Users as u
+u.name AS 'usuario',
+s.name AS 'nome'
+FROM History AS h
+INNER JOIN Users AS u
 ON u.user_id = h.user_id
-INNER JOIN Songs as s
+INNER JOIN Songs AS s
 ON s.song_id = h.song_id
 ORDER BY
 u.name ASC,
