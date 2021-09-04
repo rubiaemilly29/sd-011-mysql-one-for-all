3abd0e37f40061fe17259f671c0ddbd2cc546f1e
marlon307 <marlon-ramosb@hotmail.com>
2021-09-04 10:36:55 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT A.c_artist AS artista,
+COUNT(U.c_user) AS seguidores
+FROM t_artists AS A
+INNER JOIN t_follow_artists AS F
+ON A.id_artist = F.fk_id_artist
+INNER JOIN t_users AS U
+ON F.fk_id_user = U.id_user
+GROUP BY A.c_artist
+ORDER BY seguidores DESC, artista;
