1a0f9a76cf5c1c7285607ec76827ac0dc98482ac
IsleneGomes <islenegomes@gmail.com>
2021-09-03 22:56:49 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE FUNCTION quantidade_de_musicas_no_historico(usuarioId INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantidade_musicas_no_historico INT;
+SELECT COUNT(*)
+FROM SpotifyClone.historico
+WHERE SpotifyClone.historico.usuarioId = usuarioId INTO quantidade_musicas_no_historico;
+RETURN quantidade_musicas_no_historico;
+END $$
+DELIMITER ;
+
+SELECT quantidade_de_musicas_no_historico(3);
--- desafio9.sql
+++ desafio9.sql
@@ -7,9 +7,8 @@ SELECT a.artista AS 'artista',
 al.nome AS 'album'
 FROM SpotifyClone.artista AS a
 INNER JOIN SpotifyClone.album AS al ON al.artistaId = a.artistaId
-WHERE nomeartista = a.artista;
+WHERE nomeartista = a.artista
+ORDER BY `artista`;
 END $$
-
 DELIMITER ;
-
 CALL albuns_do_artista('Walter Phoenix');
