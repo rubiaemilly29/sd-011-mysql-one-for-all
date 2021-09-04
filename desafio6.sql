418b6945e8b384d8881b55ef4af2f1e7862236ab
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-03 19:48:33 -0700

    fixing

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,15 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-ROUND(MIN(p.plan_price), 2) AS 'faturamento_minimo',
+FORMAT(MIN(p.plan_price), 2) AS 'faturamento_minimo',
 ROUND(MAX(p.plan_price), 2) AS 'faturamento_maximo',
 ROUND(AVG(p.plan_price), 2) AS 'faturamento_medio',
 ROUND(SUM(p.plan_price), 2) AS 'faturamento_total'
-FROM plan as p;
+FROM plan as p
+INNER JOIN user as u
+ON u.plan_id = p.plan_id;
 
 DROP VIEW faturamento_atual;
 
 SELECT * FROM faturamento_atual;
+
+SELECT
