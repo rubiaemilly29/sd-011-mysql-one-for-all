587c8c9e11bef6cc3b0be01045a7898d5197a834
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 14:42:53 -0300

    Update desafio4.sql

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 CREATE VIEW top_3_artistas AS
-SELECT a.artista_nome AS artista,
+SELECT a.artista_nome AS 'artista',
 (
 SELECT COUNT(*)
 FROM SpotifyClone.artistas_seguidos AS f
 WHERE f.artista_id = a.artista_id
-) AS seguidores
+) AS 'seguidores'
 FROM SpotifyClone.artistas AS a
 ORDER BY 2 DESC, 1 ASC
 LIMIT 5;
