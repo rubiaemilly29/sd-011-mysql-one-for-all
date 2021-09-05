2c1cc1557ba82380a4502aa1ddc0cb28eb57e28e
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 12:01:42 -0300

    Create desafio3.sql

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.usuario,
+m.nome_musica AS nome
+FROM spotifyclone.usuarios AS u
+LEFT JOIN spotifyclone.historico AS h
+ON h.usuario_id = u.usuario_id
+INNER JOIN spotifyclone.musicas AS m
+ON h.musica_id = m.musica_id
+ORDER BY usuario ASC, nome ASC;
