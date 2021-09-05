0d7ab162c2a48f9ea8d274e891b8ef7092650311
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-05 16:04:58 -0300

    Ajuste lint

--- desafio1.sql
+++ desafio1.sql
@@ -9,13 +9,11 @@ CREATE TABLE `artistas` (
   `nome_artista` varchar(50) NOT NULL
 ) engine = InnoDB;
 
-
 CREATE TABLE `planos` (
 	`plano_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`nome_plano` VARCHAR(20) NOT NULL,
 	`valor_plano` DOUBLE NOT NULL
 ) engine = InnoDB;
-
 
 CREATE TABLE `albums` (
 	`album_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -24,16 +22,12 @@ CREATE TABLE `albums` (
 	FOREIGN KEY (`artista_id`) REFERENCES `artistas`(`artista_id`)
 ) engine = InnoDB;
 
-
-
 CREATE TABLE `musicas` (
 	`musica_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`nome_musica` VARCHAR(50) NOT NULL,
 	`album_id` INT NOT NULL,
 	FOREIGN KEY (`album_id`) REFERENCES `albums`(`album_id`)
 ) engine = InnoDB;
-
-
 
 CREATE TABLE `usuarios` (
 	usuario_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -43,8 +37,6 @@ CREATE TABLE `usuarios` (
 	FOREIGN KEY (plano_id) REFERENCES planos(plano_id)
 ) engine = InnoDB;
 
-
-
 CREATE TABLE `seguindo_artistas` (
 	usuario_id INT NOT NULL,
 	artista_id INT NOT NULL,
@@ -52,7 +44,6 @@ CREATE TABLE `seguindo_artistas` (
 	FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
 	FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) engine = InnoDB;
-
 
 CREATE TABLE `historico_de_reproducoes` (
 	usuario_id INT NOT NULL,
@@ -62,21 +53,18 @@ CREATE TABLE `historico_de_reproducoes` (
 	FOREIGN KEY (musica_id) REFERENCES musicas(musica_id)
 ) engine = InnoDB;
 
-
 INSERT INTO artistas (nome_artista)
 VALUES 
 	('Walter Phoenix'),
  	('Peter Strong'),
  	('Lance Day'),
-  	('Freedie Shannon');
-  
+  ('Freedie Shannon');
   
 INSERT INTO planos (nome_plano, valor_plano)
 VALUES 
 	('gratuito', 0.00),
 	('familiar', 7.99),
 	('universitário', 5.99);
- 
   
 INSERT INTO albums (nome_album, artista_id)
 VALUES 
@@ -85,30 +73,27 @@ VALUES
 	('Hallowed Steam',  2),
  	('Incandescent',  3),
 	('Temporary Culture', 4);
-  
-
  
 INSERT INTO musicas (nome_musica, album_id)
 VALUES 
 	('Soul For Us', 1),
-    ('Reflections Of Magic', 1),
-    ('Dance With Her Own', 1),
-    ('Troubles Of My Inner Fire', 2),
-    ('Time Fireworks', 2),
-    ('Magic Circus', 3),
-    ('Honey, So Do I', 3),
-    ("Sweetie, Let's Go Wild", 3),
-    ('She Knows', 3),
-    ('Fantasy For Me', 4),
-    ('Celebration Of More', 4),
-    ('Rock His Everything', 4),
-    ('Home Forever', 4),
-    ('Diamond Power', 4),
-    ("Honey, Let's Be Silly", 4),
-    ('Thang Of Thunder', 5),
-    ('Words Of Her Life', 5),
-    ('Without My Streets', 5);
-
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
 
 INSERT INTO usuarios (nome_usuario, idade_usuario, plano_id)
 VALUES
@@ -117,39 +102,31 @@ VALUES
   ('Bill', 20, 3),
   ('Roger', 45, 1);
   
- 
 INSERT INTO historico_de_reproducoes (usuario_id, musica_id)
 VALUES
-    (17, 1),
-    (17, 6),
-    (17, 14),
-    (17, 16),
-    (18, 13),
-    (18, 17),
-    (18, 2),
-    (18, 15),
-    (19, 4),
-    (19, 16),
-    (19, 6),
-    (20, 3),
-    (20, 18),
-    (20, 11);
- 
+  (17, 1),
+  (17, 6),
+  (17, 14),
+  (17, 16),
+  (18, 13),
+  (18, 17),
+  (18, 2),
+  (18, 15),
+  (19, 4),
+  (19, 16),
+  (19, 6),
+  (20, 3),
+  (20, 18),
+  (20, 11);
 
 INSERT INTO seguindo_artistas (usuario_id, artista_id)
 VALUES
-    (17, 1),
-    (17, 4),
-    (17, 3),
-    (18, 1),
-    (18, 3),
-    (19, 2),
-    (19, 1),
-    (20, 4);
- 
- 
- 
- 
- 
- 
+  (17, 1),
+  (17, 4),
+  (17, 3),
+  (18, 1),
+  (18, 3),
+  (19, 2),
+  (19, 1),
+  (20, 4);
  
