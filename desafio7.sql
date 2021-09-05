26eab9046d5269ca49596362c77aaeafd86324a6
Leandro Lopes <leandro_carlos@uol.com.br>
2021-09-05 11:04:59 -0300

    desafio 7 arquivo correto

--- desafio7.sql
+++ desafio7.sql
@@ -1,1 +1,6 @@
-SELECT * FROM SpotifyClone.perfil_artistas;
+CREATE VIEW perfil_artistas AS
+SELECT ar.artista AS artista, al.album AS album,
+(SELECT COUNT(*) FROM SpotifyClone.Seguidores WHERE artista_id = ar.artista_id) AS seguidores
+FROM SpotifyClone.Albuns AS al 
+INNER JOIN SpotifyClone.Artistas as ar ON al.artista_id = ar.artista_id
+ORDER BY seguidores DESC, artista ASC, album ASC;
