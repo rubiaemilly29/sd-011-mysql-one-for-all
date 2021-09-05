8a00b901d215576d8b5f3d20820833d98154ab37
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 14:34:47 -0300

    Update desafio5.sql

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 CREATE VIEW top_2_hits_do_momento AS
 SELECT m.nome_musica AS 'cancao',
 (SELECT COUNT(*)
-FROM spotifyclone.historico AS h
+FROM SpotifyClone.historico AS h
 WHERE m.musica_id = h.musica_id
 ) AS 'reproducoes'
-FROM spotifyclone.musicas AS m
+FROM SpotifyClone.musicas AS m
 ORDER BY 2 DESC, 1 ASC
 LIMIT 2;
