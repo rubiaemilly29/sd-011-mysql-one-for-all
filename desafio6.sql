4b524d5ddb95d259c7f2eb65a24fc2fda677d8ae
fernando lasmar <f3rnandolasmar@gmail.com>
2021-09-05 01:11:54 -0300

    desafio 8

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 CREATE VIEW faturamento_atual AS
 SELECT CAST(ROUND(MIN(P.valor_plano), 2) AS DECIMAL(10, 2)) AS faturamento_minimo,
-ROUND(MAX(P.valor_plano), 2) AS faturamento_maximo,
+CAST(ROUND(MAX(P.valor_plano), 2) AS DECIMAL(10, 2)) AS faturamento_maximo,
 CAST(ROUND(AVG(P.valor_plano), 2) AS DECIMAL(10, 2)) AS faturamento_medio,
 CAST(ROUND(SUM(P.valor_plano), 2) AS DECIMAL(10, 2)) AS faturamento_total
 FROM planos P
