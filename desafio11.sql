c185584b7be841f2028e0b4c340f1ac3c3f2c649
OldRoge <rogeriomrjr@icloud.com>
2021-09-05 01:46:59 -0300

    OPS, NOW ITS TRUE

--- desafio11.sql
+++ desafio11.sql
@@ -4,17 +4,6 @@ SELECT songs.songs AS nome,
 FROM songs AS songs
 INNER JOIN reproduction_history AS reproduction_history
 ON songs.songs_id = reproduction_history.songs_id
-WHERE reproduction_history.songs_id IN (
-  SELECT user_id FROM user WHERE plan_id <> 1
-)
-GROUP BY songs.songs
-ORDER BY `nome`;
-
-SELECT songs.songs AS nome,
-        COUNT(songs.songs) AS reproducoes
-FROM songs AS songs
-INNER JOIN reproduction_history AS reproduction_history
-ON songs.songs_id = reproduction_history.songs_id
 INNER JOIN user AS user
 ON reproduction_history.user_id = user.user_id
 WHERE user.plan_id = 2 OR user.plan_id = 3
