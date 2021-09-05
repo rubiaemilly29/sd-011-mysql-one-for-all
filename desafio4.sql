480323a395bcdba26961c02e912f870ec0c296c7
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 13:38:51 -0300

    consertado nome da coluna artista_nome

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,9 @@
-CREATE VIEW top_3_artistas AS
-  SELECT a.nome_artista AS artista,
+  SELECT a.artista_nome AS artista,
     (
       SELECT COUNT(*)
       FROM spotifyclone.artistas_seguidos AS f
       WHERE f.artista_id = a.artista_id
     ) AS seguidores
   FROM spotifyclone.artistas AS a
-  ORDER BY seguidores ASC, a.nome_artista DESC
+  ORDER BY seguidores ASC, a.artista_nome DESC
   LIMIT 3;
