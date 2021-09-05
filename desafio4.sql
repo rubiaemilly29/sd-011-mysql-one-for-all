19f9f168d366392c0960249980c1f7d504c40ea1
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 13:53:15 -0300

    codeclimate 1

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 SELECT a.artista_nome AS artista,
 (
-	SELECT COUNT(*)
-	FROM spotifyclone.artistas_seguidos AS f
-	WHERE f.artista_id = a.artista_id
+SELECT COUNT(*)
+FROM spotifyclone.artistas_seguidos AS f
+WHERE f.artista_id = a.artista_id
 ) AS seguidores
 FROM spotifyclone.artistas AS a
 ORDER BY 2 DESC, 1 ASC
