3c91115032b80a8d43f50fc92c54f19a9ca9bc4d
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-09-05 16:05:00 -0300

    Altera query de novo (usei o banco e apaguei)

--- desafio8.sql
+++ desafio8.sql
@@ -1,16 +1,12 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON SpotifyClone.usuarios
+AFTER DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.historico_de_reproducoes
+DELETE FROM historico_de_reproducoes
 WHERE OLD.usuario_id = usuario_id;
-DELETE FROM SpotifyClone.seguindo_artistas
+DELETE FROM seguindo_artistas
 WHERE OLD.usuario_id = usuario_id;
 END $$
 
 DELIMITER ;
-
-SET FOREIGN_KEY_CHECKS=0;
-DELETE from SpotifyClone.usuarios
-WHERE usuario_id = 1;
