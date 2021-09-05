4d9f6740d3d92ff6f635b065e14ed6d7eb46aadc
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-09-05 17:45:42 -0300

    Desafio 11 fixeed

--- desafio11.sql
+++ desafio11.sql
@@ -6,9 +6,9 @@ FROM SpotifyClone.reproduction_history AS rh
 INNER JOIN SpotifyClone.songs AS s
 ON s.song_id = rh.song_id
 WHERE rh.user_id IN (
- SELECT usr.user_id
- FROM users AS usr
- WHERE usr.plan_id = 2 OR usr.plan_id = 3
+SELECT usr.user_id
+FROM users AS usr
+WHERE usr.plan_id = 2 OR usr.plan_id = 3
 )
 group by s.song_id
 order by s.song_name;
