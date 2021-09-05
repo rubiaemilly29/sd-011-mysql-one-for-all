fbdbb45429faf02b5e1938c4bf95fce8575e7514
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-09-05 20:26:55 -0300

    task 5 1.1

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,9 @@
 CREATE VIEW top_2_hits_do_momento AS
-SELECT cc.cancao AS cancao, COUNT(hm.cancao_id)
-AS reproducoes FROM SpotifyClone.cancoes AS cc
+SELECT cc.cancao AS cancao,
+COUNT(hm.cancao_id) AS reproducoes
+FROM SpotifyClone.cancoes AS cc
 INNER JOIN SpotifyClone.historico_reproducoes AS hm
 ON cc.cancao_id = hm.cancao_id
 GROUP BY cc.cancao_id
-ORDER BY COUNT(hm.cancao_id) DESC, c.cancao ASC
+ORDER BY COUNT(hm.cancao_id) DESC, cc.cancao ASC
 LIMIT 2;
