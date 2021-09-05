85f154d9106b1f2e7729c71310de45af30195b4d
OldRoge <rogeriomrjr@icloud.com>
2021-09-04 23:42:26 -0300

    Fixed format

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
-SELECT ROUND(MIN(plan.plan_value), 2) AS faturamento_minimo,
-        ROUND(MAX(plan.plan_value), 2) AS faturamento_maximo,
-        ROUND(AVG(plan.plan_value), 2) AS faturamento_medio,
-        ROUND(SUM(plan.plan_value), 2) AS faturamento_total
+SELECT FORMAT(MIN(plan.plan_value), 2) AS faturamento_minimo,
+        FORMAT(MAX(plan.plan_value), 2) AS faturamento_maximo,
+        FORMAT(AVG(CEIL(plan.plan_value)), 2) AS faturamento_medio,
+        FORMAT(SUM(plan.plan_value), 2) AS faturamento_total
 FROM user AS user
 INNER JOIN plan AS plan
 ON user.plan_id = plan.plan_id;
