8d47e1e5c4568cb474a003bb4356c77a22bbfcbd
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 12:52:29 -0300

    trocado left join por inner join

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,10 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.usuario AS 'usuario',
-m.nome_musica AS nome
-FROM spotifyclone.usuarios AS u
-LEFT JOIN spotifyclone.historico AS h
-ON h.usuario_id = u.usuario_id
-INNER JOIN spotifyclone.musicas AS m
-ON h.musica_id = m.musica_id
-ORDER BY u.usuario ASC, m.nome_musica ASC;
+    SELECT 
+        u.usuario AS 'usuario', m.nome_musica AS 'nome'
+    FROM
+        SpotifyClone.historico AS h
+            INNER JOIN
+        SpotifyClone.usuarios AS u ON u.usuario_id = h.usuario_id
+            INNER JOIN
+        musicas AS m ON m.musica_id = h.musica_id
+    ORDER BY u.usuario ASC , m.nome_musica ASC;
