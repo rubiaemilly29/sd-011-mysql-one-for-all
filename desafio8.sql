74ea073c108222c615e8951eba35e0c723a6b0b6
gabiazevedo <gabiazevedoms@gmail.com>
2021-09-05 15:23:05 -0300

    Finaliza desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+  AFTER DELETE ON SpotifyClone.usuarios
+  FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.historico_de_reproducoes
+  WHERE OLD.usuario_id = usuario_id;
+  DELETE FROM SpotifyClone.seguindo_artistas
+  WHERE OLD.usuario_id = usuario_id;
+END $$
+DELIMITER ;
