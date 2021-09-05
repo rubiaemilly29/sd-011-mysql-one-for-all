d3eae3f918ed67b7311c39836710ff989995f474
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-09-05 13:47:17 -0300

    resolve req.8

--- desafio8.sql
+++ desafio8.sql
@@ -6,8 +6,11 @@ BEFORE DELETE ON SpotifyClone.usuarios
 
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.historico_reproducoes WHERE usuario_id = OLD.usuario_id;
-DELETE FROM SpotifyClone.artistas_seguidos WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico_reproducoes 
+WHERE usuario_id = OLD.usuario_id;
+
+DELETE FROM SpotifyClone.artistas_seguidos 
+WHERE usuario_id = OLD.usuario_id;
 
 END $$
 DELIMITER ;
