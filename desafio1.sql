f656a5e18946866751e16741efd8ba57f5431182
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-09-05 09:04:09 -0300

    Requisito 1-fix

--- desafio1.sql
+++ desafio1.sql
@@ -4,6 +4,11 @@ CREATE DATABASE SpotifyClone;
 
 USE SpotifyClone;
 
+CREATE TABLE artista(
+id_artista INT PRIMARY KEY AUTO_INCREMENT,
+artista VARCHAR(45) NOT NULL
+) ENGINE = InnoDB;
+
 CREATE TABLE planos(
 id_planos INT PRIMARY KEY AUTO_INCREMENT,
 plano VARCHAR(15) NOT NULL,
@@ -11,16 +16,11 @@ valor DECIMAL(2,2) NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE usuarios(
-id_usuários INT PRIMARY KEY AUTO_INCREMENT,
-usuario VARCHAR(45) NOT NULL,
+id_usuarios INT PRIMARY KEY AUTO_INCREMENT,
+usuarios VARCHAR(45) NOT NULL,
 idade INT NOT NULL,
 id_planos INT NOT NULL,
 FOREIGN KEY (id_planos) REFERENCES planos (id_planos)
-) ENGINE = InnoDB;
-
-CREATE TABLE artista(
-id_artista INT PRIMARY KEY AUTO_INCREMENT,
-artista VARCHAR(45) NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE albuns(
@@ -32,14 +32,23 @@ FOREIGN KEY (id_artista) REFERENCES artista (id_artista)
 
 CREATE TABLE cancoes(
 id_cancoes INT PRIMARY KEY AUTO_INCREMENT,
-cancao VARCHAR(60) NULL,
+cancoes VARCHAR(60)  NOT NULL,
 id_albuns INT NOT NULL,
 FOREIGN KEY (id_albuns) REFERENCES albuns (id_albuns)
 ) ENGINE = InnoDB;
 
-CREATE TABLE Usuario_segue_artista (
-id_usuários INT NOT NULL,
+CREATE TABLE usuarios_cancoes(
+id_usuarios INT NOT NULL,
+id_cancoes INT NOT NULL,
+FOREIGN KEY (id_usuarios) REFERENCES usuarios (id_usuarios),
+FOREIGN KEY (id_cancoes) REFERENCES cancoes (id_cancoes),
+CONSTRAINT PRIMARY KEY (id_usuarios, id_cancoes)
+) ENGINE = InnoDB;
+
+CREATE TABLE usuario_segue_artista (
+id_usuarios INT NOT NULL,
 id_artista INT NOT NULL,
-FOREIGN KEY (id_usuários) REFERENCES usuarios (id_usuários),
-FOREIGN KEY (id_artista) REFERENCES artista (id_artista)
+FOREIGN KEY (id_usuarios) REFERENCES usuarios (id_usuarios),
+FOREIGN KEY (id_artista) REFERENCES artista (id_artista),
+CONSTRAINT PRIMARY KEY (id_usuarios, id_artista)
 ) ENGINE = InnoDB;
