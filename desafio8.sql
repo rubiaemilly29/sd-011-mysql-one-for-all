f2680810658a2c18f990e3cc5f754a531e094e89
murilodospacos <murilopacos@gmail.com>
2021-09-05 14:42:57 -0300

    requisito 8 corrigindo id do OLD value

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.seguindo_artistas WHERE usuario_id = OLD.usuario_id;
-DELETE FROM SpotifyClone.historico_de_reproducoes WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.seguindo_artistas WHERE usuario_id = OLD.id;
+DELETE FROM SpotifyClone.historico_de_reproducoes WHERE usuario_id = OLD.id;
 END $$
 DELIMITER ;
