72b0713df8439a1de6f98f0edc80aceabbf7ff44
IsaacMagno <isaacmagno98@gmail.com>
2021-09-05 15:38:57 -0300

    faltou um S T.T

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(sc.song_id) AS 'cancoes',
 (SELECT COUNT(sa.artist_id)
-FROM SpotifyClone.artists sa) AS 'artista',
+FROM SpotifyClone.artists sa) AS 'artistas',
 (SELECT COUNT(sb.album_id)
 FROM SpotifyClone.albuns sb) AS 'albuns'
 FROM SpotifyClone.songs sc;
