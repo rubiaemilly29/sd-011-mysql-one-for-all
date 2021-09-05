503d05af85255e2a0c7deb3ea7386daf9f776c7a
lucasSCsantos <luc.cristovam10@gmail.com>
2021-09-05 17:15:34 -0300

    desafio8-retest

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,11 @@
+USE SpotifyClone;
+
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-DELETE FROM historico WHERE usuario_id = old.usuario_id;
-DELETE FROM seguindo WHERE usuario_id = old.usuario_id;
+DELETE FROM historico WHERE historico.usuario_id = old.usuario_id;
+DELETE FROM seguindo WHERE seguindo.usuario_id = old.usuario_id;
 END $$
 DELIMITER ;
