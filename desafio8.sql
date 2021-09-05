a4b8c4f3587301a3e22d5f8c284d27e6defec929
Leandro Lopes <leandro_carlos@uol.com.br>
2021-09-05 11:37:51 -0300

    desafio 8 arrumando codeclimate

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
     BEFORE DELETE ON SpotifyClone.Usuarios
     FOR EACH ROW
 BEGIN
-   DELETE FROM SpotifyClone.Historico WHERE usuario_id = OLD.usuario_id;
-   DELETE FROM SpotifyClone.Seguidores WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM SpotifyClone.Historico WHERE usuario_id = OLD.usuario_id;
+    DELETE FROM SpotifyClone.Seguidores WHERE usuario_id = OLD.usuario_id;
 END $$
 DELIMITER ;
