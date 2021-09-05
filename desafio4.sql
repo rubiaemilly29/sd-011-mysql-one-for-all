4f1a99e8bbd083dbe3c104449fe6f755419464eb
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 13:17:10 -0300

    alias no requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 CREATE VIEW top_3_artistas AS
-SELECT a.artista_nome,
-COUNT(f.artista_id)
+SELECT a.artista_nome AS artista,
+COUNT(f.artista_id) AS seguidores
 from spotifyclone.artistas AS a
 INNER JOIN spotifyclone.artistas_seguidos  AS f
 ON f.artista_id = a.artista_id
