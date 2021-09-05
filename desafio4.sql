9039f526d8975990f90b85123c3c8f9eef90da5d
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 13:44:02 -0300

    mudado order by

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,9 @@
-  SELECT a.artista_nome AS artista,
-    (
-      SELECT COUNT(*)
-      FROM spotifyclone.artistas_seguidos AS f
-      WHERE f.artista_id = a.artista_id
-    ) AS seguidores
-  FROM spotifyclone.artistas AS a
-  ORDER BY seguidores ASC, a.artista_nome DESC
-  LIMIT 3;
+SELECT a.artista_nome AS artista,
+(
+	SELECT COUNT(*)
+	FROM spotifyclone.artistas_seguidos AS f
+	WHERE f.artista_id = a.artista_id
+) AS seguidores
+FROM spotifyclone.artistas AS a
+ORDER BY 2 DESC, 1 ASC
+LIMIT 5;
