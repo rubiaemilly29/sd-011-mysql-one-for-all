d78b6a8726d1ec6618468b1ddf9188c9340601d3
jhenriquemargrafm <jh.margraf@gmail.com>
2021-09-04 21:55:35 -0300

    Informaçoes sobre planos - user_list x plan_list

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+SELECT MIN(PLANS.price) AS faturamento_minimo,
+  MAX(PLANS.price) AS faturamento_maximo,
+  ROUND(AVG(PLANS.price), 2) AS faturamento_medio,
+  SUM(PLANS.price) AS faturamento_total
+FROM SpotifyClone.user_list AS USERS
+  INNER JOIN SpotifyClone.plan_list AS PLANS
+ON USERS.plan_id = PLAN.plan_id;
