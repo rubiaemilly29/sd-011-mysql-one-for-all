8b187f27296056b7f68a89138efb191286a8055a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-09-05 17:44:36 -0300

    Desafio 1

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+CREATE VIEW cancoes_premium AS
+SELECT
+s.song_name as 'nome',
+COUNT(s.song_id) AS 'reproducoes'
+FROM SpotifyClone.reproduction_history AS rh
+INNER JOIN SpotifyClone.songs AS s
+ON s.song_id = rh.song_id
+WHERE rh.user_id IN (
+ SELECT usr.user_id
+ FROM users AS usr
+ WHERE usr.plan_id = 2 OR usr.plan_id = 3
+)
+group by s.song_id
+order by s.song_name;
