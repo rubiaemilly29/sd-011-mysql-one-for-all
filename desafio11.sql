2e5257be820ab36e189cb6cfd222b49551e3cef9
Marco Galindo <marco1408@gmail.com>
2021-09-05 14:56:40 -0300

    desafio 11 finalizado

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+create view cancoes_premium as
+select t1.musica as nome
+from musica as t1
+inner join historico_de_reproducoes as t2
+on t1.musica_id = t2.musica_id
+inner join usuario as t3
+on t2.usuario_id = t3.usuario_id
+where t3.plano_id = 2 or t3.plano_id = 3
+order by nome;
