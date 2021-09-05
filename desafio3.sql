26b3510e95325987bbf399d525898ddd7f581e2e
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-05 18:39:33 -0300

    Salvando comentario para seguir com o exercicio depois.

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS SELECT
 SELECT u.nome_usuario AS usuario, m.nome_musicas AS nome
+--  preciso fazer dois inner joins e depois juntas as tabelas. Os inners devem levar como base a tabela historico_de_reproducao.
 FROM SpotifyClone.usuarios u
 RIGHT JOIN historico_de_reproducoes hdr ON u.nome_usuario = hdr.musica_id;
 
