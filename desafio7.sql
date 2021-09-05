7685ef91857fe489036aad0a5aab135a71e8fdaf
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-09-04 22:14:51 -0300

    Adiciona desafios 6 e 7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+CREATE VIEW perfil_artistas AS
+    SELECT ar.nome_artista AS artista, a.nome_album AS album,  COUNT(s.usuario_id) AS seguidores
+    FROM SpotifyClone.artistas ar
+    INNER JOIN SpotifyClone.albuns a ON a.artista_id = ar.artista_id
+    INNER JOIN SpotifyClone.seguindo_artistas s ON s.artista_id = a.artista_id
+	GROUP BY artista, album
+    ORDER BY seguidores DESC, artista, album;
