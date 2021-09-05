d66f6f12253673bc7b042be36454240d633e451e
Diogo Augusto <diogotrescastro@gmai.com>
2021-09-05 15:59:04 -0300

    desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historicousuarios(id INT)
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE cancoes_historico INT;
-SELECT COUNT(*)
+SELECT COUNT(*) AS quantidade_musicas_no_historico
 FROM SpotifyClone.historico
 WHERE SpotifyClone.historico.usuario_id = id INTO cancoes_historico;
 RETURN cancoes_historico;
