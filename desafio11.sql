3221eea7665bd8404932ae672d6f35c56b9a05ea
OldRoge <rogeriomrjr@icloud.com>
2021-09-05 01:45:15 -0300

    DONE

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,22 @@
 CREATE VIEW cancoes_premium AS
 SELECT songs.songs AS nome,
-        COUNT(reproduction_history.songs_id) AS reproducoes
+        COUNT(songs.songs) AS reproducoes
 FROM songs AS songs
 INNER JOIN reproduction_history AS reproduction_history
 ON songs.songs_id = reproduction_history.songs_id
+WHERE reproduction_history.songs_id IN (
+  SELECT user_id FROM user WHERE plan_id <> 1
+)
 GROUP BY songs.songs
-ORDER BY `nome`
-LIMIT 8 OFFSET 2;
+ORDER BY `nome`;
+
+SELECT songs.songs AS nome,
+        COUNT(songs.songs) AS reproducoes
+FROM songs AS songs
+INNER JOIN reproduction_history AS reproduction_history
+ON songs.songs_id = reproduction_history.songs_id
+INNER JOIN user AS user
+ON reproduction_history.user_id = user.user_id
+WHERE user.plan_id = 2 OR user.plan_id = 3
+GROUP BY `nome`
+ORDER BY `nome`;
