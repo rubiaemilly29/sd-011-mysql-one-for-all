e8fa55c9d5ada31f3892716e9e9dd62f53923022
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-09-05 11:18:26 -0300

    Corrige climate

--- desafio10.sql
+++ desafio10.sql
@@ -3,12 +3,12 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(nomeUsuario VARCHAR(200))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE history_total INT;
-    SELECT COUNT(hr.cancao_id)
-    FROM SpotifyClone.historico_de_reproducoes hr
-    INNER JOIN SpotifyClone.usuarios u ON u.usuario_id = hr.usuario_id
-    WHERE u.nome = nomeUsuario INTO history_total;
-    RETURN history_total;
+DECLARE history_total INT;
+SELECT COUNT(hr.cancao_id)
+FROM SpotifyClone.historico_de_reproducoes hr
+INNER JOIN SpotifyClone.usuarios u ON u.usuario_id = hr.usuario_id
+WHERE u.nome = nomeUsuario INTO history_total;
+RETURN history_total;
 END $$
 
 DELIMITER ;
