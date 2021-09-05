fd523e69bd5596bac9e036304badcf759b3dc14c
guinhoanjos <iago.anjos@hotmail.com>
2021-09-05 13:26:42 -0300

    codeclimate 1

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,11 @@
 CREATE VIEW top_3_artistas AS
-  SELECT a.nome_artista AS 'artista',
+  SELECT a.nome_artista AS artista,
     (
       SELECT COUNT(*)
-      FROM SpotifyClone.artistas_seguidos AS f
+      FROM spotifyclone.artistas_seguidos AS f
       WHERE f.artista_id = a.artista_id
     ) AS seguidores
-  FROM
-    SpotifyClone.artistas AS a
-  ORDER BY
-    seguidores ASC,
-    a.nome_artista DESC
+  FROM spotifyclone.artistas AS a
+  ORDER BY seguidores ASC, a.nome_artista DESC
   LIMIT 3;
+  
