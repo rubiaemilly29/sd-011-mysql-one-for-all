1977450bc23baa562dc197e97f42b43e07c48735
lucasSCsantos <luc.cristovam10@gmail.com>
2021-09-05 15:40:10 -0300

    desafio8-fix

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON usuarios
 FOR EACH ROW
 BEGIN
-	DELETE FROM historico WHERE usuario_id = old.usuario_id;
-    DELETE FROM seguindo WHERE usuario_id = old.usuario_id;
+DELETE FROM historico WHERE usuario_id = old.usuario_id;
+DELETE FROM seguindo WHERE usuario_id = old.usuario_id;
 END $$
 DELIMITER ;
