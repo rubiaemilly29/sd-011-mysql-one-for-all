52fefc0a5a262eefe74e54efae2c7416741d14bc
OldRoge <rogeriomrjr@icloud.com>
2021-09-05 00:41:09 -0300

    Done requirement 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON user
+    FOR EACH ROW
+BEGIN
+DELETE FROM followers
+WHERE user_id = OLD.user_id;
+DELETE FROM reproduction_history
+WHERE user_id = OLD.user_id;
+END $$
+DELIMITER ;
