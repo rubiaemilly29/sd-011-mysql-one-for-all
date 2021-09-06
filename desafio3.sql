521a0a2f0954bbb3a386e98790ed4aa7fc8a5d9d
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-09-06 10:56:29 -0300

    requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -2,8 +2,8 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
 u.nome  AS 'usuario',
 c.cancao AS 'nome'
-FROM cancoes AS c
-INNER JOIN historico_de_reproducoes hr ON hr.cancao_id = c.id
-INNER JOIN usuario u ON hr.usuario_id = u.id
+FROM SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.historico_de_reproducoes hr ON hr.cancao_id = c.id
+INNER JOIN SpotifyClone.usuario u ON hr.usuario_id = u.id
 ORDER BY `usuario`, `nome`;
 SELECT * FROM historico_reproducao_usuarios;
