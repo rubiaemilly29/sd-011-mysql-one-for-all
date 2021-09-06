75cbb04bfd90ea280d6004194f079a4fe0b2e2ba
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2021-09-06 16:26:50 -0300

    Ajustes desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS
-SELECT COUNT(name) AS 'cancoes',
+SELECT COUNT(*) AS 'cancoes',
 (SELECT COUNT(artist_id) FROM artists) AS 'artistas',
 (SELECT COUNT(album_id) FROM albuns) AS 'albuns'
 FROM SpotifyClone.songs;
