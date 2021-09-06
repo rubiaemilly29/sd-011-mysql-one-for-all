8e48516932b92e8ed2a14d50c19bc867c02c43e3
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-06 11:58:59 -0300

    desafio 1

---
+++ desafio1.json.sql
@@ -0,0 +1,16 @@
+{
+  "coluna_usuario": "nome",
+  "tabela_que_contem_usuario": "Usuario",
+  "coluna_plano": "nome",
+  "tabela_que_contem_plano": "Plano",
+  "coluna_historico_de_reproducoes": "id_cancao",
+  "tabela_que_contem_historico_de_reproducoes": "Reproducao",
+  "coluna_seguindo_artistas": "id_artista",
+  "tabela_que_contem_seguindo_artistas": "Seguidores",
+  "coluna_artista": "nome",
+  "tabela_que_contem_artista": "Artista",
+  "coluna_album": "nome",
+  "tabela_que_contem_album": "Album",
+  "coluna_cancoes": "nome",
+  "tabela_que_contem_cancoes": "Cancao"
+}
---
+++ desafio1.sql
@@ -0,0 +1,129 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE Plano(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    valor DECIMAL(5,2) NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE Usuario(
+     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+     id_plano INT NOT NULL,
+     nome VARCHAR(50) NOT NULL,
+     idade INT NOT NULL,
+     FOREIGN KEY(id_plano) REFERENCES Plano(id)
+) engine = InnoDB;
+
+CREATE TABLE Artista(
+     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+     nome VARCHAR(50) NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE Album(
+     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+     id_artista INT NOT NULL,
+     nome VARCHAR(50) NOT NULL,
+     FOREIGN KEY(id_artista) REFERENCES Artista(id)
+) engine = InnoDB;
+
+CREATE TABLE Cancao(
+     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+     id_album INT NOT NULL,
+     nome VARCHAR(50) NOT NULL,
+     FOREIGN KEY(id_album) REFERENCES Album(id)
+) engine = InnoDB;
+
+CREATE TABLE Reproducao(
+     id_usuario INT NOT NULL,
+     id_cancao INT NOT NULL,
+     FOREIGN KEY(id_usuario) REFERENCES Usuario(id),
+     FOREIGN KEY(id_cancao) REFERENCES Cancao(id)
+) engine = InnoDB;
+
+CREATE TABLE Seguidores(
+     id_usuario INT NOT NULL,
+     id_artista INT NOT NULL,
+     FOREIGN KEY(id_usuario) REFERENCES Usuario(id),
+     FOREIGN KEY(id_artista) REFERENCES Artista(id)
+) engine = InnoDB;
+
+INSERT INTO Plano (nome, valor)
+VALUES
+  ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
+
+INSERT INTO Usuario (id_plano, nome, idade)
+VALUES
+	(1, 'Thati', 23),
+	(2, 'Cintia',35),
+	(3, 'Bill',20),
+    (1, 'Roger', 45);
+  
+  INSERT INTO Artista (nome)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+
+  INSERT INTO Album (id_artista, nome)
+VALUES
+  (1, 'Envious'),
+  (1, 'Exuberant'),
+  (2, 'Hallowed Steam'),
+  (2, 'Incandescent'),
+  (3, 'Temporary Culture');
+
+  INSERT INTO Cancao (id_album, nome)
+VALUES
+  (1, 'Soul For Us'), -- 1
+  (1, 'Reflections Of Magic'), -- 2
+  (1, 'Dance With Her Own'), -- 3
+  (2, 'Troubles Of My Inner Fire'), -- 4
+  (2, 'Time Fireworks'), -- 5
+  (3, 'Magic Circus'), -- 6
+  (3, 'Honey, So Do I'), -- 7
+  (3, 'Sweetie, Let''s Go Wild'), -- 8
+  (3, 'She Knows'), -- 9
+  (4, 'Fantasy For Me'), -- 10
+  (4, 'Celebration Of More'), -- 11
+  (4, 'Rock His Everything'), -- 12
+  (4, 'Home Forever'), -- 13
+  (4, 'Diamond Power'), -- 14
+  (4, 'Honey, Let''s Be Silly'),-- 15
+  (5, 'Thang Of Thunder'), -- 16
+  (5, 'Words Of Her Life'), -- 17
+  (5, 'Without My Streets'); -- 18
+
+  INSERT INTO Reproducao (id_usuario, id_cancao)
+VALUES
+  (1,1),
+  (1,6),
+  (1,14),
+  (1,16),
+  (2,13),
+  (2,17),
+  (2,2),
+  (2,15),
+  (3,4),
+  (3,16),
+  (3,6),
+  (4,3),
+  (4,18),
+  (4,11);
+  
+   INSERT INTO Seguidores (id_usuario, id_artista)
+VALUES
+(1,1),
+(1,4),
+(1,3),
+(2,1),
+(2,3),
+(3,2),
+(3,1),
+(4,4);
