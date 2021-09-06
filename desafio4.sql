4a7f649612dd6966aa558522861f73badf497353
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-06 18:34:20 -0300

    Desafio 4 corrigido

--- desafio4.sql
+++ desafio4.sql
@@ -3,5 +3,5 @@ SELECT a.artist AS artista, COUNT(f.artist_id) AS seguidores
 FROM SpotifyClone.follow AS f
 INNER JOIN SpotifyClone.artists AS a ON f.artist_id = a.artist_id
 GROUP BY f.artist_id
-ORDER BY seguidores DESC LIMIT 3
+ORDER BY seguidores DESC, artista ASC LIMIT 3
 );
