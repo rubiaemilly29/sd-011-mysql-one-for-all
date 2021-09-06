1cdc8f6b6df3928ebce33c9cf03eebbeeec94c6e
Dário Junior <dario.junior.rs@gmail.com>
2021-09-06 12:10:55 -0300

    Não sei o que o Code Climate quer de mim

--- desafio11.sql
+++ desafio11.sql
@@ -3,9 +3,8 @@ msc.musica_nome AS `nome`,
 COUNT(hist.musica_id) AS `reproducoes`
 FROM SpotifyClone.musicas AS msc
 INNER JOIN SpotifyClone.historico_musicas AS hist
-ON hist.musica_id = msc.musica_id
-AND (hist.usuario_id = (
-SELECT usuario_id FROM SpotifyClone.usuarios WHERE plano_id = 2)
-OR hist.usuario_id = (
-SELECT usuario_id FROM SpotifyClone.usuarios WHERE plano_id = 3))
-GROUP BY `nome` ORDER BY `nome`;
+ON hist.musica_id = msc.musica_id AND
+(hist.usuario_id = (SELECT usuario_id FROM SpotifyClone.usuarios WHERE plano_id = 2) OR
+hist.usuario_id = (SELECT usuario_id FROM SpotifyClone.usuarios WHERE plano_id = 3))
+GROUP BY `nome`
+ORDER BY `nome`;
