82eaaa50736d465934cb8aecc4383e1e528899e3
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-06 16:11:19 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+USE SpotifyClone;
+
+CREATE VIEW historico_reproducao_usuarios AS 
+SELECT 
+A.nome AS usuario,
+C.nome AS nome
+FROM Usuario A
+INNER JOIN Reproducao B ON B.id_usuario = A.id
+INNER JOIN Cancao C ON C.id = B.id_cancao
+ORDER BY A.nome, C.nome
