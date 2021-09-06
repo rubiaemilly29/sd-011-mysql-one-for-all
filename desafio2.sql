d6e2db031ea24a059eea03e5dc45d163c6d2777e
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-09-06 10:40:24 -0300

    arruma requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -5,4 +5,3 @@ COUNT(DISTINCT (al.id))  AS 'albuns'
 FROM  SpotifyClone.cancoes AS c
 INNER JOIN SpotifyClone.album  AS al ON al.id = c.album_id
 INNER JOIN SpotifyClone.artista AS ar ON al.artista_id = ar.id;
-SELECT * FROM estatisticas_musicais;
