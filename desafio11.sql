a73fc9914d9e6727f838f6b1d88bf25cc857b079
Dário Junior <dario.junior.rs@gmail.com>
2021-09-06 12:08:32 -0300

    100%?

--- desafio11.sql
+++ desafio11.sql
@@ -4,7 +4,9 @@ COUNT(hist.musica_id) AS `reproducoes`
 FROM SpotifyClone.musicas AS msc
 INNER JOIN SpotifyClone.historico_musicas AS hist
 ON hist.musica_id = msc.musica_id
-AND(hist.usuario_id = (SELECT usuario_id FROM SpotifyClone.usuarios WHERE plano_id = 2)
-OR hist.usuario_id = (SELECT usuario_id FROM SpotifyClone.usuarios WHERE plano_id = 3))
+AND (hist.usuario_id = (
+SELECT usuario_id FROM SpotifyClone.usuarios WHERE plano_id = 2)
+OR hist.usuario_id = (
+SELECT usuario_id FROM SpotifyClone.usuarios WHERE plano_id = 3))
 GROUP BY `nome`
 ORDER BY `nome`;
