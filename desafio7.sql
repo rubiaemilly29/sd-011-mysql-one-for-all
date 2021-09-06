e050a05830c6d9e56389fb8e9772cc0206aca7c4
Dário Junior <dario.junior.rs@gmail.com>
2021-09-06 12:15:22 -0300

    Não sei o que o Code Climate quer de mim

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,9 @@
 CREATE VIEW perfil_artistas AS SELECT
 art.artista_nome AS `artista`,
 alb.album_nome AS `album`,
-(
-  SELECT COUNT(artista_id)
-  FROM SpotifyClone.seguindo_artistas AS seg
-  WHERE art.artista_id = seg.artista_id) AS `seguidores`
+(SELECT COUNT(artista_id)
+FROM SpotifyClone.seguindo_artistas AS seg
+WHERE art.artista_id = seg.artista_id) AS `seguidores`
 FROM SpotifyClone.artistas AS art
 INNER JOIN SpotifyClone.albums AS alb
 ON art.artista_id = alb.artista_id
--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-  BEFORE DELETE ON SpotifyClone.usuarios
-  FOR EACH ROW
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
 BEGIN
 DELETE FROM SpotifyClone.historico_musicas
 WHERE usuario_id = OLD.usuario_id;
