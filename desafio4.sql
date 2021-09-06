7c090f24e25675a594682e8eae849be685136df8
Gui Pereira <guilhermepdo1508@gmail.com>
2021-09-06 18:30:52 -0300

    Desafio 4 corrigido

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 CREATE VIEW top_3_artistas AS (
-SELECT a.artist, COUNT(f.artist_id) AS seguidores
+SELECT a.artist AS artista, COUNT(f.artist_id) AS seguidores
 FROM SpotifyClone.follow AS f
 INNER JOIN SpotifyClone.artists AS a ON f.artist_id = a.artist_id
 GROUP BY f.artist_id
