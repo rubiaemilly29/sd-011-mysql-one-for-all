d54ac6af4c29bb80c32885ca66316b6dec00639c
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-09-06 09:57:14 -0300

    arruma requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -4,5 +4,4 @@ SELECT DISTINCT
 (SELECT COUNT(cancao) FROM  SpotifyClone.cancoes) AS cancoes,
 (SELECT COUNT(artista) FROM  SpotifyClone.artista) AS artistas,
 (SELECT COUNT(album) FROM  SpotifyClone.album) AS albuns;
-   
 SELECT * FROM estatisticas_musicais;
