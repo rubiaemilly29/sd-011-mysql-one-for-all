1003c1fdf855792e0d2f925665f48a8115eebaa4
Víctor Faria <vitilevi@hotmail.com>
2021-09-06 10:48:14 -0300

    Feito requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.Usuario AS 'usuario',
+ca.Cancao as 'nome'
+FROM SpotifyClone.Usuarios AS u
+INNER JOIN SpotifyClone.Historico_reproducoes AS hr
+ON u.UsuarioID = hr.UsuarioID
+INNER JOIN SpotifyClone.Cancoes_album AS ca
+ON hr.CancaoID = ca.CancaoID
+ORDER BY usuario;
