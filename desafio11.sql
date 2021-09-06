54c96410b39e2c439b6f64f416673e132ee8069c
guinhoanjos <iago.anjos@hotmail.com>
2021-09-06 17:42:50 -0300

    ADICIONA INNER JOIN NA TABELA DE PLANOS

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,13 @@
 CREATE VIEW cancoes_premium AS
 SELECT m.nome_musica AS nome,
-COUNT(m.nome_musica) AS reproducoes FROM SpotifyClone.usuarios AS u
+COUNT(m.nome_musica) AS reproducoes
+FROM SpotifyClone.usuarios AS u
 INNER JOIN SpotifyClone.historico AS h
 ON u.usuario_id = h.usuario_id
 INNER JOIN SpotifyClone.musicas AS m
 ON h.musica_id = m.musica_id
-WHERE u.plano_id > 1
+INNER JOIN SpotifyClone.planos AS p
+ON p.plano_id = u.plano_id
+WHERE p.plano != 'gratuito'
 GROUP BY m.nome_musica
 ORDER BY 1 ASC;
