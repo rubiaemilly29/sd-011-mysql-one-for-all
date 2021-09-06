ee6dc24cf9bc2eeeb57732d8b5b6deb41cf2ddec
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-09-06 12:04:09 -0300

    Arrisca req. 8 - ajustes de tabulação

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,10 @@
 USE SpotifyClone;
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-    BEFORE DELETE ON user
-    FOR EACH ROW
+BEFORE DELETE ON user
+FOR EACH ROW
 BEGIN
-	DELETE FROM reproduction_history WHERE user_id = old.user_id;
-    DELETE FROM artists_following WHERE user_id = old.user_id;
+DELETE FROM reproduction_history WHERE user_id = old.user_id;
+DELETE FROM artists_following WHERE user_id = old.user_id;
 END $$
 DELIMITER ; 
