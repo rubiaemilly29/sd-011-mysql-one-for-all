558d3677eda5bea4acc1182f04a6e726671cd9dc
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-06 17:45:10 -0300

    Correcoes

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 CREATE FUNCTION quantidade_musicas_no_historico (
 id_usuario INT )
-RETURNS INT DETERMINISTIC
+RETURNS INT READS SQL DATA
 BEGIN
 DECLARE contador INT;
 
--- desafio4.sql
+++ desafio4.sql
@@ -5,4 +5,5 @@ count(B.id_artista) AS seguidores
 FROM SpotifyClone.Artista A
 LEFT JOIN SpotifyClone.Seguidores B ON B.id_artista = A.id
 GROUP BY A.nome
-ORDER BY COUNT(B.id_artista)DESC , A.nome;
+ORDER BY COUNT(B.id_artista) DESC, A.nome
+LIMIT 3;
