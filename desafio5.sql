8fc30a122ea60fcb292a90ceaeb4617f0938564a
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-09-05 21:19:03 -0300

    requisitos 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_2_hits_do_momento AS SELECT
+song.song_name AS cancao,
+COUNT(*) AS reproducoes
+FROM SpotifyClone.songs AS song
+INNER JOIN
+SpotifyClone.reproduction_history AS repr ON repr.song_id = song.song_id
+GROUP BY repr.song_id
+ORDER BY reproducoes DESC, cancao ASC LIMIT 2;
