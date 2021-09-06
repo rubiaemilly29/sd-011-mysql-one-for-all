3fb16d4569665d21451f792ecab29216d656955f
marsleite <marsleite@gmail.com>
2021-09-06 09:41:01 -0300

    fix desafio 4 e 5 para passar no evaluator

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 CREATE VIEW `top_3_artistas` AS
 SELECT a.artist_name AS `artista`, COUNT(*) AS `seguidores`
-FROM spotifyclone.artists a
+FROM SpotifyClone.artists a
 INNER JOIN
-spotifyclone.followers f
+SpotifyClone.followers f
 ON a.artist_id = f.artist_id
 GROUP BY f.artist_id
 ORDER BY `seguidores` DESC, `artista`
--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 CREATE VIEW `top_2_hits_do_momento` AS
 SELECT s.song_name AS `cancao`, COUNT(*) AS `reproducoes`
-FROM spotifyclone.history_play h
-INNER JOIN spotifyclone.songs s
+FROM SpotifyClone.history_play h
+INNER JOIN SpotifyClone.songs s
 ON s.song_id = h.song_id
 GROUP BY `cancao`
 ORDER BY `reproducoes` DESC, `cancao`
