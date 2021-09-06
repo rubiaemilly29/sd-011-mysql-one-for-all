618a0a03ba877b1a88a926a6eaf11241c253e2ec
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-06 04:18:54 -0300

    Rearranges logic

--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
-CREATE PROCEDURE SpotifyClone.albuns_do_artista(IN name VARCHAR(45))
+CREATE PROCEDURE SpotifyClone.albuns_do_artista(IN artist_name VARCHAR(45))
 BEGIN
 SELECT Ar.name AS artista, Al.name AS album
 FROM SpotifyClone.Artists AS Ar
 INNER JOIN SpotifyClone.Albums AS Al
 ON Ar.artist_id = Al.artist_id
-ORDER BY album;
+WHERE Ar.name = artist_name;
 END $$
 DELIMITER ;
