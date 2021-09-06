5b371826e01ebb9cb34dcfb5e9d70d19d3eff153
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-09-06 00:27:56 -0300

    requisitos 10

--- desafio10.sql
+++ desafio10.sql
@@ -5,9 +5,8 @@ RETURNS INT READS SQL DATA
 BEGIN
     DECLARE totals_reproduction INT;
     SELECT COUNT(*)
-    FROM SpotifyClone.reproduction_history AS hist
-    INNER JOIN SpotifyClone.users AS userr ON userr.user_id = hist.user_id
-    WHERE userr.user_name = id INTO totals_reproduction;
+    FROM SpotifyClone.reproduction_history
+    WHERE user_id = id INTO totals_reproduction;
     RETURN totals_reproduction;
 END $$
 
