c0e0e63ecf1521170c9a8c7ea4c7b5c5e8a20e92
Renan <renan.vamo@gmail.com>
2021-09-06 09:54:07 -0300

    refatora novamente mais uma vez o req10

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,10 @@
 DELIMITER //
-CREATE FUNCTION quantidade_musicas_no_historico(username VARCHAR(20))
+CREATE FUNCTION quantidade_musicas_no_historico(idDoUsuario INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE contagem INT;
-SELECT COUNT(hr.usuario_id) AS quantidade_musicas_no_historico
-FROM historico_reproducoes hr
-INNER JOIN usuarios u
-ON u.usuario_id = hr.usuario_id
-WHERE username = u.usuario
-INTO contagem;
+SELECT COUNT(usuario_id) FROM historico_reproducoes
+WHERE idDoUsuario = usuario_id INTO contagem;
 RETURN contagem;
 END //
 DELIMITER ;
