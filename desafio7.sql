02d999400f9c6642e7e7a4b89f9ef671d67b6d6f
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-09-05 21:17:36 -0300

    task 7 1.2

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,8 @@
 CREATE VIEW perfil_artistas AS
-SELECT art.artista AS artista, alb.album AS album,
-COUNT(ats.artista_id) AS seguidores FROM SpotifyClone.artistas AS art
+SELECT art.artista AS artista,
+alb.album AS album,
+COUNT(ats.artista_id) AS seguidores
+FROM SpotifyClone.artistas AS art
 INNER JOIN SpotifyClone.albuns AS alb
 ON art.artista_id = alb.artista_id
 INNER JOIN SpotifyClone.artistas_seguindo as ats
