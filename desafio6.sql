ff3e22a741672642722f21f9e3a33557f61580c4
Maran <maranmorimoto@gmail.com>
2021-09-06 14:44:01 -0300

    fixing codeclimate error

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
 SELECT MIN(preco) AS 'faturamento_minimo',
 MAX(preco) AS 'faturamento_maximo',
 ROUND(AVG(preco),2) AS 'faturamento_medio',
-SUM(preco) AS 'faturamento total' FROM (
+SUM(preco) AS 'faturamento_total' FROM (
 SELECT preco
 FROM SpotifyClone.usuarios u
 INNER JOIN SpotifyClone.planos p
