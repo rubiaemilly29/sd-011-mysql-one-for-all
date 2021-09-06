d4d7a3b9cff0673017a72a1b3c7bc6ee218e7a96
Dário Junior <dario.junior.rs@gmail.com>
2021-09-05 22:48:59 -0300

    O code climate tirando todo o clima ¬¬

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,4 @@
- CREATE VIEW estatisticas_musicais AS SELECT
-	(SELECT COUNT(musica_id) FROM SpotifyClone.musicas) AS `cancoes`,
-	(SELECT COUNT(artista_id) FROM SpotifyClone.artistas) AS `artistas`,
-	(SELECT COUNT(artista_id) FROM SpotifyClone.albums) AS `albuns`;
-  
+CREATE VIEW estatisticas_musicais AS SELECT
+(SELECT COUNT(musica_id) FROM SpotifyClone.musicas) AS `cancoes`,
+(SELECT COUNT(artista_id) FROM SpotifyClone.artistas) AS `artistas`,
+(SELECT COUNT(artista_id) FROM SpotifyClone.albums) AS `albuns`;
