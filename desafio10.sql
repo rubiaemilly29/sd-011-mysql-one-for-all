cd3a58090af49511890477b23cbb73b3e256eb68
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2021-09-06 18:07:23 -0300

    Correcao desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,15 +1,12 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico (
-id INT )
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE contador INT;
 SELECT COUNT(*)
-FROM SpotifyClone.Reproducoes A 
-WHERE A.id_usuario = id
-INTO contador;
-
+FROM SpotifyClone.Reproducao  
+WHERE id_usuario = id INTO contador;
 RETURN contador;
 END $$
 
