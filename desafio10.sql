318f0203bf780c838d22ea63f96137a9befd6b6c
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-09-06 17:12:40 -0300

    requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,12 @@
 DELIMITER $$
-
-CREATE FUNCTION quantidade_musicas_no_historico(usuario VARCHAR(30))
+CREATE FUNCTION quantidade_musicas_no_historico(usuarioId INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total_musics INT;
 SELECT COUNT(hr.usuario_id)
 FROM SpotifyClone.historico_de_reproducoes AS hr
 INNER JOIN SpotifyClone.usuario AS u ON hr.usuario_id = u.id
-WHERE u.nome = usuario INTO total_musics;
+WHERE u.id = usuarioId INTO total_musics;
 RETURN total_musics;
 END $$
-
 DELIMITER ;
