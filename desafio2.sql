09192df9408dc22350a0ff90fae98cb02333fb9d
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-09-06 10:35:23 -0300

    arruma requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,8 @@
 CREATE VIEW estatisticas_musicais AS
-SELECT DISTINCT
-(SELECT COUNT(cancao) FROM  SpotifyClone.cancoes) AS 'cancoes',
-(SELECT COUNT(artista) FROM  SpotifyClone.artista) AS 'artistas',
-(SELECT COUNT(album) FROM  SpotifyClone.album) 'AS albuns';
+SELECT COUNT(DISTINCT(c.cancao)) AS 'cancoes',
+COUNT(DISTINCT (ar.id)) AS 'artistas',
+COUNT(DISTINCT (al.id))  AS 'albuns' 
+FROM  SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.album  AS al ON al.id = c.album_id
+INNER JOIN SpotifyClone.artista AS ar ON al.artista_id = ar.id;
 SELECT * FROM estatisticas_musicais;
