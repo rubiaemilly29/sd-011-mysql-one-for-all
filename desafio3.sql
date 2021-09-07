39d8e25972d2ad5e52c0874e4ccacdc684cdcb0f
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-07 15:27:01 -0400

    fixing thirdth requiriment:'

--- desafio3.sql
+++ desafio3.sql
@@ -5,5 +5,4 @@ s.song_name AS 'nome'
 FROM user AS u
 INNER JOIN user_history AS uh ON u.user_id = uh.user_id
 INNER JOIN songs AS s ON uh.song_id = s.song_id
-WHERE s.song_id = uh.song_id
 ORDER BY u.user_name, s.song_name;
