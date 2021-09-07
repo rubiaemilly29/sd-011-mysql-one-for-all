84f7453b11683a26d8907e02c96c01c76bc71b5a
jhonatavbrg <jhonatavbrg@gmail.com>
2021-09-07 11:15:35 -0300

    add challenge six

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS SELECT
+MIN(p.plano_valor) AS faturamento_minimo,
+MAX(p.plano_valor) AS faturamento_maximo,
+ROUND(AVG(p.plano_valor), 2) AS faturamento_medio,
+SUM(p.plano_valor) AS faturamento_total
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.planos AS p
+ON u.plano_id = p.plano_id;
