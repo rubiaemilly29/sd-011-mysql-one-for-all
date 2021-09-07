62535b3bf09e36c72494104bad20b23e16c1d01e
Víctor Faria <vitilevi@hotmail.com>
2021-09-07 00:52:52 -0300

    Corrige req 10

--- desafio10.sql
+++ desafio10.sql
@@ -3,7 +3,7 @@ CREATE FUNCTION quantidade_musicas_no_historico(nome VARCHAR(80))
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE result INT;
-SELECT COUNT(*)
+SELECT COUNT(*) AS 'quantidade_musicas_no_historico'
 FROM SpotifyClone.Historico_reproducoes AS hr
 INNER JOIN SpotifyClone.Usuarios AS u
 ON hr.UsuarioID = u.UsuarioID
