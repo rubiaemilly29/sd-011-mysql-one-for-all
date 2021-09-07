879404e125b45326acc369f3eeca471bcae33a55
Maran <maranmorimoto@gmail.com>
2021-09-07 10:04:08 -0300

    deleting unnecessary code from requirement 08

--- desafio8.sql
+++ desafio8.sql
@@ -9,10 +9,3 @@ DELETE FROM SpotifyClone.seguindoartistas
 WHERE OLD.usuario_id = usuario_id;
 END $$
 DELIMITER ;
-
-SELECT usuario_id FROM usuarios
-WHERE nome = 'Thati';
-
-DELETE FROM SpotifyClone.usuarios where nome = 'Thati';
-
-SET SQL_SAFE_UPDATES = 0;
