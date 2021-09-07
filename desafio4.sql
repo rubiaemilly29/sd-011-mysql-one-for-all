078d65b1ecfc10e86ed6097971f9e9aa8743f341
jhonatavbrg <jhonatavbrg@gmail.com>
2021-09-07 10:45:30 -0300

    add challenge four

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+CREATE VIEW top_3_artistas AS SELECT
+artista_nome AS artista, COUNT(usuario_id) AS seguidores
+FROM SpotifyClone.artistas AS artistas
+INNER JOIN SpotifyClone.artistas_seguidores AS seguidos
+ON artistas.artista_id = seguidos.artista_id
+GROUP BY artista
+ORDER BY seguidores DESC, artista_nome LIMIT 3;
