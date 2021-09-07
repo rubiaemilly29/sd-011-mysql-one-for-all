8bb522225d83d7fc19912dd79ccde48ecda6aa83
Víctor Faria <vitilevi@hotmail.com>
2021-09-07 00:41:09 -0300

    Feito requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(nome VARCHAR(80))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE result INT;
+SELECT COUNT(*)
+FROM SpotifyClone.Historico_reproducoes AS hr
+INNER JOIN SpotifyClone.Usuarios AS u
+ON hr.UsuarioID = u.UsuarioID
+WHERE u.Usuario = nome INTO result;
+RETURN result;
+END$$
