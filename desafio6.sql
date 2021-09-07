6ec2bc6f34ae4faf2ae1bbaf3a2e7fbd5b876ec7
CaioOK <78622405+CaioOK@users.noreply.github.com>
2021-09-07 02:57:16 -0300

    Corrigindo edentação desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,8 +3,8 @@ CREATE VIEW faturamento_atual AS (
     ROUND(MIN(p.valor), 2) AS faturamento_minimo,
     ROUND(MAX(p.valor), 2) AS faturamento_maximo,
     ROUND(AVG(p.valor), 2) AS faturamento_medio,
-	ROUND(SUM(p.valor), 2) AS faturamento_total
-	FROM SpotifyClone.planos AS p
-	INNER JOIN SpotifyClone.usuarios AS u
-	ON u.plano_id = p.plano_id
+    ROUND(SUM(p.valor), 2) AS faturamento_total
+    FROM SpotifyClone.planos AS p
+    INNER JOIN SpotifyClone.usuarios AS u
+    ON u.plano_id = p.plano_id
 );
