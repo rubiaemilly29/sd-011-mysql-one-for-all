216ebd9e96c0fab7fcd370cbfb4dbf9d56c2931d
Víctor Faria <vitilevi@hotmail.com>
2021-09-07 01:17:11 -0300

    Corrigido o 10 bora brow

--- desafio10.sql
+++ desafio10.sql
@@ -1,12 +1,14 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(nome VARCHAR(80))
+CREATE FUNCTION quantidade_musicas_no_historico(id VARCHAR(80))
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE result INT;
-SELECT COUNT(*)
+DECLARE total INT;
+SELECT COUNT(hr.CancaoID)
 FROM SpotifyClone.Historico_reproducoes AS hr
 INNER JOIN SpotifyClone.Usuarios AS u
 ON hr.UsuarioID = u.UsuarioID
-WHERE u.Usuario = nome INTO result;
-RETURN result;
+WHERE u.UsuarioID = id
+INTO total;
+RETURN total;
 END$$
+DELIMITER ;
