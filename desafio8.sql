577d94048d0bac2a8a17c8c95065667c0350955a
Nick-713 <pereiranicolas713@gmail.com>
2021-09-07 15:36:41 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuario
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.segue_artista
+WHERE
+usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.historico_de_reproducoes
+WHERE
+usuario_id = OLD.usuario_id;
+END $$
+DELIMITER ;
