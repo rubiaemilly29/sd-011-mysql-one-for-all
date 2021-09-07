e1fea205a9bc38fdebd7405649af64491023ec0e
Gustavo Mendes <gusm217@gmail.com>
2021-09-07 12:50:04 -0300

    Adiciona o 9

---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR (50))
+BEGIN
+SELECT
+a.artista AS artista,
+al.album AS album
+FROM SpotifyClone.artista AS a
+INNER JOIN SpotifyClone.album AS al
+ON a.artista_id = al.artista_id
+WHERE a.artista = nome_artista
+ORDER BY album;
+END $$
+
+DELIMITER ;
