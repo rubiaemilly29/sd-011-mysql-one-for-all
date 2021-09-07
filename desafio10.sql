9a8950153af0579e5d459ac21295ebd82c79b257
Hamander <1hamander@gmail.com>
2021-09-07 20:22:08 -0300

    quantidade 2

--- desafio10.sql
+++ desafio10.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico (id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE quant INT;
+DECLARE quantidade INT;
 SELECT COUNT(*) AS quantidade_musicas_no_historico
 FROM spotifyclone.historico
-WHERE usuario_id = id INTO id;
+WHERE usuario_id = id INTO quantidade;
 RETURN quantidade;
 END $$
 
