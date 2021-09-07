815eb803e01158bbad078154a7f7545a20952375
Luan Xavier <luantenorio3@gmail.com>
2021-09-07 17:15:01 -0300

    Requisito 8 finalizado

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    AFTER DELETE ON usuario
+    FOR EACH ROW
+BEGIN
+    DELETE FROM SpotifyClone.seguindo_artistas
+    WHERE OLD.usuario_id = SpotifyClone.seguindo_artistas.usuario_id;
+	DELETE FROM SpotifyClone.historico_de_reproducoes
+    WHERE OLD.usuario_id = SpotifyClone.historico_de_reproducoes.usuario_id;
+END $$
+DELIMITER ;
