07b99f740b9d3e60eda5e6d3e4cd613cf0fb6260
Vitor <vitorguima@gmail.com>
2021-09-07 16:06:42 -0300

    resolve desafio4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 CREATE VIEW top_3_artistas AS
-SELECT
-A.nome AS 'artista',
-COUNT(AC.usuario_id) AS seguidores
-FROM SpotifyClone.artista_seguidores AS AC
+SELECT 
+A.nome AS 'artista', 
+COUNT(ASE.usuario_id) AS 'seguidores'
+FROM SpotifyClone.artista_seguidores AS ASE
 LEFT JOIN SpotifyClone.artista AS A
-ON AC.artista_id = A.artista_id
+ON ASE.artista_id = A.artista_id
 GROUP BY A.nome
-ORDER BY COUNT(AC.usuario_id), A.nome DESC
+ORDER BY COUNT(ASE.usuario_id) DESC, A.nome
 LIMIT 3;
