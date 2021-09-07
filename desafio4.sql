cc353e45cdb465b00b6c0a4b192cc4c9b01b9de4
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-07 19:13:19 -0300

    ajeitado desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 CREATE VIEW top_3_artistas AS 
 SELECT 
-a.artista AS artista, COUNT(s.usuario_id)
+a.artista AS artista, COUNT(s.usuario_id) AS seguidores
 FROM SpotifyClone.artistas AS a
 INNER JOIN SpotifyClone.seguidores AS s ON s.artista_id = a.artista_id
 GROUP BY 1
