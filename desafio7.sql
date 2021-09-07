2c3cb9acc096b09c70f9046658b4be7bb6655d69
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-07 20:16:58 -0300

    Feito desafio7.

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,11 @@
---  artista
--- album
--- seguidores
--- decrecente por seguidores 
--- empate pelo nome e al
-CREATE VIEW perfil_artistas AS
+CREATE VIEW perfil_artistas AS SELECT
+a.nome_artista AS artista,
+al.nome_album AS album,
+COUNT(s.usuario_id) AS seguidores
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.albums as al
+ON a.artista_id = al.artista_id
+INNER JOIN SpotifyClone.seguindo_artistas as s
+ON a.artista_id = s.artista_id
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista, album;
