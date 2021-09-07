c4ee30b67fbebbf8ac8e054c535ea15b95d0f7f2
jhonatavbrg <jhonatavbrg@gmail.com>
2021-09-07 10:25:01 -0300

    add challenge three

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS SELECT
+u.usuario_nome AS usuario,
+m.musica_nome AS nome
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico_reproducoes AS h
+INNER JOIN SpotifyClone.musicas AS m
+ON h.usuario_id = u.usuario_id AND h.musica_id = m.musica_id
+ORDER BY usuario, nome;
