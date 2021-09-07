4761eefafb239abcdac7c593266ad1b57c9b839d
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-07 20:03:36 -0300

    Feito desafio6.

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,8 @@
 CREATE VIEW faturamento_atual AS
-SELECT FORMAT(MIN(p.valor_plano), 2) AS faturamento_minimo,
-MAX(p.valor_plano) AS faturamento_maximo,
-FORMAT(AVG(p.valor_plano), 2) AS faturamento_medio,
-SUM(p.valor_plano) AS faturamento_total
+SELECT ROUND(MIN(p.valor_plano), 2) AS faturamento_minimo,
+ROUND(MAX(p.valor_plano), 2) AS faturamento_maximo,
+ROUND(AVG(p.valor_plano), 2) AS faturamento_medio,
+ROUND(SUM(p.valor_plano), 2) AS faturamento_total
 FROM SpotifyClone.planos AS p
 INNER JOIN SpotifyClone.usuarios AS u
 ON u.plano_id = p.plano_id;
-
-SELECT * FROM faturamento_atual;
