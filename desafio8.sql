c6bb786af0bc757ae530cfd3d60fc1c8fbff1802
Gustavo Mendes <gusm217@gmail.com>
2021-09-07 12:31:00 -0300

    Adiciona o 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuario
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.historico WHERE usuario_id = old.usuario_id;
+DELETE FROM SpotifyClone.seguindo WHERE usuario_id = old.usuario_id;
+END $$
+
+DELIMITER ;
