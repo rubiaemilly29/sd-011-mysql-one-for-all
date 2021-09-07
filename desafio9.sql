090c218ed77d4cc29cd0cee73ce984b23666673f
Nick-713 <pereiranicolas713@gmail.com>
2021-09-07 15:47:27 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(
+  IN artista_nome VARCHAR(60)
+)
+BEGIN
+SELECT A.nome_do_artista AS 'artista', 
+B.titulo AS 'album'
+FROM SpotifyClone.artista AS A
+INNER JOIN
+SpotifyClone.album AS B
+ON B.artista_id = A.artista_id
+WHERE
+A.nome_do_artista = artista_nome;
+END $$
+DELIMITER;
