d28abdc3be629034928070a4c0e4d21115f2d536
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-09-07 18:16:58 -0300

    Requisitos 8-fix

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON usuarios
+BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
 DELETE FROM usuarios_cancoes WHERE id_usuarios = OLD.id_usuarios;
