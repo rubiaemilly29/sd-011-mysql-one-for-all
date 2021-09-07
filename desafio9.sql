1071f511248199b331e0d92fec4b8a928b71b1d5
Nick-713 <pereiranicolas713@gmail.com>
2021-09-07 16:02:15 -0300

    desafiooo 9

--- desafio9.sql
+++ desafio9.sql
@@ -3,14 +3,11 @@ CREATE PROCEDURE albuns_do_artista(
   IN artista_nome VARCHAR(60)
 )
 BEGIN
-SELECT A.nome_do_artista AS 'artista', 
-B.titulo AS 'album'
-FROM SpotifyClone.artista AS A
-INNER JOIN
-SpotifyClone.album AS B
+SELECT A.nome_do_artista AS 'artista', B.titulo_do_album AS 'album'
+FROM artista AS A
+INNER JOIN album AS B
 ON B.artista_id = A.artista_id
 WHERE
 A.nome_do_artista = artista_nome;
-END 
-$$
-DELIMITER;
+END $$
+DELIMNITER ;
