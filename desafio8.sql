658337afef9a351c3cb8849bd15ab8cbb769a5fd
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-09-07 07:50:22 -0300

    Desafio * finalizado!

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.historico_reproducoes AS hr
+  WHERE OLD.usuario_id = hr.usuario_id;
+  DELETE FROM SpotifyClone.seguindo_artistas AS sa
+  WHERE OLD.usuario_id = sa.usuario_id;
+END $$
+
+DELIMITER ;
