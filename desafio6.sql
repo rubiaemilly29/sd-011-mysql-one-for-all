ca439a396dbf342922d8e30d96efff8c39375d13
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-07 20:17:29 -0300

    Feito desafio7.

--- desafio6.sql
+++ desafio6.sql
@@ -5,5 +5,4 @@ FORMAT(AVG(p.valor_plano), 2) AS faturamento_medio,
 FORMAT(SUM(p.valor_plano), 2) AS faturamento_total
 FROM SpotifyClone.planos AS p
 INNER JOIN SpotifyClone.usuarios AS u
-
 ON u.plano_id = p.plano_id;
