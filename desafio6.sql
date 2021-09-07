e311d4d96e07c8b7a352b641cc4c3c10e8126a8c
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-09-07 16:19:35 -0300

    Requisitos 6fix

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 CREATE VIEW faturamento_atual AS
-SELECT MIN(valor)  AS faturamento_minimo,
-MAX(valor) AS faturamento_maximo,
+SELECT ROUND(MIN(valor), 2) AS faturamento_minimo,
+ROUND(MAX(valor), 2) AS faturamento_maximo,
 ROUND(AVG(CEIL (valor)), 2) AS faturamento_medio,
 ROUND(SUM(valor), 2) AS faturamento_total
 FROM planos AS P
