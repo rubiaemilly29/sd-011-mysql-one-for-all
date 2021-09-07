a897ed53555b42716afc5ef156d77de333db8d94
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-07 17:50:11 -0300

    n pra de vir erro

--- desafio9.sql
+++ desafio9.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 CREATE PROCEDURE albunsDoArtista(IN  nome VARCHAR(50))
 BEGIN
 SELECT 
-    a.artista,
-    al.album
-    FROM SpotifyClone.artistas AS a
+a.artista,
+al.album
+FROM SpotifyClone.artistas AS a
 INNER JOIN SpotifyClone.albuns AS al ON a.artista_id = al.artista_id
-	WHERE nome = a.artista
+WHERE nome = a.artista
 ORDER BY 2;
 END $$
 DELIMITER ;
