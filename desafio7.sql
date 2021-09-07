4fe95172878a8b1149643370cd7f0b489ec416eb
Jessica Arruda <sicarruda@gmail.com>
2021-09-07 16:35:14 -0300

    exercicio 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+-- CREATE VIEW perfil_artistas AS
+SELECT A.nome, AL.album, SA.seguidores
+FROM SpotifyClone.artistas AS A
+INNER JOIN 
+(SELECT id_artista, count(*) AS seguidores 
+FROM SpotifyClone.tabela_seguidores_artistas 
+GROUP BY 1) AS SA
+ON SA.id_artista = A.id_artista
+INNER JOIN albuns AS AL
+ON AL.id_artista = A.id_artista
+ORDER BY 3 DESC, 1 ASC, 2 ASC;
