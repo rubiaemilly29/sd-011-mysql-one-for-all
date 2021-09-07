1a96cf34a2ccdf38358f05245963a4ba9a4ee332
Jessica Arruda <sicarruda@gmail.com>
2021-09-07 17:03:45 -0300

    exercicio 8 correção code climate

--- desafio8.sql
+++ desafio8.sql
@@ -3,9 +3,9 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.tabela_seguidores_artistas
-    WHERE SpotifyClone.usuarios.id_usuario = old.id_usuario;
-    DELETE FROM SpotifyClone.historico_de_reproducoes
-    WHERE SpotifyClone.usuarios.id_usuario = old.id_usuario;
+DELETE FROM SpotifyClone.tabela_seguidores_artistas
+WHERE SpotifyClone.usuarios.id_usuario = old.id_usuario;
+DELETE FROM SpotifyClone.historico_de_reproducoes
+WHERE SpotifyClone.usuarios.id_usuario = old.id_usuario;
 END $$
 DELIMITER ;
