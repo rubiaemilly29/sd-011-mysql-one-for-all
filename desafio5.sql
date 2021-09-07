e979c9e042d2db0c9b84774bc6ecd8927867568f
jhonatavbrg <jhonatavbrg@gmail.com>
2021-09-07 10:51:32 -0300

    add challenge five

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+CREATE VIEW top_2_hits_do_momento AS SELECT
+musica_nome AS cancao, COUNT(usuario_id) AS reproducoes
+FROM SpotifyClone.musicas AS cacoes
+INNER JOIN SpotifyClone.historico_reproducoes AS historico
+ON cacoes.musica_id = historico.musica_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao LIMIT 2;
