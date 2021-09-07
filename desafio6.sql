a2adbb533f8e208b15368e119787f6b6c7d65fd5
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-09-07 16:28:35 -0300

    Requisitos 6fix2

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
-SELECT ROUND(MIN(valor), 2) AS faturamento_minimo,
-ROUND(MAX(valor), 2) AS faturamento_maximo,
-ROUND(AVG(CEIL (valor)), 2) AS faturamento_medio,
-ROUND(SUM(valor), 2) AS faturamento_total
+SELECT FORMAT(MIN(valor), 2) AS faturamento_minimo,
+FORMAT(MAX(valor), 2) AS faturamento_maximo,
+FORMAT(AVG(CEIL (valor)), 2) AS faturamento_medio,
+FORMAT(SUM(valor), 2) AS faturamento_total
 FROM planos AS P
 INNER JOIN  usuarios AS U 
 ON P.id_planos = U.id_planos;
