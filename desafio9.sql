0858b4e7e773e37be6dde016bdafd2ccc10779a2
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-07 17:45:56 -0300

    agora vaii

--- desafio9.sql
+++ desafio9.sql
@@ -6,8 +6,8 @@ SELECT
     a.artista,
     al.album
     FROM SpotifyClone.artistas AS a
-	INNER JOIN SpotifyClone.albuns AS al ON a.artista_id = al.artista_id
-        WHERE nome = a.artista
+INNER JOIN SpotifyClone.albuns AS al ON a.artista_id = al.artista_id
+	WHERE nome = a.artista
 ORDER BY 2;
 END $$
 DELIMITER ;
