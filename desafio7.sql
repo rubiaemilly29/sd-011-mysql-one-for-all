b3958632d51f93a0dc031f1ddb3ecfa79a159afa
CaioOK <78622405+CaioOK@users.noreply.github.com>
2021-09-07 03:35:55 -0300

    Desafio 7 finalizado!

---
+++ desafio7.sql
@@ -0,0 +1,16 @@
+CREATE VIEW perfil_artistas AS (
+  SELECT
+    art.artista AS artista,
+    alb.album AS album,
+    COUNT(sa.artista_id) AS seguidores
+  FROM SpotifyClone.artistas AS art
+  INNER JOIN SpotifyClone.albuns AS alb
+  ON art.artista_id = alb.artista_id
+  INNER JOIN SpotifyClone.seguindo_artistas AS sa
+  ON art.artista_id = sa.artista_id
+  GROUP BY alb.album
+  ORDER BY
+    seguidores DESC,
+    artista ASC,
+    album ASC
+);
