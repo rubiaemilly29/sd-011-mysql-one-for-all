305fb13b30b6b4f236695d3a898547bea85576b1
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-07 15:10:19 -0400

    fixing 6th and 8th requiriments

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-MIN(p.plan_price) AS 'faturamento_minimo',
-MAX(p.plan_price) AS 'faturamento_maximo',
-ROUND(AVG(p.plan_price), 2) AS 'faturamento_medio',
-ROUND(SUM(p.plan_price), 2) AS 'faturamento_total'
-FROM spotifyClone.plan as p
-INNER JOIN user as u
+MIN(p.plan_price) AS faturamento_minimo,
+MAX(p.plan_price) AS faturamento_maximo,
+ROUND(AVG(p.plan_price), 2) AS faturamento_medio,
+ROUND(SUM(p.plan_price), 2) AS faturamento_total
+FROM spotifyClone.plan AS p
+INNER JOIN spotifyClone.user AS u
 ON u.plan_id = p.plan_id;
--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,11 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON user
+BEFORE DELETE ON user
 FOR EACH ROW
 BEGIN
 DELETE FROM user_history 
-WHERE user_history.user_id = old.user_id;
+WHERE OLD.user_id = user_id;
 DELETE FROM user_follow 
-WHERE user_follow.user_id = old.user_id;
+WHERE OLD.user_id = user_id;
 END $$
 DELIMITER ;
