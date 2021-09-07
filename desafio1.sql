9f92447589037975252ec38ad9c5656810401952
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-09-07 10:11:16 -0300

    Requisitos-fix

--- desafio1.sql
+++ desafio1.sql
@@ -60,6 +60,14 @@ VALUES
 ('Lance Day'),
 ('Freedie Shannon');
 
+INSERT INTO albuns (album, id_artista)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
 INSERT INTO planos (plano, valor)
 VALUES
 ('gratuito', 0),
@@ -73,51 +81,26 @@ VALUES
 ('Bill', 20, 3),
 ('Roger', 45, 1);
 
-INSERT INTO albuns (album, id_artista)
-VALUES
-('Envious', 1),
-('Exuberant', 1),
-('Hallowed Steam', 2),
-('Incandescent', 3),
-('Temporary Culture', 4);
-
 INSERT INTO cancoes (cancao, id_albuns)
 VALUES
-('Soul For Us', 1),
-('Reflection Of Magic', 1),
-('Dance With Her Own', 1),
-('Troubles Of Inner Fire', 2),
-('Time Fireworks', 2),
-('Magic Circus', 3),
-('Honey, So, Do, I', 3),
-('Sweetie, Lets Go Wild', 3),
-('She Knows', 3),
-('Fantasy For Me', 4),
-('Celebration Of More', 4),
-('Home Forever', 4),
-('Diamond Power', 4),
-('Rock His Everthing', 4),
-('Honey, Lets Be Silly', 4),
-('Thang Of Thunder', 5),
-('Wors Of Her Life', 5),
-('Without My Streets', 5);
-
-INSERT INTO usuarios_cancoes (id_usuarios, id_cancoes)
-VALUES
-(1, 1),
-(1, 6),
-(1, 14),
-(1, 16),
-(2, 13),
-(2, 17),
-(2, 2),
-(2, 15),
-(3, 4),
-(3, 16),
-(3, 6),
-(4, 3),
-(4, 18),
-(4, 11);  
+("Soul For Us", 1), 
+("Reflections Of Magic", 1),
+("Dance With Her Own", 1), 
+("Troubles Of My Inner Fire'", 2),
+("Time Fireworks", 2),
+("Magic Circus", 3),
+("Honey, So Do I", 3),
+("Sweetie Let's Go Wild", 3),
+("She Knows", 3),
+("Fantasy For Me", 4),
+("Celebration Of More", 4),
+("Rock His Everything", 4),
+("Home Forever", 4),
+("Diamond Power", 4),
+("Honey, Let's Be Silly", 4),
+("Thang Of Thunder", 5),
+("Words Of Her Life", 5),
+("Without My Streets", 5);
 
 INSERT INTO usuario_seguidor (id_usuarios, id_artista)
 VALUES
@@ -129,3 +112,20 @@ VALUES
 (3, 1),
 (3, 2),
 (4, 4);
+
+INSERT INTO usuarios_cancoes (id_usuarios, id_cancoes)
+VALUES
+(1, 1),
+(1, 6),
+(1, 15),
+(1, 17),
+(2, 2),
+(2, 14),
+(2, 16),
+(2, 18),
+(3, 4),
+(3, 6),
+(3,17),
+(4, 3),
+(4, 12),
+(4, 18);  
