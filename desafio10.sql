d763d3a4ba87b5cfca7349fd051d1d73ffeee729
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-09-07 08:46:37 -0300

    Corrigindo desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ USE SpotifyClone;
 
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(nome_artista VARCHAR(50))
+CREATE FUNCTION quantidade_musicas_no_historico(id_artista VARCHAR(50))
   RETURNS INT READS SQL DATA
   BEGIN
     DECLARE quantidade INT;
@@ -10,7 +10,7 @@ CREATE FUNCTION quantidade_musicas_no_historico(nome_artista VARCHAR(50))
     FROM SpotifyClone.historico_reproducoes AS hr
     INNER JOIN SpotifyClone.usuarios AS u
     ON hr.usuario_id = u.usuario_id
-    WHERE u.usuario = nome_artista
+    WHERE u.usuario_id = id_artista
     INTO quantidade;
     RETURN quantidade;
   END $$
