00f81fcdbd913c71371ad335d56d351e859b8392
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-07 19:14:43 -0300

    Feito desafio4.

--- desafio3.sql
+++ desafio3.sql
@@ -6,15 +6,3 @@ INNER JOIN SpotifyClone.historico_de_reproducoes AS hdr
 INNER JOIN SpotifyClone.musicas AS m
 ON hdr.musica_id = m.musica_id AND hdr.usuario_id = u.usuario_id;
 ORDER BY usuario, nome;
-
-SELECT * FROM historico_reproducao_usuarios;
-
-CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.usuario_nome AS usuario,
-m.musica_nome AS nome
-FROM SpotifyClone.usuarios AS u
-INNER JOIN SpotifyClone.historico_reproducoes AS hr
-INNER JOIN SpotifyClone.musicas AS m
-ON hr.musica_id = m.musica_id
-AND hr.usuario_id = u.usuario_id
-ORDER BY usuario, nome;
---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_3_artistas AS 
+SELECT a.nome_artista AS artista,
+COUNT(*) AS seguidores
+FROM SpotifyClone.artistas a
+INNER JOIN SpotifyClone.seguindo_artistas AS seguindo
+ON a.artista_id = seguindo.artista_id
+GROUP BY artista
+ORDER BY seguidores DESC, artista
+LIMIT 3;
