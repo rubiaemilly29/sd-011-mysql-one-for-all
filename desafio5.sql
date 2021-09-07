997f2ba353dbeaaba3a04941b64872af86cbf7fc
Luan Xavier <luantenorio3@gmail.com>
2021-09-07 15:09:06 -0300

    Requisito 5 finalizado

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT c.cancao AS cancao, COUNT(h.cancao_id) AS reproducoes
+FROM SpotifyClone.cancao AS c
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h
+ON c.cancao_id = h.cancao_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao ASC
+LIMIT 2;
