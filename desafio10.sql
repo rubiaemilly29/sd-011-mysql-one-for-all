ced6def1eeb27850d8b0c6c813b5d055a3d43c02
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-09-07 08:20:38 -0300

    Corrigindo o desafio  8 e enviando o 10 finalizado!

---
+++ desafio10.sql
@@ -0,0 +1,18 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE FUNCTION quantidade_de_musicas_no_historico(nome_artista VARCHAR(50))
+  RETURNS INT READS SQL DATA
+  BEGIN
+    DECLARE quantidade INT;
+    SELECT COUNT(hr.musica_id)
+    FROM SpotifyClone.historico_reproducoes AS hr
+    INNER JOIN SpotifyClone.usuarios AS u
+    ON hr.usuario_id = u.usuario_id
+    WHERE u.usuario = nome_artista
+    INTO quantidade;
+    RETURN quantidade;
+  END $$
+
+DELIMITER ;
--- desafio8.sql
+++ desafio8.sql
@@ -6,10 +6,10 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
-  DELETE FROM SpotifyClone.historico_reproducoes AS hr
-  WHERE OLD.usuario_id = hr.usuario_id;
-  DELETE FROM SpotifyClone.seguindo_artistas AS sa
-  WHERE OLD.usuario_id = sa.usuario_id;
+  DELETE FROM SpotifyClone.historico_reproducoes
+  WHERE OLD.usuario_id = historico_reproducoes.usuario_id;
+  DELETE FROM SpotifyClone.seguindo_artistas
+  WHERE OLD.usuario_id = seguindo_artistas.usuario_id;
 END $$
 
 DELIMITER ;
