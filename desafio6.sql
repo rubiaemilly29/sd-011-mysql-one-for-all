93bf2b5cafc3c8815d3f21cb093d737b62e8d4b0
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-09-07 07:28:47 -0300

    Corrigindo desafios 6 e 7

--- desafio6.sql
+++ desafio6.sql
@@ -4,7 +4,7 @@ CREATE VIEW faturamento_atual AS (
     ROUND(MAX(p.valor), 2) AS faturamento_maximo,
     ROUND(AVG(p.valor), 2) AS faturamento_medio,
     ROUND(SUM(p.valor), 2) AS faturamento_total
-    FROM SpotifyClone.planos AS p
-    INNER JOIN SpotifyClone.usuarios AS u
-    ON u.plano_id = p.plano_id
+  FROM SpotifyClone.planos AS p
+  INNER JOIN SpotifyClone.usuarios AS u
+  ON u.plano_id = p.plano_id
 );
--- desafio7.sql
+++ desafio7.sql
@@ -3,13 +3,14 @@ CREATE VIEW perfil_artistas AS (
     art.artista AS artista,
     alb.album AS album,
     COUNT(sa.artista_id) AS seguidores
-  FROM SpotifyClone.artistas AS art
+    FROM SpotifyClone.artistas AS art
   INNER JOIN SpotifyClone.albuns AS alb
   ON art.artista_id = alb.artista_id
   INNER JOIN SpotifyClone.seguindo_artistas AS sa
   ON art.artista_id = sa.artista_id
-  GROUP BY alb.album
+  GROUP BY album, artista
   ORDER BY
-    seguidores DESC,
-    artista ASC,
-    album ASC);
+  seguidores DESC,
+  artista ASC,
+  album ASC
+);
