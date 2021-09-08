737d5cc2536deb9f037268ffb8c9dde5fa6bfc41
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-08 20:46:44 -0300

    desafio11

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+CREATE VIEW cancoes_premium AS;
+SELECT S.song nome,
+COUNT(H.user_id) reproducoes
+FROM SpotifyClone.history H
+INNER JOIN SpotifyClone.songs S
+ON H.song_id = S.song_id
+INNER JOIN SpotifyClone.user_info U
+ON U.user_id = H.user_id
+WHERE U.plan_id <> 1
+GROUP BY S.song
+ORDER BY nome;
