5a52534fa5b74c8b45fb40cf2aa9ab20784a91c1
Cris Lustosa <cmlustosa@gmail.com>
2021-09-08 09:58:30 -0300

    Retira última linha do arquivo

--- desafio1.sql
+++ desafio1.sql
@@ -65,22 +65,22 @@ VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent',
 
 INSERT INTO cancoes (cancao_título, album_id)
 VALUES
-('Soul For Us', 1),
-('Reflections Of Magic', 1),
-('Dance With Her Own', 1),
-('Troubles Of My Inner Fire', 2 ),
-('Time Fireworks', 2),
-('Magic Circus', 3),
-('Honey, So Do I', 3),
-("Sweetie, Let's Go Wild", 3),
-('She Knows', 3),
-('Rock His Everything', 4),
-('Home Forever', 4),
-('Diamond Power', 4),
-("Honey, Let's Be Silly", 4),
-('Thang Of Thunder', 5),
-('Words Of Her Life', 5),
-('Without My Streets', 5);
+	('Soul For Us', 1),
+	('Reflections Of Magic', 1),
+	('Dance With Her Own', 1),
+	('Troubles Of My Inner Fire', 2 ),
+	('Time Fireworks', 2),
+	('Magic Circus', 3),
+	('Honey, So Do I', 3),
+	("Sweetie, Let's Go Wild", 3),
+	('She Knows', 3),
+	('Rock His Everything', 4),
+	('Home Forever', 4),
+	('Diamond Power', 4),
+	("Honey, Let's Be Silly", 4),
+	('Thang Of Thunder', 5),
+	('Words Of Her Life', 5),
+	('Without My Streets', 5);
 
 INSERT INTO historico_de_reproducoes (usuario_id, cancao_id)
 VALUES
@@ -101,12 +101,11 @@ VALUES
 
 INSERT INTO seguindo_artistas (usuario_id, artista_id)
 VALUES
-(1, 1),
-(1, 3),
-(1, 4),
-(2, 1),
-(2, 3),
-(3, 1),
-(3, 2),
-(4, 4);
-	
+	(1, 1),
+	(1, 3),
+	(1, 4),
+	(2, 1),
+	(2, 3),
+	(3, 1),
+	(3, 2),
+	(4, 4);	
