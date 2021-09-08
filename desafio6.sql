14260c862dfdfa2c381ef839457012e70c808a92
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-08 09:34:30 -0700

    fixing 6th

--- desafio6.sql
+++ desafio6.sql
@@ -4,6 +4,6 @@ MIN(p.plan_price) AS faturamento_minimo,
 MAX(p.plan_price) AS faturamento_maximo,
 ROUND(AVG(p.plan_price), 2) AS faturamento_medio,
 ROUND(SUM(p.plan_price), 2) AS faturamento_total
-FROM spotifyClone.plan AS p
-INNER JOIN spotifyClone.user AS u
+FROM plan AS p
+INNER JOIN user AS u
 ON u.plan_id = p.plan_id;
