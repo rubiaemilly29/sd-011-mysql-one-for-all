2886fe48b02eb99e5848687cc13be88f10bf39b0
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-08 16:48:05 -0300

    Ajuste requisito 9.

--- desafio9.sql
+++ desafio9.sql
@@ -1,18 +1,3 @@
---  Precisa ter um delimiter
---  nome CREATE PROCEDURE albuns_do_artista
---  Comando before ou after e onde ele é aplicado
---  Para quais linhas
---  Precisa ter begin
---  O comando e onde ele deve ser feito. Usar o id comparando o old. Repete quantas vezes for necessario
---  Comando end e o delimiter
--- volta o delimiter do jeito que era
--- 
---  Para chamar usamos o comando CALL + nome da procedure
---  AS procedures podem ter valores de entrada. PAra isso usamos dentro dos parametros () IN e o tipo ex: NUMERIC
---  AS procedures podem ter valores de saidas. PAra isso usamos dentro dos parametros () OUT e o nome da variavel de saida + tipo - EX DOUBLE
---  AS procedures podem ter valores de entrada e saidas. PAra isso usamos ambas as sintaxes
---  AS procedures podem ter valores de entrada e saidas juntos. PAra isso usamos o parametro INOUT com o nome da variavel + tipo 7
--- CALL albuns_do_artista('Walter Phoenix');
 DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista (IN nome VARCHAR(50))
