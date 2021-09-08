d6ca8ff38f7b66d9d68d2281a5f40d6bf285aabd
Renan <renan.vamo@gmail.com>
2021-09-08 12:51:43 -0300

    refatora o 10

--- desafio10.sql
+++ desafio10.sql
@@ -3,7 +3,7 @@ CREATE FUNCTION quantidade_musicas_no_historico(idDoUsuario INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE contagem INT;
-SELECT COUNT(usuario_id) FROM historico_reproducoes
+SELECT COUNT(*) FROM historico_reproducoes
 WHERE idDoUsuario = usuario_id INTO contagem;
 RETURN contagem;
 END //
