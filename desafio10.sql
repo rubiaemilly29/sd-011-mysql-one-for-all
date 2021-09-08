34e5112f338d4d8ea50b98acb5c0855fa06e6b05
Marcelo Almeida de Araujo <almeidadearaujomarcelo@gmail.com>
2021-09-08 01:17:36 -0300

    Adds desafio10.sql

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(search_user_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantidade_musicas_no_historico INT;
+SELECT
+COUNT(user_id)
+FROM History
+WHERE user_id = search_user_id INTO quantidade_musicas_no_historico;
+RETURN quantidade_musicas_no_historico;
+END $$
+DELIMITER ;
