0893bc1c82bfc511dfb9ed16d4403bd4826feb6c
Vinicius <viniciusfod@gmail.com>
2021-09-08 15:30:39 -0300

    req-6

---
+++ desafio6.sql
@@ -0,0 +1,17 @@
+CREATE VIEW faturamento_atual AS
+  SELECT
+    ROUND(MIN(valor), 2) AS 'faturamento_minimo',
+    ROUND(MAX(valor), 2) AS 'faturamento_maximo',
+    ROUND(AVG(valor), 2) AS 'faturamento_medio',
+    ROUND(SUM(valor), 2) AS 'faturamento_total'
+  FROM
+    (
+      SELECT
+        u.usuario_id,
+        p.valor
+      FROM
+        SpotifyClone.usuario AS u
+      INNER JOIN
+        SpotifyClone.plano AS p
+        ON p.plano_id = u.plano_id
+    ) AS faturamento;
