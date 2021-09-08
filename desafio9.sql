59947a799b49e75bd3f143a10edfaeee919a786b
Vinicius <viniciusfod@gmail.com>
2021-09-08 15:31:31 -0300

    req-9

---
+++ desafio9.sql
@@ -0,0 +1,21 @@
+SHOW COLUMNS FROM artista;
+
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(
+  IN artista_nome VARCHAR(60)
+)
+BEGIN
+  SELECT
+    at.nome_artistico AS 'artista',
+    ab.titulo AS 'album'
+  FROM
+    SpotifyClone.artista AS at
+  INNER JOIN
+    SpotifyClone.album AS ab
+    ON ab.artista_id = at.artista_id
+  WHERE
+    at.nome_artistico = artista_nome;
+END $$
+
+DELIMITER ;
