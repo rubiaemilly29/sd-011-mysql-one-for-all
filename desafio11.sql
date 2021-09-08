3dfdbf690d6ff59f2ae14d7ee6a70f435d54839e
Matheus Gaspar <msouzagaspar@gmail.com>
2021-09-08 22:20:26 +0200

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+CREATE VIEW cancoes_premium AS
+SELECT c.titulo, COUNT(*) FROM reproducoes AS r
+JOIN usuarios AS u ON u.id = r.usuario_id
+JOIN cancoes AS c ON c.id = r.cancao_id
+WHERE u.plano_id IN (2, 3)
+GROUP BY c.id
+ORDER BY c.titulo;
