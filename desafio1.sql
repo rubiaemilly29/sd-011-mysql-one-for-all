2c64ccb5ed0b44395c0493f26a67632d674f84d4
heyynat <natalifplima@gmail.com>
2021-09-08 12:33:10 -0400

    test

--- desafio1.sql
+++ desafio1.sql
@@ -54,3 +54,80 @@ CREATE TABLE seguindo_artistas(
   FOREIGN KEY (id_artista) REFERENCES artistas (id_artista),
   FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
 ) engine = InnoDB;
+
+INSERT INTO plano (nome_plano, valor)
+VALUES
+  ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
+
+INSERT INTO artista (nome_artista)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+
+INSERT INTO album (nome_album, id_artista)
+VALUES
+  ('Envious', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Exuberant', 1),
+  ('Temporary Culture', 4);
+
+INSERT INTO usuarios (nome_usuario, idade, id_plano)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+
+INSERT INTO cancao (nome_cancao, id_album)
+VALUES
+  ('Soul For Us', 1),
+  ('Thang Of Thunder', 5),
+  ('Troubles Of My Inner Fire', 2),
+  ('Without My Streets', 5),
+  ('Words Of Her Life', 5),
+  ('Time Fireworks', 2),
+  ('Honey, So Do I', 3),
+  ("Honey, Let's Be Silly", 4),
+  ('Magic Circus', 3),
+  ('Rock His Everything', 4);
+  ('Celebration Of More', 4),
+  ('Dance With Her Own', 1),
+  ('Reflections Of Magic', 1),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Diamond Power', 4),
+  ('Home Forever', 4),
+
+INSERT INTO historico_de_reproducoes (id_usuario, id_cancao)
+VALUES
+  (1, 8),
+  (1, 6),
+  (2, 4),
+  (2, 12),
+  (2, 7),
+  (2, 5),
+  (3, 10),
+  (3, 9),
+  (3, 6),
+  (4, 2),
+  (1, 3),
+  (1, 9),
+  (4, 11),
+  (4, 1);
+
+INSERT INTO seguindo_artistas (id_usuario, id_artista)
+VALUES
+  (1, 1),
+  (1, 4),
+  (3, 2),
+  (2, 1),
+  (2, 3),
+  (3, 1),
+  (1, 3),
+  (4, 4);
