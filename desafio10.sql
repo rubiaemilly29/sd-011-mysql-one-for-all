6efed6958a0991a1a11ab38eabd5226f846ea346
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-09-08 18:43:15 -0300

    vamos ver se vai

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(musicas INT)
+RETURN DOUBLE READS SQL DATA
+BEGIN
+    SELECT COUNT(*)
+    FROM SpotifyClone.UserSong
+    WHERE user_id = id_user INTO songs_total;
+    RETURN songs_total;
+END $$ 
+
+DELIMITER ;
