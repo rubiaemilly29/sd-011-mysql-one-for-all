5d8dcdbb77ce13ee40de23913eeaae2629fb647e
Vinicius <viniciusfod@gmail.com>
2021-09-08 15:31:16 -0300

    req-8

---
+++ desafio8.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+  BEFORE DELETE ON SpotifyClone.usuario
+  FOR EACH ROW
+BEGIN
+  DELETE FROM SpotifyClone.usuario_seguindo_artista
+    WHERE
+      usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.usuario_historico_reproducoes
+    WHERE
+      usuario_id = OLD.usuario_id;
+END $$
+
+DELIMITER ;
