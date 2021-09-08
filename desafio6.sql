a3e88c84ee14f9a2dfb7ffb30e3b1d9df4708d3a
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-08 09:41:02 -0700

    fixing 6th

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS
 SELECT
-MIN(p.plan_price) AS faturamento_minimo,
 MAX(p.plan_price) AS faturamento_maximo,
 ROUND(AVG(p.plan_price), 2) AS faturamento_medio,
+MIN(p.plan_price) AS faturamento_minimo,
 ROUND(SUM(p.plan_price), 2) AS faturamento_total
 FROM plan AS p
 INNER JOIN user AS u
