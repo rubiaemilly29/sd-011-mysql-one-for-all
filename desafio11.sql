384a32c8ea066d59d7ef9d52ed5f2f516c1d02d0
Matheus Gaspar <msouzagaspar@gmail.com>
2021-09-08 22:25:08 +0200

    correção desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 CREATE VIEW cancoes_premium AS
-SELECT c.titulo, COUNT(*) FROM reproducoes AS r
+SELECT c.titulo AS 'nome', COUNT(*) AS 'reproducoes' FROM reproducoes AS r
 JOIN usuarios AS u ON u.id = r.usuario_id
 JOIN cancoes AS c ON c.id = r.cancao_id
 WHERE u.plano_id IN (2, 3)
