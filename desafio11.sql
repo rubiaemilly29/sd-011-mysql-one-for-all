78fc2e02093a5af8df22e93d36992a121e5abcc6
Gustavo Lemes <gustavolemes997@gmail.com>
2021-09-08 13:08:52 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+CREATE VIEW cancoes_premium AS
+SELECT
+Sc.cancao,
+COUNT(Sh.cancao_id) AS reproducoes
+FROM SpotifyClone.cancao AS Sc
+INNER JOIN SpotifyClone.historico_reproducao AS Sh
+ON Sc.cancao_id = Sh.cancao_id
+INNER JOIN SpotifyClone.usuario AS Su
+ON Su.usuario_id = Sh.usuario_id 
+WHERE Su.plano_id IN (2, 3)
+GROUP BY Sh.cancao_id
+ORDER BY Sc.cancao;
