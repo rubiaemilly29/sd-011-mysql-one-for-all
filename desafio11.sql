fde44d63ae369bfc264254baf9a70f78914aa692
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-09-08 19:56:17 -0300

    ajusta desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,6 +4,6 @@ FROM Cancoes AS C
 INNER JOIN Historico_de_Reproducoes AS HR
   ON C.cancao_id = HR.cancao_id
 INNER JOIN Usuarios AS U
-  ON U.usuario_id = HR.usuario_id AND u.usuario_id IN (2, 3)
+  ON U.usuario_id = HR.usuario_id AND U.usuario_id IN (2, 3)
 GROUP BY nome
 ORDER BY nome;
