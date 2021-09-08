2347a45c0c6b86cbc8b05da6634df09a753989c1
Matheus Gaspar <msouzagaspar@gmail.com>
2021-09-08 22:11:08 +0200

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total_musicas INT;
+SELECT COUNT(*) FROM reproducoes
+WHERE usuario_id = user_id INTO total_musicas;
+RETURN total_musicas;
+END $$
+DELIMITER ;
