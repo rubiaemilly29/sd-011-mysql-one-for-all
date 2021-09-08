004de4c6a7e95e8bf178dc6cb8f165e92718c566
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-08 09:31:00 -0700

    fixing

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
-u.user_name AS 'usuario',
-s.song_name AS 'nome'
+s.song_name AS 'nome',
+u.user_name AS 'usuario'
 FROM user AS u
 INNER JOIN user_history AS uh ON u.user_id = uh.user_id
 INNER JOIN songs AS s ON uh.song_id = s.song_id
