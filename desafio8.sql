c5451ba35a85b3ca5e0b8a38d8a8112f2f0cc848
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-09-08 16:35:40 -0300

    Ajuste requisito 8.

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,3 @@
---  Precisa ter um delimiter
---  nome CREATE TRIGGER trigger_usuario_delete
---  Comando before ou after e onde ele é aplicado
---  Para quais linhas
---  Precisa ter begin
---  O comando e onde ele deve ser feito. Usar o id comparando o old. Repete quantas vezes for necessario
---  Comando end e o delimiter
--- volta o delimiter do jeito que era
--- DELETE FROm SpotifyClone.usuarios WHERE nome_usuario = 'Cintia';
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
