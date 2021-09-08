f2aa42c17b0e030058b7f2fe978e7af26d51404b
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-09-08 00:30:04 -0300

    realiza desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(50))
+BEGIN
+SELECT A.artista AS artista, Al.album AS album
+FROM Artistas AS A
+INNER JOIN Albuns AS Al
+  ON Al.artista_id = A.artista_id
+WHERE nome = A.artista
+ORDER BY artista;
+END $$
+DELIMITER ;
