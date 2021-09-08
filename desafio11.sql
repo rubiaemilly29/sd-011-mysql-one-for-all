26e500924ebf1c0d683df3280928ecda8113c727
Luan Xavier <luantenorio3@gmail.com>
2021-09-08 11:58:50 -0300

    Requisito 11: remover gambiarra

--- desafio11.sql
+++ desafio11.sql
@@ -2,6 +2,8 @@ CREATE VIEW cancoes_premium AS
 SELECT c.cancao AS nome, COUNT(h.cancao_id) AS reproducoes
 FROM SpotifyClone.cancao AS c
 INNER JOIN SpotifyClone.historico_de_reproducoes AS h
-ON c.cancao_id = h.cancao_id AND h.usuario_id <> 1 AND h.usuario_id <> 4
+ON c.cancao_id = h.cancao_id
+INNER JOIN SpotifyClone.usuario AS u
+ON h.usuario_id = u.usuario_id AND u.plano_id <> 1
 GROUP BY nome
 ORDER BY nome ASC;
