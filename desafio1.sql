b518e1468137bbd2a589ed2154624af05cf87e72
Cris Lustosa <cmlustosa@gmail.com>
2021-09-08 11:58:17 -0300

    Corrigindo desafio1, agora vai

--- desafio1.sql
+++ desafio1.sql
@@ -5,13 +5,13 @@ USE SpotifyClone;
 CREATE TABLE planos(
 plano_id INT PRIMARY KEY AUTO_INCREMENT,
 plano VARCHAR(50) NOT NULL,
-plano_valor DECIMAL(3,2) NOT NULL 
+valor_plano DECIMAL(3,2) NOT NULL 
 ) engine = InnoDB;
 
 CREATE TABLE usuarios(
 usuario_id INT PRIMARY KEY AUTO_INCREMENT,
 nome_usuario VARCHAR(50) NOT NULL,
-usuario_idade INT NOT NULL,
+idade INT NOT NULL,
 plano_id INT NOT NULL,
 FOREIGN KEY (plano_id) REFERENCES planos(plano_id)
 ) engine = InnoDB;
@@ -36,7 +36,7 @@ FOREIGN KEY (album_id) REFERENCES albuns(album_id)
 ) engine = InnoDB;
 
 CREATE TABLE seguindo_artistas(
-usario_id INT NOT NULL,
+usuario_id INT NOT NULL,
 artista_id INT NOT NULL,
 CONSTRAINT PRIMARY KEY (usuario_id, artista_id),
 FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
@@ -46,41 +46,41 @@ FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 CREATE TABLE historico_de_reproducoes(
 usuario_id INT NOT NULL,
 musica_id INT NOT NULL,
-CONSTRAINT PRIMARY KEY (usuario_id, cancao_id),
+CONSTRAINT PRIMARY KEY (usuario_id, musica_id),
 FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
 FOREIGN KEY (musica_id) REFERENCES musicas(musica_id)
 ) engine = InnoDB;
 
-INSERT INTO planos (plano_id, plano, valor_plano)
-VALUES (1, 'Familiar', 7.99), (2, 'Universitário', 5.99), (3, 'Gratuito', 0.00);
+INSERT INTO planos (plano, valor_plano)
+VALUES ('Familiar', 7.99), ('Universitário', 5.99), ('Gratuito', 0.00);
 
-INSERT INTO usuarios (usuario_id, nome_usuario, plano_id)
-VALUES (1, 'Thati', 23, 3), (2, 'Cintia', 35, 1), (3, 'Bill', 20, 2), (4, 'Roger', 45, 3);
+INSERT INTO usuarios (nome_usuario, idade, plano_id)
+VALUES ('Thati', 23, 3), ('Cintia', 35, 1), ('Bill', 20, 2), ('Roger', 45, 3);
 
-INSERT INTO artistas (artista_id, nome_artista)
-VALUES (1, 'Walter Phoenix'), (2, 'Peter Strong'), (3, 'Lance Day'), (4, 'Freedie Shannon');
+INSERT INTO artistas (nome_artista)
+VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
 
-INSERT INTO albuns (album_id, nome_album, artista_id)
-VALUES (1,'Envious', 1), (2,'Exuberant', 1), (3,'Hallowed Steam', 2), (4, 'Incandescent', 3), (5, 'Temporary Culture', 4);
+INSERT INTO albuns (nome_album, artista_id)
+VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
 
-INSERT INTO musicas (musica_id, nome_musica, album_id)
+INSERT INTO musicas (nome_musica, album_id)
 VALUES
-(1, 'Soul For Us', 1),
-(2, 'Reflections Of Magic', 1),
-(3, 'Dance With Her Own', 1),
-(4, 'Troubles Of My Inner Fire', 2 ),
-(5, 'Time Fireworks', 2),
-(6, 'Magic Circus', 3),
-(7, 'Honey, So Do I', 3),
-(8, "Sweetie, Let's Go Wild", 3),
-(9, 'She Knows', 3),
-(10, 'Rock His Everything', 4),
-(11, 'Home Forever', 4),
-(12, 'Diamond Power', 4),
-(13, "Honey, Let's Be Silly", 4),
-(14, 'Thang Of Thunder', 5),
-(15, 'Words Of Her Life', 5),
-(16, 'Without My Streets', 5);
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2 ),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
 
 INSERT INTO seguindo_artistas (usuario_id, artista_id)
 VALUES
