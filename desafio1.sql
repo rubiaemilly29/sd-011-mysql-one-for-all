0817df5dfd9f2bd9ebdef22fb96da8b20f0de05a
Matheus Gaspar <msouzagaspar@gmail.com>
2021-09-08 14:25:59 +0200

    correção desafio 3

--- desafio1.sql
+++ desafio1.sql
@@ -56,21 +56,21 @@ FOREIGN KEY (album_id) REFERENCES albums (id)
 INSERT INTO cancoes (titulo, album_id)
 VALUES
 ('Soul For Us', 1),
-('Reflections of Magic', 1),
+('Reflections Of Magic', 1),
 ('Dance With Her Own', 1),
-('Troubles of My Inner Fire', 2),
+('Troubles Of My Inner Fire', 2),
 ('Time Fireworks', 2),
 ('Magic Circus', 3),
 ('Honey, So Do I', 3),
 ("Sweetie, Let's Go Wild", 3),
 ("She Knows", 3),
 ("Fantasy For Me", 4),
-("Celebration of More", 4),
+("Celebration Of More", 4),
 ("Rock His Everything", 4),
 ("Home Forever", 4),
 ("Diamond Power", 4),
 ("Honey, Let's Be Silly", 4),
-("Thang of Thunder", 5),
+("Thang Of Thunder", 5),
 ("Words Of Her Life", 5),
 ("Without My Streets", 5);
 CREATE TABLE reproducoes (
