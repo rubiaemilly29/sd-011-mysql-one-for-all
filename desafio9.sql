e5a838de5cfe55016de86f7a72935001f604374f
israeltonholo <israeltonholo@hotmail.com>
2021-09-08 23:27:25 -0300

    corrige 9

--- desafio9.sql
+++ desafio9.sql
@@ -5,7 +5,7 @@ BEGIN
     SELECT AR.name AS artista, AL.name AS album
     FROM SpotifyClone.artists AS AR
     INNER JOIN SpotifyClone.albuns AS AL ON A.artist_id = AR.artist_id
-    WHERE ar.name = nome
+    WHERE AR.name = nome
     ORDER BY album;
 END $$
 
