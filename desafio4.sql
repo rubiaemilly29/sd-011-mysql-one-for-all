30ecf7568c0b496746aeeee6b80e200fcf0a84c4
LeoLacana <wilson17414@gmail.com>
2021-09-09 17:10:58 -0300

    requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS
+SELECT a.artista AS artista , COUNT(s.usuario_id) AS seguidores
+FROM SpotifyClone.artistas a
+INNER JOIN SpotifyClone.seguindo_artistas s
+ON s.artista_id = a.artista_id
+GROUP BY a.artista
+ORDER BY seguidores DESC
+LIMIT 3;
