527610f9c47b9a7a6814c871a0a5097178a563a8
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-09-08 23:51:42 -0300

    req11 completo

---
+++ desafio11.sql
@@ -0,0 +1,15 @@
+CREATE VIEW cancoes_premium AS
+SELECT
+C.song,
+COUNT(*)
+FROM
+SpotifyClone.Usuario AS U
+INNER JOIN
+SpotifyClone.Planos AS P ON U.plano_id = P.id
+INNER JOIN
+SpotifyClone.Historico_De_Reproducoes AS hdr ON U.id = hdr.usuario_id
+INNER JOIN
+SpotifyClone.Cancoes AS C ON hdr.cancoes_id = C.id
+WHERE P.tipo IN ('familiar', 'universitário')
+GROUP BY C.song
+ORDER BY C.song;
