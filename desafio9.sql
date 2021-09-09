1efedacca48917b6e4703016e71719b937c98ce8
israeltonholo <israeltonholo@hotmail.com>
2021-09-08 23:32:14 -0300

    corrige 9

--- desafio9.sql
+++ desafio9.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(100))
 BEGIN
     SELECT AR.name AS artista, AL.name AS album
     FROM SpotifyClone.artists AS AR
-    INNER JOIN SpotifyClone.albuns AS AL ON A.artist_id = AR.artist_id
+    INNER JOIN SpotifyClone.albuns AS AL ON AL.artist_id = AR.artist_id
     WHERE AR.name = nome
     ORDER BY album;
 END $$
