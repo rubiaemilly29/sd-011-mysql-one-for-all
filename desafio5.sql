ee7dd5c97fcc0ff7c2566acc75e4b9780a6f43f9
AlexSmith7192 <alexsmith7192@gmail.com>
2021-09-08 23:40:05 -0300

    5 - fix name column

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 CREATE VIEW top_2_hits_do_momento AS
-SELECT SongName AS cancoes,
+SELECT SongName AS cancao,
 COUNT(his.SongID) AS reproducoes
 FROM songs AS son
 INNER JOIN history AS his
 ON son.ID = his.SongID
 GROUP BY his.SongID
-ORDER BY reproducoes DESC, cancoes
+ORDER BY reproducoes DESC, cancao
 LIMIT 2;
