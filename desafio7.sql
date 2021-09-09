3a9447cb6c0a092e7245cea5a61ecc630bf914c8
Amandha W. Barok <amandha.barok@gmail.com>
2021-09-08 18:16:18 -0700

    agora vai

--- desafio7.sql
+++ desafio7.sql
@@ -7,4 +7,4 @@ FROM artist AS a
 INNER JOIN album AS alb ON a.artist_id = alb.artist_id
 INNER JOIN user_follow AS uf ON a.artist_id = uf.artist_id
 GROUP BY a.artist_name, alb.album_name
-ORDER BY COUNT(a.artist_name) DESC, alb.album_name;
+ORDER BY COUNT(a.artist_name) DESC, a.artist_name, alb.album_name;
