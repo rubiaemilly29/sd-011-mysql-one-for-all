03d9888bc79b5170595684495a99d24a7f873907
AlexSmith7192 <alexsmith7192@gmail.com>
2021-09-08 23:17:00 -0300

    4 - resolves chanllenge four

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+CREATE VIEW top_3_artistas AS
+SELECT art.ArtistName AS artista,
+COUNT(fol.ArtistID) AS seguidores
+FROM artists AS art
+INNER JOIN following AS fol
+ON art.ID = fol.ArtistID
+GROUP BY art.ID
+ORDER BY seguidores DESC
+LIMIT 3;
