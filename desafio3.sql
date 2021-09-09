e3de7c950a53d6e2fbe20f8c49cda7e914af8ada
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-08 22:50:34 -0300

    fix: req 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
-u.usuario_nome AS 'usuario',
-c.cancao_nome AS 'nome' 
+u.usuario_nome AS usuario,
+c.cancao_nome AS nome 
 FROM SpotifyClone.users AS u
 INNER JOIN SpotifyClone.historico_reproducoes AS h ON h.usuario_id = u.usuario_id
 INNER JOIN SpotifyClone.cancoes AS c ON c.cancao_id = h.cancao_id
