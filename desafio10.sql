462aba07951f33ae4eb508aa7dd7fac05373e1da
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-09 09:01:41 -0300

    add: req 10

---
+++ desafio10.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+CREATE FUNCTION quantidade_musicas_no_historico(usuario VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE cancao_total INT;
+SELECT 
+COUNT(hist.cancao_id) AS quantidade_musicas_no_historico
+FROM SpotifyClone.historico_reproducoes AS his
+INNER JOIN SpotifyClone.users AS u
+ON u.usuario_id = his.usuario_id
+WHERE usuario = u.usuario_nome;
+RETURN cancao_total;
+END $$
+DELIMITER ;
