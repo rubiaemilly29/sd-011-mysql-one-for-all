2f7601828cbb910f98a3515bce2d8989d50f531b
jorgemeyrelles <jotaengpuc@gmail.com>
2021-09-09 16:17:02 -0300

    fix: inserindo valores da tabela

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE IF NOT EXISTS SpotifyClone;
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
 
 USE SpotifyClone;
 
@@ -9,10 +11,10 @@ artist_name VARCHAR(50) NOT NULL
 
 INSERT INTO artists(artist_name)
 VALUES
-('Zeca Pagodinho'),
-('Jovelina Perola Negra'),
-('Wando'),
-('Fagner');
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
 
 CREATE TABLE plan(
 id INT PRIMARY KEY AUTO_INCREMENT,
@@ -22,9 +24,9 @@ price DOUBLE(4, 2) NOT NULL
 
 INSERT INTO plan(plan_name, price)
 VALUES
-('Gold', 5.90),
-('Silver', 4.90),
-('Bronze', 3.90);
+('gratuito', 0),
+('universitário', 5.99),
+('familiar', 7.99);
 
 CREATE TABLE users(
 id INT PRIMARY KEY AUTO_INCREMENT,
@@ -36,10 +38,10 @@ FOREIGN KEY (plan_id) REFERENCES plan(id)
 
 INSERT INTO users(user_name, age, plan_id)
 VALUES
-('João', 48, 1),
-('Maria', 35, 2),
-('Francisco', 18, 3),
-('Ligia', 23, 2);
+('Thati', 23, 1),
+('Cintia', 35, 3),
+('Bill', 20, 2),
+('Roger', 45, 1);
 
 CREATE TABLE album(
 id INT PRIMARY KEY AUTO_INCREMENT,
@@ -50,11 +52,11 @@ FOREIGN KEY (artist_id) REFERENCES artists(id)
 
 INSERT INTO album(album_name, artist_id)
 VALUES
-('Ser humano', 1),
-('Roda de samba', 2),
-('Você é luz', 3),
-('Romance no deserto', 4),
-('Sangue bom', 2);
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
 CREATE TABLE songs(
 id 	INT PRIMARY KEY AUTO_INCREMENT,
@@ -65,16 +67,24 @@ FOREIGN KEY (album_id) REFERENCES album(id)
 
 INSERT INTO songs(song_name, album_id)
 VALUES
-('Mangas e panos', 1),
-('Samba na cozinha', 1),
-('Luz do repente', 2),
-('Sorriso aberto', 2),
-('No mesmo manto', 2),
-('Confusão na horta', 2),
-('Emoções', 3),
-('Senhorita', 3),
-('Deslizes', 4),
-('Lua no Leblon', 4);
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+('Sweetie, Let''s Go Wild', 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+('Honey, Let''s Be Silly', 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
 
 CREATE TABLE historic(
 user_id INT NOT NULL,
@@ -86,7 +96,8 @@ FOREIGN KEY (user_id) REFERENCES users(id)
 
 INSERT INTO historic(user_id, song_id)
 VALUES
-(1, 1), (1, 6), (2, 10), (2, 8), (3, 4), (3, 5), (4, 3), (4, 7), (1, 9), (2, 6), (3, 7), (4, 8), (2, 1);
+(1, 1), (1, 6), (1, 14), (1, 16), (2, 13), (2, 17), (2,2),
+(2, 15), (3, 4), (3, 16), (3, 6), (4, 3), (4, 18), (4, 11);
 
 CREATE TABLE followers(
 user_id INT NOT NULL,
@@ -98,4 +109,4 @@ FOREIGN KEY (user_id) REFERENCES users(id)
 
 INSERT INTO followers(user_id, artist_id)
 VALUES
-(1, 4), (1, 2), (2, 4), (2, 1), (3, 1), (3, 4), (4, 2), (4, 1);
+(1, 1),(1, 3),(1, 4),(2, 1),(2, 3),(3, 2),(3, 1),(4, 4);
