746037f1791d6789be49e02c2175cfff4bdd5689
Michel Pereira <michelbecauro@gmail.com>
2021-09-09 06:15:50 -0300

    Muda alias e add comentario no requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,10 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(c.cancao) AS cancoes, COUNT(DISTINCT at.artista) AS artistas, 
-COUNT(DISTINCT ab.album) AS albuns 
+COUNT(DISTINCT al.album) AS albuns 
 FROM SpotifyClone.cancao AS c
-INNER JOIN SpotifyClone.album AS ab
-ON c.album_id = ab.album_id
+INNER JOIN SpotifyClone.album AS al
+ON c.album_id = al.album_id
 INNER JOIN SpotifyClone.artista AS at
 ON at.artista_id = ab.artista_id;
+
+-- Alternativamente poderia usar subqueries ao invés de Joins.
