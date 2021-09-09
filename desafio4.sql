9e7f6b8ef3a39d72740559aa2e6cabc5f0bfc42e
Cris Lustosa <cmlustosa@gmail.com>
2021-09-09 20:47:31 -0300

    Adiciona desafio4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS
+SELECT a.nome_artista AS artista, COUNT(sa.usuario_id) AS seguidores
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.seguindo_artistas AS sa
+ON a.artista_id = sa.artista_id
+GROUP BY `artista`
+ORDER BY `seguidores` DESC, `artista`
+LIMIT 3;
