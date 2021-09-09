c64f3e934a63921faf6c1a29da931a950449c456
DADA <davimarque2000@gmail.com>
2021-09-09 16:30:24 -0300

    desafio 8 CORRIGIDO

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,9 @@
 DELIMITER $$
-
 CREATE TRIGGER trigger_usuario_delete
   BEFORE DELETE ON SpotifyClone.usuarios
   FOR EACH ROW
 BEGIN
-  DELETE FROM SpotifyClone.historico_reproducao
-  WHERE OLD.usuario_id = usuario_id;
-  DELETE FROM SpotifyClone.artistas_seguidores
-  WHERE OLD.usuario_id = usuario_id;
+  DELETE FROM SpotifyClone.seguidores WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.historico_reproducao WHERE usuario_id = OLD.usuario_id;
 END $$
-DELIMITER;
+DELIMITER ;
