99ab29b176e618a770026edc1b0e68a195de6436
AlexSmith7192 <alexsmith7192@gmail.com>
2021-09-09 17:10:19 -0300

    11 - fix where

--- desafio11.sql
+++ desafio11.sql
@@ -4,5 +4,8 @@ COUNT(his.SongID) AS reproducoes
 FROM songs AS son
 INNER JOIN history AS his
 ON son.ID = his.SongID
+INNER JOIN users AS us
+ON his.UserID = us.ID
+WHERE us.PlanID IN (2, 3)
 GROUP BY his.SongID, nome
 ORDER BY nome;
