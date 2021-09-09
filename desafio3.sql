9a30af1d12ee0fd7c488fce2f33b5b550350210e
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-08 23:15:49 -0300

    add: req 6

--- desafio3.sql
+++ desafio3.sql
@@ -1,20 +1,8 @@
--- CREATE VIEW historico_reproducao_usuarios AS
--- SELECT 
--- u.usuario_nome AS usuario,
--- c.cancao_nome AS nome 
--- FROM SpotifyClone.users AS u
--- INNER JOIN SpotifyClone.historico_reproducoes AS h ON h.usuario_id = u.usuario_id
--- INNER JOIN SpotifyClone.cancoes AS c ON c.cancao_id = h.cancao_id
--- ORDER BY usuario_nome ASC, cancao_nome ASC;
-
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT
-Su.usuario,
-Sc.cancao AS nome
-FROM SpotifyClone.usuario AS Su
-INNER JOIN SpotifyClone.historico_reproducao AS Sh
-ON Su.usuario_id = Sh.usuario_id
-INNER JOIN SpotifyClone.cancao AS Sc
-ON Sc.cancao_id = Sh.cancao_id
-ORDER BY Su.usuario, Sc.cancao;
-
+SELECT 
+u.usuario_nome AS usuario,
+c.cancao_nome AS nome 
+FROM SpotifyClone.users AS u
+INNER JOIN SpotifyClone.historico_reproducoes AS h ON h.usuario_id = u.usuario_id
+INNER JOIN SpotifyClone.cancoes AS c ON c.cancao_id = h.cancao_id
+ORDER BY usuario_nome ASC, cancao_nome ASC;
---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW faturamento_atual AS
+SELECT
+MIN(p.plano_valor) AS faturamento_minimo,
+MAX(p.plano_valor) AS faturamento_maximo,
+ROUND(AVG(p.plano_valor), 2) AS faturamento_medio,
+SUM(p.plano_valor) AS faturamento_total
+FROM SpotifyClone.planos AS p
+INNER JOIN SpotifyClone.users AS u ON u.plano_id = p.plano_id;
