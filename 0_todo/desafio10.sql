4108f08817a642bdf56d9c14dc876572e55b309b
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-09 11:46:39 -0300

    desafio10

--- 0_todo/desafio10.sql
+++ 0_todo/desafio10.sql
@@ -1,15 +1,13 @@
 DELIMITER $$
 
-CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico
-(@id_user)
+CREATE FUNCTION SpotifyClone.quantidade_musicas_no_historico(id_user INT)
 RETURNS INT READS SQL DATA
 BEGIN
-    DECLARE total_songs INT;
-    SELECT COUNT(song_id) FROM SpotifyClone.history
-    WHERE user_id = @id_user
-    RETURN resultado_a_ser_retornado
-    INTO total_songs;
-    RETURN total_songs;
+DECLARE total_songs INT;
+SELECT COUNT(song_id) FROM SpotifyClone.history
+WHERE user_id = id_user
+INTO total_songs;
+RETURN total_songs;
 END $$
 
 DELIMITER ;
