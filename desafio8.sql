6296a8af8fc5520e48da6dac0ad023ec091ae9be
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-09 10:55:57 -0300

    desafio8

--- desafio8.sql
+++ desafio8.sql
@@ -7,7 +7,7 @@ BEGIN
 DELETE FROM SpotifyClone.history
 WHERE user_id = :old.user_id;
 DELETE FROM SpotifyClone.user_following
-WHERE user_id = :old.user_id;
+WHERE user_id = old.user_id;
 END $$
 
 DELIMITER ;
