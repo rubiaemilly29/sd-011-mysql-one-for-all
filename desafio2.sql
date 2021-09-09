64aa657bcd957e6857a292ace00eb8058d0e8510
Michel Pereira <michelbecauro@gmail.com>
2021-09-09 06:30:37 -0300

    Corrige erro bôbo no requisito 2 - Esqueci de renomear um Alias no ON

--- desafio2.sql
+++ desafio2.sql
@@ -2,7 +2,8 @@ CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(c.cancao) AS cancoes, COUNT(DISTINCT at.artista) AS artistas, 
 COUNT(DISTINCT al.album) AS albuns 
 FROM SpotifyClone.cancao AS c
-INNER JOIN SpotifyClone.album AS al -- Alternativamente poderia usar subqueries ao invés de Joins.
+INNER JOIN SpotifyClone.album AS al
 ON c.album_id = al.album_id
 INNER JOIN SpotifyClone.artista AS at
-ON at.artista_id = ab.artista_id;
+ON at.artista_id = al.artista_id;
+-- Alternativamente poderia usar subqueries ao invés de Joins.
