bb86827f8a93c115afd88aa124cf3c987b1d343e
israeltonholo <israeltonholo@hotmail.com>
2021-09-08 23:06:13 -0300

    corrige 7

--- desafio7.sql
+++ desafio7.sql
@@ -3,7 +3,7 @@ SELECT A.name AS artista, AL.name AS album, COUNT(*) AS seguidores
 FROM SpotifyClone.albuns AS AL
 INNER JOIN SpotifyClone.artists AS A
 ON AL.artist_id = A.artist_id
-INNER JOIN SpotifyClone.users_artists AS U
+INNER JOIN SpotifyClone.users_follow_artists AS U
 ON U.artist_id = A.artist_id
 GROUP BY AL.album_id
 ORDER BY seguidores DESC, artista, album;
