db7813d50497253467ac30aa14558070fc59426c
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-09 10:36:27 -0300

    add: req 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,11 @@
--- DELIMITER $$
--- CREATE TRIGGER trigger_usuario_delete
--- AFTER DELETE ON users
--- FOR EACH ROW
--- BEGIN
--- INSERT INTO
--- VALUES
--- END $$
--- DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON users
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.historico_reproducoes
+WHERE OLD.usuario_id = usuario_id;
+DELETE FROM SpotifyClone.seguindo_artistas
+WHERE OLD.usuario_id = usuario_id;
+END $$
+DELIMITER ;
