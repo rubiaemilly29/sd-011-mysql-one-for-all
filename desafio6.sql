4e67b376268f173bfa0b96f65dc4f8bc6c8f326a
DADA <davimarque2000@gmail.com>
2021-09-09 10:02:16 -0300

    correçao do desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-REATE VIEW faturamento_atual AS SELECT
+CREATE VIEW faturamento_atual AS SELECT
   ROUND(MIN(p.valor_plano), 2) AS faturamento_minimo,
   ROUND(MAX(p.valor_plano), 2) AS faturamento_maximo,
 ROUND(AVG(p.valor_plano), 2) AS faturamento_medio,
