20d5ab9790aad6824ecd49956cefbefde208ee09
israeltonholo <israeltonholo@hotmail.com>
2021-09-08 22:30:20 -0300

    corrige 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,7 @@
-CREATE VIEW estatisticas_musicais AS (
-  SELECT (SELECT COUNT(*) FROM SpotifyClone.songs) AS cancoes,
-    (SELECT COUNT(*) FROM SpotifyClone.artist) AS artistas,
-    (SELECT COUNT(*) FROM SpotifyClone.albuns) AS albuns
-);
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(songs.song_id) AS cancoes, MAX(artists.artist_id) AS artistas, MAX(alb.album_id) AS albuns
+FROM SpotifyClone.songs AS songs
+INNER JOIN SpotifyClone.artists AS artists
+ON artists.artist_id = songs.artist_id
+INNER JOIN SpotifyClone.albuns AS alb
+ON alb.album_id = songs.album_id;
