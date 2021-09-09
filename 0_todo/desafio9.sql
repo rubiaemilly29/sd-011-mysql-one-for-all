2c224acd5d1520bc9129b7959cabd4cc80517111
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-09-09 10:49:22 -0300

    desafio9

--- 0_todo/desafio9.sql
+++ 0_todo/desafio9.sql
@@ -1,12 +1,15 @@
+USE SpotifyClone;
 DELIMITER $$
 
-CREATE PROCEDURE SpotifyClone.albuns_do_artista(IN @artist_name VARCHAR(200))
+CREATE PROCEDURE SpotifyClone.albuns_do_artista(IN artist_name VARCHAR(200))
 BEGIN
-SELECT T.artist, A.album
+SELECT T.artist artista, A.album album
 FROM SpotifyClone.albums A
 INNER JOIN SpotifyClone.artists T
 ON T.artist_id = A.artist_id
-WHERE T.artist = @artist_name;
+WHERE T.artist = artist_name;
 END $$
 
 DELIMITER ;
+
+CALL albuns_do_artista('Walter Phoenix');
