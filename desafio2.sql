661a03543c173375dac451fae18128aef35de65f
Juliana Happatsch <jhappatsch@gmail.com>
2021-09-09 19:03:45 -0300

    req 2 - corrigindo erro

--- desafio2.sql
+++ desafio2.sql
@@ -2,4 +2,4 @@ CREATE VIEW estatisticas_musicais AS
 SELECT
 (SELECT COUNT(*) FROM SpotifyClone.songs) AS cancoes,
 (SELECT COUNT(*) FROM SpotifyClone.singers) AS artistas,
-(SELECT COUNT(*) FROM SpotifyClone.albums) AS albums;
+(SELECT COUNT(*) FROM SpotifyClone.albums) AS albuns;
