bfd9cfa80f239a0bda7243e5e0daec2af1542c6a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-09-10 00:19:53 -0300

    Correction of select for view

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
-select Count(s.title) AS cancoes, 
-Count(DISTINCT a.artistId) AS artistas, Count(DISTINCT s.albumId) AS albuns
-from SpotifyClone.songs As s
+CREATE VIEW estatisticas_musicais AS
+SELECT Count(s.title) AS cancoes, Count(DISTINCT a.artistId) AS artistas, Count(DISTINCT s.albumId) AS albuns
+FROM SpotifyClone.songs As s
 INNER JOIN SpotifyClone.albums AS a
 ON s.albumId = a.albumId;
