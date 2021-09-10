e88f7456b8f58f239e34b285a34c98d0bdac19aa
anacarol-r-santos <kroline0693@gmail.com>
2021-09-10 11:33:08 -0300

    desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,8 @@
-SELECT * FROM sakila.actor;
+CREATE VIEW estatisticas_musicais AS
+SELECT
+COUNT(c.*) cancoes,
+COUNT(a.*) artistas,
+COUNT(ab.*) albuns
+FROM SpotifyClone.Cancoes c 
+INNER JOIN SpotifyClone.Albuns ab ON c.album_id = ab.album_id
+INNER JOIN SpotifyClone.Artistas a ON a.artista_id = ab.artista_id;
