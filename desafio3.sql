bd4e33730231b345297788f6c714140086474b8a
gab-boff <gmeninboff@gmail.com>
2021-09-09 22:33:25 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+u.nome_usuario AS 'usuario',
+c.cancao AS 'nome'
+FROM SpotifyClone.historico_de_reproducoes AS r
+JOIN SpotifyClone.usuarios AS u ON r.usuario_id = u.usuario_id
+JOIN SpotifyClone.cancoes AS c ON r.cancao_id = c.cancao_id
+ORDER BY nome, nome;
