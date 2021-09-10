4b1f38473a96f17655106dabc781abc7d24716bb
Marconi <marconieng@gmail.com>
2021-09-09 23:18:22 -0300

    Adiciona desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,12 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT 
+c.cancao AS "cancao",
+COUNT(h.cancao_id) AS "reproducoes"
+FROM SpotifyClone.cancoes AS c
+
+INNER JOIN SpotifyClone.historico_musical AS h
+ON c.cancao_id = h.cancao_id
+
+GROUP BY `cancao`
+ORDER BY `reproducoes` DESC, `cancao` ASC
+LIMIT 2;
