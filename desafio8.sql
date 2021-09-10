8742da2c0bb5687cf3c71111a47dce8418d9a98d
Reginara Gomes <refagomes@gmail.com>
2021-09-10 00:07:13 -0300

    Adiciona req 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.historico_reproducoes
+WHERE OLD.usuario_id = usuario_id;
+DELETE FROM SpotifyClone.artistas_seguidores
+WHERE OLD.usuario_id = usuario_id;
+END $$
+
+DELIMITER ;
