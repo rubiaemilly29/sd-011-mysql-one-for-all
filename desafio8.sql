9230dac22b5b12d7d5d2491724f3bdf799c07ff1
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-09-10 03:25:28 -0400

    resolved req: 08 a 09 codeclimate 02

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON users
 FOR EACH ROW
 BEGIN
-	DELETE FROM SpotifyClone.follow WHERE usuario_id = OLD.usuario_id;
+DELETE FROM SpotifyClone.follow WHERE usuario_id = OLD.usuario_id;
   DELETE FROM SpotifyClone.history WHERE usuario_id = OLD.usuario_id;
 END $$
 
--- desafio9.sql
+++ desafio9.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE albuns_do_artista(IN artist VARCHAR(50))
 BEGIN
 SELECT
 ar.artist_name AS 'artista',
-		a.album_name AS 'album'
+	a.album_name AS 'album'
     FROM SpotifyClone.album AS a
     INNER JOIN SpotifyClone.artist AS ar ON a.artist_id = ar.artist_id
     WHERE ar.artist_name = artist;
