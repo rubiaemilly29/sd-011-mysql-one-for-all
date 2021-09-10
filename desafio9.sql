d5265bfeb11bfaa70efeeef49dad308ef9ea94e8
Juliana Happatsch <jhappatsch@gmail.com>
2021-09-09 22:05:26 -0300

    corrigindo req 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,3 @@
-USE banco_de_dados;
 DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN singer VARCHAR(100))
@@ -7,6 +6,7 @@ SELECT s.name AS artista, a.title AS album
 FROM SpotifyClone.singers s
 INNER JOIN SpotifyClone.albums a
 ON s.singer_id = a.singer_id
+WHERE s.name = singer
 ORDER BY album;
 END $$
 
