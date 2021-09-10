11d5c3c85f5148bc1472b7726728dcbf40208ba7
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-09-09 22:45:56 -0300

    desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatisticas_musicais AS 
+SELECT
+(SELECT COUNT(cancao) FROM SpotifyClone.cancao) AS cancoes,
+(SELECT COUNT(artista) FROM SpotifyClone.artista) AS artistas,
+(SELECT COUNT(album) FROM SpotifyClone.album) AS albuns;
