43e72f3b028932b40f7bf8b8f0d85c88f72e0391
LeoLacana <wilson17414@gmail.com>
2021-09-10 01:41:43 -0300

    requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_hisorico(nome_usuario INT)
+CREATE FUNCTION quantidade_musicas_no_historico(nome_usuario INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE result_musics INT;
-SELECT COUNT(cancao_id)
+SELECT COUNT(*) AS quantidade_musicas_no_historico
 FROM SpotifyClone.historico_de_reproducao
 WHERE usuario_id = nome_usuario INTO result_musics;
 RETURN result_musics;
