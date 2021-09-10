4ae83802592771cbbe7916a4e2287efc4beecd7f
Juliana Happatsch <jhappatsch@gmail.com>
2021-09-09 21:08:57 -0300

    corrigindo codeclimate

--- desafio6.sql
+++ desafio6.sql
@@ -4,5 +4,3 @@ SELECT
 (SELECT MAX(price) FROM SpotifyClone.plans) AS faturamento_maximo,
 (SELECT ROUND(AVG(price), 2) FROM SpotifyClone.plans) AS faturamento_medio,
 (SELECT SUM(price) FROM SpotifyClone.plans) AS faturamento_total;
-
-SELECT * FROM faturamento_atual;
