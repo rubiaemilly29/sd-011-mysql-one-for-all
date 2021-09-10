03c3cdae82be625b82b41bd03acc4b96dc3c45f2
Marconi <marconieng@gmail.com>
2021-09-10 00:14:03 -0300

    Inserindo comando round nos valores min e max e as 2 casas apos a virgula

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 CREATE VIEW faturamento_atual AS
 SELECT 
-ROUND(MIN(plano_valor)) AS "faturamento_minimo",
-ROUND(MAX(plano_valor)) AS "faturamento_maximo",
+ROUND(MIN(plano_valor), 2) AS "faturamento_minimo",
+ROUND(MAX(plano_valor), 2) AS "faturamento_maximo",
 ROUND(AVG(plano_valor), 2) AS "faturamento_medio",
 ROUND(SUM(plano_valor), 2) AS "faturamento_total"
 FROM (
