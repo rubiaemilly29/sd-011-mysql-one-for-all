4139f3d70c9d6f4bf3bee186c673d200dce9ed04
JoaoCabrera <joaocabrerati@gmail.com>
2021-09-10 01:44:36 -0300

    Adiciona requisito 9

---
+++ requisito9.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(50))
+BEGIN
+SELECT a.artista AS 'artista',
+al.album AS 'album'
+FROM SpotifyClone.artistas AS a
+INNER JOIN albuns AS al
+ON a.artista_id = al.artista_id
+WHERE artista = a.artista;
+END $$
+DELIMITER ;
