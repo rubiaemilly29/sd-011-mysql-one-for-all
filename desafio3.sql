969b891acdc0608a1bc5d6124fff56808cb2215c
gab-boff <gmeninboff@gmail.com>
2021-09-09 22:53:53 -0300

    Corrigindo Desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -5,4 +5,4 @@ c.cancao AS 'nome'
 FROM SpotifyClone.historico_de_reproducoes AS r
 JOIN SpotifyClone.usuarios AS u ON r.usuario_id = u.usuario_id
 JOIN SpotifyClone.cancoes AS c ON r.cancao_id = c.cancao_id
-ORDER BY nome, nome;
+ORDER BY u.nome_usuario ASC, c.cancao ASC;
