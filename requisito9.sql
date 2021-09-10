7b43360cd2e2f6424490a71287e51bddf6eb6696
JoaoCabrera <joaocabrerati@gmail.com>
2021-09-10 02:08:29 -0300

    corrige novamente

--- requisito9.sql
+++ requisito9.sql
@@ -1,12 +1,12 @@
 DELIMITER $$
-CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(50))
+CREATE PROCEDURE albuns_do_artista(IN artista_nome VARCHAR(50))
 BEGIN
 SELECT a.artista AS 'artista',
 al.album AS 'album'
 FROM SpotifyClone.artistas AS a
-INNER JOIN albuns AS al
+INNER JOIN SpotifyClone.albuns AS al
 ON a.artista_id = al.artista_id
-WHERE artista = a.artista
+WHERE artista_nome = a.artista
 ORDER BY al.album;
 END $$
 DELIMITER ;
