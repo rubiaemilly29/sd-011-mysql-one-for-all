8a1d76e389cd1a9050e556e95e08f7bd2b65b52b
Aline <aline.melryaa@gmail.com>
2021-09-10 08:11:42 -0400

    req11-refatorando

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,10 @@
 CREATE VIEW cancoes_premium AS
 SELECT musica.nome_musica AS nome,
-COUNT(*) AS reproducoes
+COUNT(historico_tocadas.fk_musica_id) AS reproducoes
 FROM SpotifyClone.musica AS musica
-INNER JOIN SpotifyClone.historico_tocadas AS hist
-ON hist.fk_musica_id = musica.musica_id
+INNER JOIN SpotifyClone.historico_tocadas AS historico_tocadas
+ON musica.musica_id = historico_tocadas.fk_musica_id
 INNER JOIN SpotifyClone.usuario AS u
-ON u.usuario_id = hist.fk_musica_id 
+ON u.usuario_id = historico_tocadas.fk_usuario_id 
 AND u.plano_id IN(2,3)
-GROUP BY musica.nome_musica ORDER BY musica.nome_musica;
+GROUP BY historico_tocadas.fk_musica_id ORDER BY musica.nome_musica;
