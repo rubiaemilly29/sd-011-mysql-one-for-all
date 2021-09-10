5c92ae5d2baf84c091f96fe40c9f62bbbf8925c3
theusseveen22 <mateus.krauss@gmail.com>
2021-09-10 12:54:14 -0300

    Adiciona requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 create view faturamento_atual as
 SELECT
-MIN(planos.valor_plano) AS faturamento_minimo,
-MAX(planos.valor_plano) AS faturamento_maximo,
-cast(ROUND(AVG(planos.valor_plano), 2) as decimal(5,2)) AS faturamento_medio,
-SUM(planos.valor_plano) AS faturamento_total
+MIN(planos.valor_plano) AS "faturamento_minimo",
+MAX(planos.valor_plano) AS "faturamento_maximo",
+cast(ROUND(AVG(planos.valor_plano), 2) as decimal(5,2)) AS "faturamento_medio",
+SUM(planos.valor_plano) AS "faturamento_total"
 FROM planos
 INNER JOIN usuario
 ON planos.plano_id = usuario.plano_id;
