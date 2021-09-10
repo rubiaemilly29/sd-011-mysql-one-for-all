9f17073bcc0624bf2716e20fa1921f3743a9de45
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-09-09 23:37:50 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+CREATE VIEW cancoes_premium AS
+SELECT
+Sc.cancao AS nome,
+COUNT(Sh.cancao_id) AS reproducoes
+FROM SpotifyClone.cancao AS Sc
+INNER JOIN SpotifyClone.historico_de_reproducoes AS Sh
+ON Sc.cancao_id = Sh.cancao_id
+INNER JOIN SpotifyClone.usuario AS Su
+ON Su.usuario_id = Sh.usuario_id 
+WHERE Su.plano_id IN (2, 3)
+GROUP BY Sh.cancao_id
+ORDER BY Sc.cancao;
