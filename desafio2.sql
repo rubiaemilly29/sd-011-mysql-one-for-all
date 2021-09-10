8abb598ea4b817b78a257c26a03e56a53da861ce
anacarol-r-santos <kroline0693@gmail.com>
2021-09-10 11:41:12 -0300

    desafio 2 correto

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,5 @@
 CREATE VIEW estatisticas_musicais AS
-SELECT
-COUNT(c.*) cancoes,
-COUNT(a.*) artistas,
-COUNT(ab.*) albuns
-FROM SpotifyClone.Cancoes c 
-INNER JOIN SpotifyClone.Albuns ab ON c.album_id = ab.album_id
-INNER JOIN SpotifyClone.Artistas a ON a.artista_id = ab.artista_id;
+SELECT COUNT(title) cancoes,
+(SELECT COUNT(artista) FROM SpotifyClone.Artistas) artistas,
+(SELECT COUNT(album) FROM SpotifyClone.Albuns) albuns
+FROM SpotifyClone.Cancoes;
