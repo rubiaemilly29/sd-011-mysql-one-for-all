f8a31bc4cf5ab5e0e1401c00361d8b9846b3fce8
Marconi <marconieng@gmail.com>
2021-09-09 21:32:42 -0300

    Corrigindo erro ortografico no desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -61,9 +61,9 @@ INSERT INTO albuns(album, artista_id)
 VALUES ("Envious", 1), ("Exuberant", 1), ("Hallowed Steam", 2), ("Incandescent", 3), ("Temporary Culture", 4);
 
 INSERT INTO cancoes(cancao, album_id)
-VALUES ("Soul For Us", 1), ("Reflections For Magic", 1), ("Dance With Her Own", 1), ("Troubles Of My Inner Fire", 2),
+VALUES ("Soul For Us", 1), ("Reflections Of Magic", 1), ("Dance With Her Own", 1), ("Troubles Of My Inner Fire", 2),
 ("Time Fireworks", 2), ("Magic Circus", 3), ("Honey, So Do I", 3), ("Sweetie, Let's Go Wild", 3), ("She Knows", 3),
-("Fantasy For Me", 4), ("Celebration Of More", 4), ("Rock Is Everything", 4), ("Home For Ever", 4),
+("Fantasy For Me", 4), ("Celebration Of More", 4), ("Rock Is Everything", 4), ("Home Forever", 4),
 ("Diamond Power", 4), ("Honey, Let's Be Silly", 4), ("Thang Of Thunder", 5), ("Words Of Her Life", 5),
 ("Without My Streets", 5);
 
