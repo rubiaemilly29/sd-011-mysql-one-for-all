6a210ba8558a708329133094b5f2cde6beed7161
JoaoCabrera <joaocabrerati@gmail.com>
2021-09-10 00:24:28 -0300

    corrige requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,8 @@
-SELECT c.cancao AS 'cancao', COUNT(h.cancao_id) AS 'reproducoes'
+CREATE VIEW top_2_hits_do_momento AS
+SELECT c.cancao AS 'cancao', COUNT(h.usuario_id) AS 'reproducoes'
 FROM SpotifyClone.cancoes AS c
 INNER JOIN SpotifyClone.historico_de_reproducoes AS h
 ON h.cancao_id = c.cancao_id
 GROUP BY h.cancao_id
-ORDER BY COUNT(h.cancao_id) DESC, c.cancao
+ORDER BY COUNT(h.usuario_id) DESC, c.cancao
 LIMIT 2;
