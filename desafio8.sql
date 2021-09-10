ec683cae0fba83da973fb422a753095fda5bd4e6
LeoLacana <wilson17414@gmail.com>
2021-09-10 00:29:32 -0300

    requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON SpotifyClone.usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM SpotifyClone.historico_de_reproducao
+WHERE OLD.usuario_id = usuario_id;
+DELETE FROM SpotifyClone.seguindo_artistas
+WHERE OLD.usuario_id = usuario_id;
+END $$
+DELIMITER ;
