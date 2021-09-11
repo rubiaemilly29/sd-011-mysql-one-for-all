c90caa1a7c9e2f36b7ab9e7a0c979f16d90cbd38
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-11 01:13:21 -0300

    soluciona desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+-- create view faturamento_atual as
+SELECT round(min(su.price), 2) AS faturamento_minimo,
+  round(max(su.price), 2) AS faturamento_maximo,
+  round(avg(su.price), 2) AS faturamento_medio,
+  round(sum(su.price), 2) AS faturamento_total
+FROM SpotifyClone.subscriptions AS su
+  INNER JOIN SpotifyClone.users AS usr ON su.subscription_id = usr.subscription_id;
