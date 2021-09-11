57002e041e8cddf867d21f74fdc852b89e797cab
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-11 00:50:31 -0300

    soluciona desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,1 +1,9 @@
-CREATE VIEW top_3_artistas
+CREATE VIEW top_3_artistas AS
+SELECT ar.name AS artista,
+  COUNT(fl.artist_id) AS seguidores
+FROM SpotifyClone.following_list AS fl
+  INNER JOIN SpotifyClone.artists AS ar ON fl.artist_id = ar.artist_id
+GROUP BY fl.artist_id
+ORDER BY seguidores DESC,
+  artista ASC
+LIMIT 3;
