8189a3218ceaad7f9546912a8294bd5c2410836a
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-09-10 23:24:12 -0300

    Cria view estatisticas_musicais

---
+++ desafio2.sql
@@ -0,0 +1,14 @@
+CREATE VIEW  estatisticas_musicais  AS
+SELECT 
+    COUNT(*) AS cancoes,
+    (SELECT 
+            COUNT(*)
+        FROM
+            SpotifyClone.Artistas) AS artistas,
+    (SELECT 
+            COUNT(*)
+        FROM
+            SpotifyClone.Albuns) AS albuns
+FROM
+    SpotifyClone.Musicas;
+    
