a52375a1b46a5a78c3d4e35f33691c620b41cbda
DiegoCS777 <diegomethal@gmail.com>
2021-09-11 10:46:50 -0300

    desafio5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT M.nome_cancoes AS cancao, COUNT(HR.cancoes_id) AS reproducoes
+FROM SpotifyClone.musicas M
+INNER JOIN SpotifyClone.historico_reproducoes HR ON M.cancoes_id = HR.cancoes_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
+SELECT * FROM SpotifyClone.top_2_hits_do_momento;
