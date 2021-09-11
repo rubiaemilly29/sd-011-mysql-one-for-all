e2b814b4ea1f9618454a336fac2b3148d7fb1a76
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-11 00:26:16 -0300

    soluciona desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,11 @@
+-- CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(so.song_id) AS 'cancoes',
+  (
+    SELECT COUNT(ar.artist_id)
+    FROM SpotifyClone.artists AS ar
+  ) AS 'artistas',
+  (
+    SELECT COUNT(ab.album_id)
+    FROM SpotifyClone.albums AS ab
+  ) AS 'albuns'
+FROM SpotifyClone.songs AS so;
