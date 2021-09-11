45eded76dc0707bf697182e286cd1e27605ca61b
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-11 19:59:16 -0300

    melhor deixar assim mesmo, lint não deixou :(

--- desafio10.sql
+++ desafio10.sql
@@ -11,16 +11,3 @@ WHERE u.usuario_id = user_id INTO hist_rep;
 RETURN hist_rep;
 END $$
 DELIMITER ;
-
--- Outra forma:
-
--- DELIMITER $$ 
--- CREATE FUNCTION quantidade_musicas_no_historico(userID INT)
--- RETURNS INT READS SQL DATA BEGIN
--- DECLARE hist_rep INT;
--- SELECT COUNT(h.usuario_id)
--- FROM SpotifyClone.historico AS h
--- WHERE h.usuario_id = user_id INTO hist_rep;
--- RETURN hist_rep;
--- END $$
--- DELIMITER ;
