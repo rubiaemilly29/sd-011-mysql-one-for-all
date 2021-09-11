da84fde6e901605deedf0ba3d39f4e4ff21356d5
DiegoCS777 <diegomethal@gmail.com>
2021-09-11 17:34:44 -0300

    code climate desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -5,4 +5,3 @@ COUNT(DISTINCT A.album_id) AS albuns
 FROM SpotifyClone.albuns A 
 INNER JOIN SpotifyClone.musicas M ON A.album_id = M.album_id
 INNER JOIN SpotifyClone.artistas AR ON AR.id_artista = A.id_artista;
-
