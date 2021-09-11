2c3e9565e613ce161bef5d1ac7e3185580dbc0f8
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-11 02:05:52 -0300

    aplica correçao solicitada pelo code climate

--- desafio8.sql
+++ desafio8.sql
@@ -1,17 +1,13 @@
 DELIMITER $$
-
 CREATE TRIGGER trigger_on_user_delete
 BEFORE  DELETE ON SpotifyClone.users
 FOR EACH ROW
-
 BEGIN
 DELETE FROM SpotifyClone.history
 WHERE
 user_id = OLD.user_id;
-
 DELETE FROM SpotifyClone.following_list
 WHERE
 user_id = OLD.user_id;
 END $$
-
 DELIMITER ;
