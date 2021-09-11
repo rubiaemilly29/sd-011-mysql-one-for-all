cbc67612dadef4e2bbd0e7e975dd1917bb69301a
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-11 16:57:52 -0300

    fix 7

--- desafio7.sql
+++ desafio7.sql
@@ -5,5 +5,5 @@ INNER JOIN SpotifyClone.album AS b
 ON a.artista_id = b.artista_id
 INNER JOIN SpotifyClone.seguindo_artistas AS s
 ON b.artista_id = s.artista_id
-GROUP BY b.album
+GROUP BY s.artista_id, b.album
 ORDER BY 3 DESC, 1 ASC, 2 ASC;
