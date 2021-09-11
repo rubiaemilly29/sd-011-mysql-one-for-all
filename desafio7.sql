6d5c165ffebd088f8b62eb85293a167b86f24ba8
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-11 16:49:02 -0300

    requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+CREATE VIEW perfil_artistas AS
+SELECT a.nome_artista, b.album, COUNT(s.artista_id) AS seguidores
+FROM SpotifyClone.artistas AS a
+INNER JOIN SpotifyClone.album AS b
+ON a.artista_id = b.artista_id
+INNER JOIN SpotifyClone.seguindo_artistas AS s
+ON b.artista_id = s.artista_id
+GROUP BY b.album
+ORDER BY 3 DESC, 1 ASC, 2 ASC;
