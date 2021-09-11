d0ba6f25311436619f842094ebe03041b07f66e6
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-11 15:01:46 -0300

    Adicionando scritp desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT
+(
+SELECT COUNT(*) FROM cancoes
+) AS cancoes,
+(
+SELECT COUNT(*) FROM artistas
+) AS artistas,
+(
+SELECT COUNT(*) FROM albuns
+) AS albuns;
+SELECT * FROM estatisticas_musicais;
