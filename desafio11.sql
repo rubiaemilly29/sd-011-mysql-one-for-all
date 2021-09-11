612e3d528288e966997c8d59258592685b692099
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-11 20:34:50 -0300

    fix req11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 CREATE VIEW cancoes_premium AS
-SELECT c.cancao AS nome, COUNT(h.cancao_id) AS album
+SELECT c.cancao AS nome, COUNT(h.cancao_id) AS reproducoes
 FROM SpotifyClone.cancoes AS c
 INNER JOIN SpotifyClone.historico AS h
 ON c.cancao_id = h.cancao_id
@@ -9,4 +9,4 @@ INNER JOIN SpotifyClone.plano AS p
 ON u.plano_id = p.plano_id
 WHERE p.tipo_plano IN('familiar', 'universitário')
 GROUP BY c.cancao
-ORDER BY 1;
+ORDER BY 1 ASC;
