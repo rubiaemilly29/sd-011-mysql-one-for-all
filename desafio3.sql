1724e56bb1278f9f8575965513de0dcc22b3b6b8
Alan Gonçalves <vekio.soft@gmail.com>
2021-09-11 13:11:15 -0300

    fix code req 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.nome_usuario , cancao
+SELECT u.nome_usuario AS usuario , c.cancao AS nome
 FROM SpotifyClone.usuario AS u
 INNER JOIN SpotifyClone.historico AS h
 ON u.usuario_id = h.usuario_id
