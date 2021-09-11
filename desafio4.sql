bb52e587295d395cbd54ce38765952668fe8b9f6
DiegoCS777 <diegomethal@gmail.com>
2021-09-11 10:36:23 -0300

    desafio2 e desafio4

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_3_artistas AS
+SELECT A.artista_nome AS artista, COUNT(S.id_assinante) AS seguidores
+FROM SpotifyClone.seguindo S
+INNER JOIN SpotifyClone.artistas A ON A.id_artista = S.id_artista
+GROUP BY artista
+ORDER BY seguidores DESC, artista
+LIMIT 3;
+SELECT * FROM SpotifyClone.top_3_artistas;
