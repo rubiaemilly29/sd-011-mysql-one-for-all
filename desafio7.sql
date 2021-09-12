ab11681b37a254712b7f0ed6d94897f7456719ad
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-12 17:10:43 -0300

    Corrigindo script desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT ar.artista AS artista,
+al.album AS album,
+(
+SELECT COUNT(*) FROM seguindo_artistas
+WHERE artista_id = ar.artista_id
+) AS seguidores
+FROM albuns AS al
+INNER JOIN artistas AS ar ON al.artista_id = ar.artista_id
+ORDER BY seguidores DESC, artista, album;
