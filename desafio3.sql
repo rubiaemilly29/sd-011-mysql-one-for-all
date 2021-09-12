bec8f5d905fee644e7f6d427fd9a2e6903219232
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-11 21:31:54 -0300

    Adicionando script desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.usuario, c.cancao
+FROM historico_reproducoes AS hr
+INNER JOIN usuarios AS u ON hr.usuario_id = u.usuario_id
+INNER JOIN cancoes AS c ON c.cancao_id = hr.cancao_id
+ORDER BY u.usuario, c.cancao;
