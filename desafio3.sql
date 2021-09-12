d73dcc214f46e8380384f506ddc96b7f5ed68d57
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-11 21:42:52 -0300

    Corrigindo script desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,8 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.usuario, c.cancao
+SELECT u.usuario AS 'usuario', c.cancao AS 'nome'
 FROM historico_reproducoes AS hr
 INNER JOIN usuarios AS u ON hr.usuario_id = u.usuario_id
 INNER JOIN cancoes AS c ON c.cancao_id = hr.cancao_id
 ORDER BY u.usuario, c.cancao;
+
+SELECT * FROM historico_reproducao_usuarios;	
