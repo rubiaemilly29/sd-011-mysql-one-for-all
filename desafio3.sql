f6980239b7386774d8877ef2836d0ba3cb6fbb89
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-11 21:43:38 -0300

    Corrigindo script desafio 3 novamente

--- desafio3.sql
+++ desafio3.sql
@@ -4,5 +4,3 @@ FROM historico_reproducoes AS hr
 INNER JOIN usuarios AS u ON hr.usuario_id = u.usuario_id
 INNER JOIN cancoes AS c ON c.cancao_id = hr.cancao_id
 ORDER BY u.usuario, c.cancao;
-
-SELECT * FROM historico_reproducao_usuarios;	
