5d8c782d94e911bfd354a814b2f8016b88708f05
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-09-12 19:39:35 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+SELECT 
+    u.nome AS usuario, m.nome AS nome
+FROM
+    SpotifyClone.historico_reproducao AS hr
+        INNER JOIN
+    SpotifyClone.Usuarios AS u ON hr.Usuario_ID = u.Usuario_ID
+        INNER JOIN
+    SpotifyClone.Musicas AS m ON hr.Musica_ID = m.Musica_ID
+ORDER BY usuario , nome;
