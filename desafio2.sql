9bb28f71072eef8e56b409fea517ee329eb71463
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-12 17:45:48 -0300

    Ajustando requisitos 2 e 3

--- desafio2.sql
+++ desafio2.sql
@@ -2,10 +2,8 @@ USE SpotifyClone;
 
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(C.cancao) AS cancoes,
-COUNT(A.artista_id) AS artistas,
-COUNT(AL.album) AS albuns
+COUNT(DISTINCT(artista_id)) AS artistas,
+COUNT(DISTINCT(AL.album)) AS albuns
 FROM cancoes AS C
 INNER JOIN albuns AS AL
-ON C.album_id = AL.album_id
-INNER JOIN  artistas AS A
-ON A.artista_id = AL.artista_id;
+ON C.album_id = AL.album_id;
--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,10 @@
 USE SpotifyClone;
 
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT usuario AS usuario, usuario_id AS nome
+SELECT usuario AS usuario, cancao AS nome
 FROM usuarios AS U
 INNER JOIN historico AS H
-ON U.usario_id =  H.usuario_id
+ON U.usuario_id =  H.usuario_id
+INNER JOIN cancoes AS C
+ON C.cancoes_id = H.cancoes_id
 ORDER BY usuario, nome;
