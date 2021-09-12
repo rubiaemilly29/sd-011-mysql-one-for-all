4c986c4c3f390cb832cbf6acaf9c266b9e616863
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-09-12 17:54:34 -0300

    REQ 2 e REQ 3

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,7 @@
-USE SpotifyClone;
-
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(C.cancao) AS cancoes,
 COUNT(DISTINCT(artista_id)) AS artistas,
 COUNT(DISTINCT(AL.album)) AS albuns
-FROM cancoes AS C
-INNER JOIN albuns AS AL
+FROM SpotifyClone.cancoes AS C
+INNER JOIN SpotifyClone.albuns AS AL
 ON C.album_id = AL.album_id;
--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,8 @@
-USE SpotifyClone;
-
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT usuario AS usuario, cancao AS nome
-FROM usuarios AS U
-INNER JOIN historico AS H
+FROM SpotifyClone.usuarios AS U
+INNER JOIN SpotifyClone.historico AS H
 ON U.usuario_id =  H.usuario_id
-INNER JOIN cancoes AS C
+INNER JOIN SpotifyClone.cancoes AS C
 ON C.cancoes_id = H.cancoes_id
 ORDER BY usuario, nome;
