38caa66d0a0de55907e20d7535a5e72f37328b70
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-12 21:21:19 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT u.usuario AS usuario, c.cancao AS nome
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h
+ON u.usuario_id = h.usuario_id
+INNER JOIN SpotifyClone.cancoes AS c
+ON h.cancao_id = c.cancao_id
+ORDER BY usuario ASC, nome ASC;
