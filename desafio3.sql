ed85d8777f64dc76732d1b751ed56bde6b33a812
Lais Simenikim <l.simenikim@gmail.com>
2021-09-12 21:44:05 -0300

    Ajust challenge 3.2

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,9 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT USUARIOS.usuario_nome AS 'usuario',
-(SELECT MUSICAS.musica_nome
-FROM SpotifyClone.MUSICAS) AS 'nome'
+MUSICAS.musica_nome AS 'nome'
 FROM SpotifyClone.USUARIOS
+INNER JOIN SpotifyClone.HISTORICO
+ON HISTORICO.usuario = USUARIOS.usuario_id
+INNER JOIN SpotifyClone.MUSICAS
+ON MUSICAS.musica_id = HISTORICO.musica
 ORDER BY `usuario`, `nome`;
