8b2ccbe1abfb84964bd3767060f6330b2471d57d
Maria Lisboa <marialisboa1515@gmail.com>
2021-09-13 13:19:05 -0300

    segunda tentativa

--- desafio1.sql
+++ desafio1.sql
@@ -88,27 +88,26 @@ VALUES
     ('Lance Day',4),
     ('Freedie Shannon',5);
   
-  INSERT INTO cancoes (nome_cancao)
+    INSERT INTO cancoes (nome_cancao, id_album)
 VALUES
-    ('Soul For Us'),
-    ('Reflections Of Magic'),
-    ('Dance With Her Own'),
-    ('Troubles Of My Inner Fire'),
-    ('Time Fireworks'),
-    ('Magic Circus'),
-    ('Honey, So Do I'),
-    ('Sweetie, Let\'s Go Wild'),
-    ('She Knows'),
-    ('Fantasy For Me'),
-    ('Celebration Of More'),
-    ('Rock His Everything'),
-    ('Home Forever'),
-    ('Diamond Power'),
-    ('Honey, Let\'s Be Silly'),
-    ('Thang Of Thunder'),
-    ('Words Of Her Life'),
-    ('Without My Streets');
-  
+    ('Soul For Us', 1),
+    ('Reflections Of Magic', 1),
+    ('Dance With Her Own', 1),
+    ('Troubles Of My Inner Fire', 2),
+    ('Time Fireworks', 2),
+    ('Magic Circus', 3),
+    ('Honey, So Do I', 3),
+    ('Sweetie, Let\'s Go Wild', 3),
+    ('She Knows', 3),
+    ('Fantasy For Me', 4),
+    ('Celebration Of More', 4),
+    ('Rock His Everything', 4),
+    ('Home Forever', 4),
+    ('Diamond Power', 4),
+    ('Honey, Let\'s Be Silly', 4),
+    ('Thang Of Thunder', 5),
+    ('Words Of Her Life', 5),
+    ('Without My Streets', 5);
   INSERT INTO historico_reproducoes (id_usuario, id_cancao)
 VALUES
     (1, 1),
