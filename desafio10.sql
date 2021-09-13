67e37bd2a603fb8eb203eb1aec3fb53da570b53a
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-09-12 23:31:56 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+DELIMITER $$;
+
+CREATE FUNCTION quantidade_musicas_no_historico(id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE historico INT;
+SELECT COUNT(*) FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico_de_reproducoes AS h
+ON u.usuario_id = h.usuario_id
+WHERE u.usuario_id = id
+INTO historico;
+RETURN historico;
+END $$
+
+DELIMITER ;
