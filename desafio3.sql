7e79e46a9dcbb57ca81fe71e6b09e5b7069f7642
Lais Simenikim <l.simenikim@gmail.com>
2021-09-12 21:21:59 -0300

    Ajust challenge 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT USUARIOS.usuario_nome AS 'usuario',
-(SELECT MUSICAS.musica_id
+(SELECT MUSICAS.musica_nome
 FROM SpotifyClone.MUSICAS) AS 'nome'
 FROM SpotifyClone.USUARIOS
 ORDER BY `usuario`, `nome`;
