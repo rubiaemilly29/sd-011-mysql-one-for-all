45f27e2d56a08d75f9b9beff5449e3fa3cc4e585
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-09-12 23:22:41 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(Usuario_ID INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantity INT;
+SELECT  COUNT(hr.Musica_ID) FROM SpotifyClone.Usuarios AS u
+INNER JOIN SpotifyClone.historico_reproducao AS hr
+ON hr.Usuario_ID = u.Usuario_ID
+WHERE u.Usuario_ID = Usuario_ID 
+GROUP BY u.nome
+INTO quantity;
+RETURN quantity;
+END $$
+DELIMITER ;
