20e26466e910c32fa7ba11b059bd23fea0a018da
juliabaptista <juliabaptistadeoliveira@gmail.com>
2021-09-13 11:35:52 -0300

    Fix: req-07 / refactor req-04

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,9 @@
 CREATE VIEW top_3_artistas AS
 SELECT artist.nome_artista AS artista,
-    COUNT(x.usuario_id) AS seguidores
+    COUNT(followers.usuario_id) AS seguidores
 FROM SpotifyClone.artistas AS artist
     INNER JOIN 
-        SpotifyClone.seguindo_artistas AS x ON artist.artista_id = x.artista_id
+        SpotifyClone.seguindo_artistas AS followers ON artist.artista_id = followers.artista_id
 GROUP BY `artista`
 ORDER BY `seguidores` DESC, `artista`
 LIMIT 3;
--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 CREATE VIEW top_2_hits_do_momento AS
 SELECT music.nome_musica AS cancao,
-    COUNT(hist_repro.usuario_id) AS reproducoes
+    COUNT(hr.usuario_id) AS reproducoes
 FROM SpotifyClone.musicas AS music
     INNER JOIN
-        SpotifyClone.historico_de_reproducoes AS hist_repro ON music.musica_id = hist_repro.musica_id
+        SpotifyClone.historico_de_reproducoes AS hr ON music.musica_id = hr.musica_id
 GROUP BY `cancao`
 ORDER BY `reproducoes` DESC, `cancao`
 LIMIT 2;
