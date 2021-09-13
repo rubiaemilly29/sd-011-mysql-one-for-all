49f51f7c0124cd7d67686712255221aa5e4c5f7e
Lais Simenikim <l.simenikim@gmail.com>
2021-09-12 23:23:40 -0300

    Add challenge 10

--- desafio10.sql
+++ desafio10.sql
@@ -3,9 +3,9 @@ CREATE FUNCTION quantidade_musicas_no_historico(usuario INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total_musicas INT;
-SELECT COUNT(HISTORICO.usuario)
+SELECT COUNT(HISTORICO.musica)
 FROM SpotifyClone.HISTORICO
-WHERE HISTORICO.usuario = 3
+WHERE HISTORICO.usuario = usuario
 INTO total_musicas;
 RETURN total_musicas;
 END $$
