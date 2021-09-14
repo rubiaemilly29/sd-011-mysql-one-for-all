91177e816911acd0b90627af8a8b74f1f6638782
Maria Lisboa <marialisboa1515@gmail.com>
2021-09-14 09:17:05 -0300

    desafio3

--- desafio3.sql
+++ desafio3.sql
@@ -2,7 +2,7 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT us.nome_usuario AS 'usuario',
   ca.nome_cancao AS 'nome'
 FROM SpotifyClone.usuarios AS us
-  INNER JOIN SpotifyClone.historico_reproducoes AS hs ON us.user_id = hs.user_id
-  INNER JOIN SpotifyClone.cancoes AS ca ON hs.song_id = ca.song_id
+  INNER JOIN SpotifyClone.historico_reproducoes AS hs ON us.id_usuarios = hs.id_usuarios
+  INNER JOIN SpotifyClone.cancoes AS ca ON hs.id_cancoes = ca.id_cancoes
 ORDER BY us.nome_usuario ASC,
   ca.nome_cancao ASC;
