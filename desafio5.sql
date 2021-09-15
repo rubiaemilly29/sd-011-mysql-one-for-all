939e0b83258c370ef6cb5eea85cd0a791ce76612
juliabaptista <juliabaptistadeoliveira@gmail.com>
2021-09-15 13:02:50 -0300

    Fix refactor req-05

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 CREATE VIEW top_2_hits_do_momento AS
-SELECT music.nome_musica AS cancao,
+SELECT m.nome_musica AS cancao,
     COUNT(hr.usuario_id) AS reproducoes
-FROM SpotifyClone.musicas AS music
+FROM SpotifyClone.musicas AS m
     INNER JOIN
-        SpotifyClone.historico_de_reproducoes AS hr ON music.musica_id = hr.musica_id
+        SpotifyClone.historico_de_reproducoes AS hr ON m.musica_id = hr.musica_id
 GROUP BY `cancao`
 ORDER BY `reproducoes` DESC, `cancao`
 LIMIT 2;
