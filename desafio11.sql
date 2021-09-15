c722cd10acffb93bdfa3bfe3286a3d6e7ba5e954
juliabaptista <juliabaptistadeoliveira@gmail.com>
2021-09-15 13:04:27 -0300

    Fix req-11

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,12 @@
-USE SpotifyClone;
 CREATE VIEW cancoes_premium AS
     SELECT music.nome_musica AS nome, COUNT(*) AS reproducoes
-    FROM usuarios AS user
+    FROM SpotifyClone.usuarios AS user
         INNER JOIN 
-            historico_de_reproducoes AS hr ON user.usuario_id = hr.usuario_id
+            SpotifyClone.historico_de_reproducoes AS hr ON user.usuario_id = hr.usuario_id
         INNER JOIN
-            musicas AS music ON music.musica_id = hr.musica_id
+            SpotifyClone.musicas AS music ON music.musica_id = hr.musica_id
         INNER JOIN
-        planos AS plans ON u.plano_id = plans.plano_id
+            SpotifyClone.planos AS plans ON user.plano_id = plans.plano_id
         WHERE plans.plano IN ('Familiar', 'Universitário')
     GROUP BY `nome`
     ORDER BY `nome`;
