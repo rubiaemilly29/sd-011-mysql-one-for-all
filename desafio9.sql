cb0dfe022a15dcd766f83cf53cdad67159ff21ed
juliabaptista <juliabaptistadeoliveira@gmail.com>
2021-09-15 12:15:00 -0300

    Feat: req-09

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+USE SpotifyClone;
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista(IN artista VARCHAR(30))
+BEGIN
+SELECT artist.nome_artista AS artista, al.nome_album AS album
+FROM artistas AS artist
+    INNER JOIN albuns AS al ON al.artista_id = artist.artista_id
+WHERE artist.nome_artista = artista
+ORDER BY album;
+END $$
+DELIMITER ;
